7d21f9bd895615262d085a4d6ef232b8af250675
Janel <mac@macs-MacBook-Air.local>
2018-09-23 13:37:34 -0400

    Removed IoT dashboard, changed some theme colors, added profile card, added follow runs card, added follow maps card

--- client/src/app/pages/dashboard/dashboard.module.ts
+++ client/src/app/pages/dashboard/dashboard.module.ts
@@ -1,50 +1,83 @@
 import { NgModule } from '@angular/core';
-
 import { NgxEchartsModule } from 'ngx-echarts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
-import { StatusCardComponent } from './status-card/status-card.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
-import { TemperatureComponent } from './temperature/temperature.component';
-import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
-import { TeamComponent } from './team/team.component';
-import { KittenComponent } from './kitten/kitten.component';
-import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
-import { ElectricityComponent } from './electricity/electricity.component';
-import { ElectricityChartComponent } from './electricity/electricity-chart/electricity-chart.component';
-import { WeatherComponent } from './weather/weather.component';
-import { SolarComponent } from './solar/solar.component';
-import { PlayerComponent } from './rooms/player/player.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { TrafficChartComponent } from './traffic/traffic-chart.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { ECommerceChartsPanelComponent } from './charts-panel/charts-panel.component';
+import { OrdersChartComponent } from './charts-panel/charts/orders-chart.component';
+import { ProfitChartComponent } from './charts-panel/charts/profit-chart.component';
+import { ChartPanelHeaderComponent } from './charts-panel/chart-panel-header/chart-panel-header.component';
+import { ChartPanelSummaryComponent } from './charts-panel/chart-panel-summary/chart-panel-summary.component';
+import { ChartModule } from 'angular2-chartjs';
+import { StatsBarAnimationChartComponent } from './profile-card/front-side/stats-bar-animation-chart.component';
+import { StatsCardFrontComponent } from './profile-card/front-side/stats-card-front.component';
+import { TrafficRevealCardComponent } from './traffic-reveal-card/traffic-reveal-card.component';
+import { TrafficBarComponent } from './traffic-reveal-card/front-side/traffic-bar/traffic-bar.component';
+import { TrafficFrontCardComponent } from './traffic-reveal-card/front-side/traffic-front-card.component';
+import { TrafficCardsHeaderComponent } from './traffic-reveal-card/traffic-cards-header/traffic-cards-header.component';
+import { TrafficBackCardComponent } from './traffic-reveal-card/back-side/traffic-back-card.component';
+import { TrafficBarChartComponent } from './traffic-reveal-card/back-side/traffic-bar-chart.component';
+import {
+  ECommerceVisitorsStatisticsComponent,
+} from './follow-runs/visitors-statistics/visitors-statistics.component';
+import { ECommerceLegendChartComponent } from './legend-chart/legend-chart.component';
+import { ECommerceUserActivityComponent } from './user-activity/user-activity.component';
+import { ECommerceProgressSectionComponent } from './follow-maps/progress-section.component';
+import { SlideOutComponent } from './slide-out/slide-out.component';
 
+import { CountryOrdersComponent } from './country-orders/country-orders.component';
+import { CountryOrdersMapComponent } from './country-orders/map/country-orders-map.component';
+import { CountryOrdersMapService } from './country-orders/map/country-orders-map.service';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { CountryOrdersChartComponent } from './country-orders/chart/country-orders-chart.component';
+import { EarningCardComponent } from './earning-card/earning-card.component';
+import { EarningCardBackComponent } from './earning-card/back-side/earning-card-back.component';
+import { EarningPieChartComponent } from './earning-card/back-side/earning-pie-chart.component';
+import { EarningCardFrontComponent } from './earning-card/front-side/earning-card-front.component';
+import { EarningLiveUpdateChartComponent } from './earning-card/front-side/earning-live-update-chart.component';
 
 @NgModule({
   imports: [
     ThemeModule,
+    ChartModule,
     NgxEchartsModule,
+    NgxChartsModule,
+    LeafletModule,
   ],
   declarations: [
     DashboardComponent,
-    StatusCardComponent,
-    TemperatureDraggerComponent,
-    ContactsComponent,
-    RoomSelectorComponent,
-    TemperatureComponent,
-    RoomsComponent,
-    TeamComponent,
-    KittenComponent,
-    SecurityCamerasComponent,
-    ElectricityComponent,
-    ElectricityChartComponent,
-    WeatherComponent,
-    PlayerComponent,
-    SolarComponent,
-    TrafficComponent,
-    TrafficChartComponent,
+    StatsCardFrontComponent,
+    StatsBarAnimationChartComponent,
+    ProfileCardComponent,
+    ECommerceChartsPanelComponent,
+    ChartPanelHeaderComponent,
+    ChartPanelSummaryComponent,
+    OrdersChartComponent,
+    ProfitChartComponent,
+    TrafficRevealCardComponent,
+    TrafficBarChartComponent,
+    TrafficFrontCardComponent,
+    TrafficBackCardComponent,
+    TrafficBarComponent,
+    TrafficCardsHeaderComponent,
+    CountryOrdersComponent,
+    CountryOrdersMapComponent,
+    CountryOrdersChartComponent,
+    ECommerceVisitorsStatisticsComponent,
+    ECommerceLegendChartComponent,
+    ECommerceUserActivityComponent,
+    ECommerceProgressSectionComponent,
+    SlideOutComponent,
+    EarningCardComponent,
+    EarningCardFrontComponent,
+    EarningCardBackComponent,
+    EarningPieChartComponent,
+    EarningLiveUpdateChartComponent,
+  ],
+  providers: [
+    CountryOrdersMapService,
   ],
 })
 export class DashboardModule { }
--- client/src/app/pages/pages-menu.ts
+++ client/src/app/pages/pages-menu.ts
@@ -2,15 +2,10 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'nb-e-commerce',
+    title: 'Dashboard',
+    icon: 'nb-home',
     link: '/pages/dashboard',
     home: true,
-  },
-  {
-    title: 'IoT Dashboard',
-    icon: 'nb-home',
-    link: '/pages/iot-dashboard',
   },
   {
     title: 'FEATURES',
