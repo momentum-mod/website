18c2afad62bda8c61c1c077975f39cb8df88e060
Corey Piasecki <faulk_128@hotmail.com>
2018-09-25 12:03:37 -0400

    Merged server changes

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,9 +3,11 @@ const { User } = require('../../config/sqlize');
 
 module.exports = {
 
-	find: (steamID) => {
-		return User.findOne({ where: {id: steamID}});
-	},
+	Permission: Object.freeze({
+		BANNED: 1 << 0,
+		ADMIN: 1 << 1,
+		MAPPER: 1 << 2
+	}),
 
 	findOrCreate: (openID) => {
 		return new Promise((resolve, reject) => {
@@ -15,6 +17,45 @@ module.exports = {
 				console.log(created);
 				resolve(usr);
 			}).catch(reject);
+		});
+	},
+
+	find: (steamID) => {
+		return User.findOne({ where: {id: steamID}});
+	},
+
+	// find: (context) => {
+	// 	return new Promise((resolve, reject) => {
+	// 		// fetch user from db using context given
+	// 		resolve([{
+	// 			id: 42,
+	// 			name: "Gabe Newell",
+	// 			alias: "Gaben",
+	// 			permissions: 0
+	// 		}]);
+	// 	});
+	// },
+
+	update: (userID, userInfo) => {
+		return new Promise((resolve, reject) => {
+			// update user in db
+			resolve();
+		});
+	},
+
+	addPermission: (userID, permission) => {
+		return new Promise((resolve, reject) => {
+			// update permission in db to
+			// current permission | permission
+			resolve();
+		});
+	},
+
+	removePermission: (userID, permission) => {
+		return new Promise((resolve, reject) => {
+			// update permission in db to
+			// current permission & ~permission
+			resolve();
 		});
 	}
 
