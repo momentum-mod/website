a2fc9c8373c03e7f725e28c92a602e40bc816fc6
Corey Piasecki <faulk_128@hotmail.com>
2018-09-25 22:10:36 -0400

    Update dashboard to display user avatar and display name

--- client/src/app/pages/dashboard/profile-card/profile-card.component.ts
+++ client/src/app/pages/dashboard/profile-card/profile-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../@core/data/user.service';
 
 @Component({
   selector: 'ngx-profile-card',
@@ -8,6 +9,14 @@ import { Component } from '@angular/core';
 export class ProfileCardComponent {
 
   flipped = false;
+
+  user: any;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.user = this.userService.getInfo();
+  }
 
   toggleView() {
     this.flipped = !this.flipped;
