ef9b596c9e254232f4b89c4c0d4b6128add4c530
Corey Piasecki <faulk_128@hotmail.com>
2018-09-25 21:08:48 -0400

    Lint making me fix whitespace :'(

--- client/src/app/@core/data/auth.service.ts
+++ client/src/app/@core/data/auth.service.ts
@@ -3,46 +3,46 @@ import { CookieService } from 'ngx-cookie-service';
 import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
 
 interface AccessTokenPayload {
-	id: number;
-	displayName: string;
-	avatar: string;
-	permissions: number;
+  id: number;
+  displayName: string;
+  avatar: string;
+  permissions: number;
 }
 
 @Injectable()
 export class AuthService {
 
-	constructor(private cookieService: CookieService, private jwtModule: JwtModule) {
-		const cookieExists = this.cookieService.check('accessToken');
-		if (cookieExists) {
-			const accessToken = this.cookieService.get('accessToken');
-			localStorage.setItem('accessToken', accessToken);
-			this.cookieService.delete('accessToken');
-		}
-	}
+  constructor(private cookieService: CookieService, private jwtModule: JwtModule) {
+    const cookieExists = this.cookieService.check('accessToken');
+    if (cookieExists) {
+      const accessToken = this.cookieService.get('accessToken');
+      localStorage.setItem('accessToken', accessToken);
+      this.cookieService.delete('accessToken');
+    }
+  }
 
-	public logout(): void {
-		localStorage.setItem('accessToken', '');
-	}
+  public logout(): void {
+    localStorage.setItem('accessToken', '');
+  }
 
-	public isLoggedIn(): boolean {
-		const accessToken = localStorage.getItem('accessToken');
-		const jwtHelperService = new JwtHelperService();
-		if (accessToken === null) {
-			return false;
-		}
-		const isTokenExpired = jwtHelperService.isTokenExpired(accessToken);
-		return !isTokenExpired;
-	}
+  public isLoggedIn(): boolean {
+    const accessToken = localStorage.getItem('accessToken');
+    const jwtHelperService = new JwtHelperService();
+    if (accessToken === null) {
+      return false;
+    }
+    const isTokenExpired = jwtHelperService.isTokenExpired(accessToken);
+    return !isTokenExpired;
+  }
 
-	public getAccessTokenPayload(): AccessTokenPayload {
-		const accessToken = localStorage.getItem('accessToken');
-		const jwtHelperService = new JwtHelperService();
-		if (accessToken === null) {
-			return null;
-		}
-		const payload = jwtHelperService.decodeToken(accessToken);
-		return payload;
-	}
+  public getAccessTokenPayload(): AccessTokenPayload {
+    const accessToken = localStorage.getItem('accessToken');
+    const jwtHelperService = new JwtHelperService();
+    if (accessToken === null) {
+      return null;
+    }
+    const payload = jwtHelperService.decodeToken(accessToken);
+    return payload;
+  }
 
 }
--- client/src/app/@core/data/user.service.ts
+++ client/src/app/@core/data/user.service.ts
@@ -2,32 +2,32 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class UserService {
 
-	private id: number;
-	private displayName: string;
-	private avatar: string;
-	private permissions: number;
+  private id: number;
+  private displayName: string;
+  private avatar: string;
+  private permissions: number;
 
-	constructor(private authService: AuthService) {
-		const userInfo = this.authService.getAccessTokenPayload();
-		if (userInfo !== null) {
-			this.id = userInfo.id;
-			this.displayName = userInfo.displayName;
-			this.avatar = userInfo.avatar;
-			this.permissions = userInfo.permissions;
-		}
-	}
+  constructor(private authService: AuthService) {
+    const userInfo = this.authService.getAccessTokenPayload();
+    if (userInfo !== null) {
+      this.id = userInfo.id;
+      this.displayName = userInfo.displayName;
+      this.avatar = userInfo.avatar;
+      this.permissions = userInfo.permissions;
+    }
+  }
 
-	public getInfo() {
-		return {
-			id: this.id,
-			displayName: this.displayName,
-			avatar: this.avatar,
-			permissions: this.permissions
-		}
-	}
+  public getInfo() {
+    return {
+      id: this.id,
+      displayName: this.displayName,
+      avatar: this.avatar,
+      permissions: this.permissions
+    }
+  }
 
 }
