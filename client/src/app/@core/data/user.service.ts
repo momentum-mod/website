6b3a630a47724ad4f8b8cb38c4a75695a07ae849
Corey Piasecki <faulk_128@hotmail.com>
2018-09-26 22:29:07 -0400

    Update client auth and user service

--- client/src/app/@core/data/user.service.ts
+++ client/src/app/@core/data/user.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
+export enum Permission {
+  MAPPER = 1 << 0,
+  MODERATOR = 1 << 1,
+  ADMIN = 1 << 2,
+  BANNED_LEADERBOARDS = 1 << 3,
+  BANNED_ALIAS = 1 << 4,
+  BANNED_AVATAR = 1 << 5,
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -27,7 +36,15 @@ export class UserService {
       displayName: this.displayName,
       avatar: this.avatar,
       permissions: this.permissions,
-  };
+    };
+  }
+
+  public isLoggedIn() {
+    return this.authService.isAuthenticated();
+  }
+
+  public hasPermission(permission: Permission) {
+    return permission & this.permissions;
   }
 
 }
