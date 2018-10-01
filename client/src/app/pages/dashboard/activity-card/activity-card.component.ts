a48e3a3e8987ac3891932a412f89b571917d2da9
Nick K <Gocnak@users.noreply.github.com>
2018-10-01 19:48:57 -0400

    Give example activity stuff

--- client/src/app/pages/dashboard/activity-card/activity-card.component.ts
+++ client/src/app/pages/dashboard/activity-card/activity-card.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {Activity, Activity_Type, MapActivityData} from './activity';
 
 @Component({
   selector: 'activity-card',
@@ -7,7 +8,36 @@ import {Component, Input} from '@angular/core';
 
 export class ActivityCardComponent {
   @Input('header') headerTitle: string;
+
   constructor() {
     this.headerTitle = 'Activity';
+    this.activities = this.activitiesAll;
+  }
+
+  filterValue: number = 0;
+
+  // TODO use a service for this
+  activitiesAll: Activity[] = [
+    new Activity(Activity_Type.MAP_UPLOADED, 1, new MapActivityData(1)),
+  ];
+
+  activities: Activity[];
+
+  getActivities(): Activity[] {
+    if (this.filterValue > 0) {
+      return this.activitiesAll.filter((value => value.type !== this.filterValue));
+    } else {
+      return this.activitiesAll;
+    }
+  }
+
+  // TODO figure out why this doesn't work
+  filterSelected(value: number) {
+    this.filterValue = value;
+    /*console.log('Check ' + value);
+    console.log('Before: ');
+    console.log(this.activities);*/
+    this.activities = this.getActivities();
+    // console.log(this.activities);
   }
 }
---
+++ client/src/app/pages/dashboard/activity-card/activity.ts
@@ -0,0 +1,19 @@
+
+
+export enum Activity_Type {
+  TIME_SUBMITTED = 1,
+  MAP_UPLOADED = 2,
+}
+
+export class MapActivityData {
+  constructor(public mapID: number /* TODO more info*/) {
+  }
+}
+
+export class Activity {
+  constructor(public type: Activity_Type, // Type of the activity
+              public userID: number,   // ID of the user who did this activity
+              // Data object: map uploads have the map object, times has the submission to leaderboard, etc
+              public data: any) {
+  }
+}
