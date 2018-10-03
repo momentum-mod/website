8c2ea0bd3454adea52558a6f3c7f9566ae365c56
Nick K <Gocnak@users.noreply.github.com>
2018-10-03 14:06:12 -0400

    Fix production build
    Cache both yarn and node_modules

--- client/src/app/pages/dashboard/profile-card/profile-card.component.ts
+++ client/src/app/pages/dashboard/profile-card/profile-card.component.ts
@@ -12,7 +12,7 @@ export class ProfileCardComponent implements OnInit {
   user: any;
   permission = Permission;
 
-  constructor(private userService: UserService) {}
+  constructor(public userService: UserService) {}
 
   ngOnInit() {
     this.user = this.userService.getInfo();
