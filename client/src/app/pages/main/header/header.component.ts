ee514d6d2161b9a8149536dc06c50206e3c8a2b4
Janel <janelyousif1@gmail.com>
2018-10-04 13:44:39 -0400

    Lint issues

--- client/src/app/pages/main/header/header.component.ts
+++ client/src/app/pages/main/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
