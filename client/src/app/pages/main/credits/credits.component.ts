4c023bd108fd76fd2f3c85ef809e920bd60817a2
Janel <janelyousif1@gmail.com>
2018-10-04 12:02:35 -0400

    Updated main page

---
+++ client/src/app/pages/main/credits/credits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-credits',
+  templateUrl: './credits.component.html',
+  styleUrls: ['./credits.component.scss'],
+})
+export class CreditsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/pages/main/main-page.module.ts
+++ client/src/app/pages/main/main-page.module.ts
@@ -1,8 +1,13 @@
 import {NgModule} from '@angular/core';
 import {MainPageComponent} from './main-page.component';
+import { AboutComponent } from './about/about.component';
+import { CreditsComponent } from './credits/credits.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, AboutComponent, CreditsComponent, HeaderComponent, FooterComponent, NavComponent],
 })
 export class MainPageModule {}
