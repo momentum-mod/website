abe57ec844a3867e8bd78215b8d4465ab224d3c1
Janel <janelyousif1@gmail.com>
2018-10-04 13:44:39 -0400

    Lint issues

--- client/src/app/pages/main/about/about.component.ts
+++ client/src/app/pages/main/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
 
