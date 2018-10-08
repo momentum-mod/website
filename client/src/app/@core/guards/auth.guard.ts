ff6007f62a216e699430e1ac8a37930413e599d3
Corey Piasecki <faulk_128@hotmail.com>
2018-10-07 23:00:37 -0400

    Added client auth guard

---
+++ client/src/app/@core/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../data/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    let hasPermission = true;
+    if (route.data && route.data.onlyAllow) {
+      hasPermission = this.checkPermissions(route.data.onlyAllow);
+    }
+    if (hasPermission && this.userService.isLoggedIn()) {
+      return true;
+    }
+    window.location.href = '/api/auth/steam';
+  }
+
+  checkPermissions(permissions): boolean {
+    let hasPermission = false;
+    permissions.forEach(permission => {
+      if (this.userService.hasPermission(permission)) {
+        hasPermission = true;
+      }
+    });
+    return hasPermission;
+  }
+
+}
