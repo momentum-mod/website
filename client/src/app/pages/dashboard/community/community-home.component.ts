c35168f67374ca2124e082c97d626fec709c870e
ssyasoni <ssyasoni@oakland.edu>
2018-10-09 10:04:56 -0400

    Twitch Page edited, layout, images

--- client/src/app/pages/dashboard/community/community-home.component.ts
+++ client/src/app/pages/dashboard/community/community-home.component.ts
@@ -1,7 +1,14 @@
 import {Component} from '@angular/core';
 
+
 @Component({
   selector: 'community',
   templateUrl: './community-home.component.html',
+  styleUrls: ['./community-home.component.scss'],
+
 })
-export class CommunityHomeComponent {}
+export class CommunityHomeComponent {
+
+
+
+}
