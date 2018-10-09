4b2439939358c808f77cbac42653b1b21309d8fd
lubna Isaac <lisaac@oakland.edu>
2018-10-09 15:07:02 -0400

    added functionality to browse button
    in map upload page

--- client/src/app/pages/dashboard/maps/uploads/uploads.component.ts
+++ client/src/app/pages/dashboard/maps/uploads/uploads.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-map-queue',
   templateUrl: './uploads.component.html',
+  styleUrls: ['./uploads.component.scss'],
 })
+
 export class UploadsComponent {
+  @ViewChild('uploadFile') uploadFile;
+
+  fileToUpload: File;
+  onFileSelected(event) {
+    this.fileToUpload = event.target.files[0];
+  }
 }
+
