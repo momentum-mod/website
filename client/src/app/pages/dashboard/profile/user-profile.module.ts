e99d93889f0f7e9820599ee6925baa5254ce151d
cjshiner <cjshiner@oakland.edu>
2018-10-10 14:38:25 -0400

    Fixed Build

--- client/src/app/pages/dashboard/profile/user-profile.module.ts
+++ client/src/app/pages/dashboard/profile/user-profile.module.ts
@@ -2,8 +2,8 @@
 import { UserProfileComponent } from './user-profile.component';
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../../@theme/theme.module';
-import { ProfileCardComponent } from '../profile-card/profile-card.component';
- import { ProfileInfoComponent } from '../profile-info/profile-info.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+ import { ProfileInfoComponent } from './profile-info/profile-info.component';
 
 
 @NgModule({
