a70a3eff267c274d4c2438e319d82738db16a5f5
ssyasoni <ssyasoni@oakland.edu>
2018-10-10 13:56:42 -0400

    Update Community Layout/search feature

---
+++ client/src/app/pages/dashboard/community/community-list.component.ts
@@ -0,0 +1,63 @@
+import {Component} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import {UsersService} from '../../../@core/data/users.service';
+import { CommunityHomeComponent } from './community-home/community-home.component';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './community-list.component.html',
+  styles: [`
+    nb-card {
+      transform: translate3d(0, 0, 0);
+    }
+  `],
+})
+export class CommunityListComponent {
+
+  settings = {
+    mode: 'external',
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    columns: {
+
+      avatarUrl: {
+        title: '',
+        type: 'html',
+        valuePrepareFunction: (photo: string) => `<img width="50px" src="${photo}" />`,
+        filter: false,
+      },
+      alias: {
+        title: 'Alias',
+        type: 'string',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private usersService: UsersService, private modalService: NgbModal) {
+    this.usersService.getUsers().subscribe(data => {
+      if (data) {
+        this.source.load(data);
+      }
+    }, error => {
+      this.source.load([{
+        avatarUrl: '../assets/images/kate.png', alias: 'Kate',
+      }, {avatarUrl: '../assets/images/jack.png', alias: 'Jack',
+      }, {avatarUrl: '../assets/images/eva.png', alias: 'Eva',
+      },
+      ]);
+    });
+  }
+
+
+  onEdit(event): void {
+    const activeModal = this.modalService.open(CommunityHomeComponent, { size: 'lg' });
+    activeModal.componentInstance.loadUserData(event.data);
+  }
+}
--- client/src/app/pages/dashboard/dashboard.module.ts
+++ client/src/app/pages/dashboard/dashboard.module.ts
@@ -11,7 +11,7 @@ import { DashboardComponent } from './dashboard.component';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardHomeComponent} from './home/dashboard-home.component';
 import {UserListComponent} from './user-list/user-list.component';
-import {CommunityHomeComponent} from './community/community-home.component';
+import {CommunityListComponent} from './community/community-list.component';
 import {ActivityCardComponent} from './activity-card/activity-card.component';
 import {NotFoundModule} from '../not-found/not-found.module';
 import {SmartTableModule} from './smart-table/smart-table.module';
@@ -19,6 +19,7 @@ import { UserEditModalComponent } from './user-list/user-edit-modal/user-edit-mo
 import { VisitorsStatisticsComponent } from './follow-runs/visitors-statistics/visitors-statistics.component';
 import { ProgressSectionComponent } from './follow-maps/progress-section.component';
 import {UserProfileModule} from './profile/user-profile.module';
+import {CommunityHomeComponent} from './community/community-home/community-home.component';
 
 @NgModule({
   imports: [
@@ -37,6 +38,7 @@ import {UserProfileModule} from './profile/user-profile.module';
     DashboardComponent,
     DashboardHomeComponent,
     CommunityHomeComponent,
+    CommunityListComponent,
     UserListComponent,
     ActivityCardComponent,
     UserEditModalComponent,
