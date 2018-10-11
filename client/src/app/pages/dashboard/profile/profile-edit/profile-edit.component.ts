6d1eae716ccefd0dcc6f62954a9ebfa2569a1178
Gocnak <Gocnak@users.noreply.github.com>
2018-10-11 01:11:39 -0400

    Implemented toast
    Still working on profile updating

--- client/src/app/pages/dashboard/profile/profile-edit/profile-edit.component.ts
+++ client/src/app/pages/dashboard/profile/profile-edit/profile-edit.component.ts
@@ -1,6 +1,10 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ProfileService, UserProfile} from '../../../../@core/data/profile.service';
 import {LocalUserService} from '../../../../@core/data/local-user.service';
+import {User} from '../../../../@core/data/users.service';
+import {BodyOutputType, Toast, ToasterConfig, ToasterService} from 'angular2-toaster';
+
+import 'style-loader!angular2-toaster/toaster.css';
 
 @Component({
   selector: 'profile-edit',
@@ -9,25 +13,53 @@ import {LocalUserService} from '../../../../@core/data/local-user.service';
 })
 export class ProfileEditComponent implements OnInit {
   model: UserProfile;
+  user: User;
   @Output() onEditSuccess: EventEmitter<any> = new EventEmitter();
 
+  toasterConfig: ToasterConfig;
   constructor(private profileService: ProfileService,
-              private localUserService: LocalUserService) {
-
+              private localUserService: LocalUserService,
+              private toasterService: ToasterService) {
+    this.toasterConfig = new ToasterConfig({
+      positionClass: 'toast-top-full-width',
+      timeout: 5000,
+      newestOnTop: true,
+      tapToDismiss: true,
+      preventDuplicates: true,
+      animation: 'fade', // 'fade', 'flyLeft', 'flyRight', 'slideDown', 'slideUp'
+      limit: 5,
+    });
   }
   ngOnInit(): void {
     this.localUserService.getLocal().subscribe(usr => {
+      this.user = usr;
       this.model = usr.profile;
     });
   }
 
   onSubmit(): void {
-    this.profileService.updateUserProfile(this.model).subscribe(data => {
+    this.profileService.updateUserProfile(this.user.id, this.model).subscribe(data => {
+      // console.log('Response: ' + data);
       this.onEditSuccess.emit(this.model);
       this.localUserService.refreshLocal();
-      // TODO: redirect or toast (or both?)
+      // TODO: redirect to user profile?
+      this.showToast('success', 'Updated user profile!', '');
     }, error => {
-      alert(error.message); // TODO: replace with toaster!!! :)
+      this.showToast('error', 'Failed to update user profile!', error.message);
     });
+  }
+
+  showToast(type: string, title: string, body: string) {
+    // types: ['default', 'info', 'success', 'warning', 'error']
+    const toast: Toast = {
+      type: type,
+      title: title,
+      body: body,
+      timeout: 5000,
+      showCloseButton: true,
+      bodyOutputType: BodyOutputType.TrustedHtml,
+    };
+
+    this.toasterService.popAsync(toast);
   }
 }
