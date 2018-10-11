24eb6d3a82c9f8d6fd2d67019366d0b2128139b2
Corey Piasecki <faulk_128@hotmail.com>
2018-10-10 21:17:36 -0400

    Allowed CORS on server in dev mode

--- server/config/express.js
+++ server/config/express.js
@@ -14,6 +14,12 @@ module.exports = (app, config) => {
 
 	if (app.get('env') === 'development') {
 		app.use(logger('dev'));
+		app.use((req, res, next) => {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept");
+			next();
+		});
 	}
 
     app.use(express.json());
