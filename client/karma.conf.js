dd164bf8d214d89a18f860062aef94749b28bdc9
Gocnak <Gocnak@users.noreply.github.com>
2018-10-18 13:51:42 -0400

    Add sandbox flag for travis chrome

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     customLaunchers: {
       Chrome_travis_ci: {
         base: 'Chrome',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox --disable-setuid-sandbox']
       }
     },
     singleRun: false
