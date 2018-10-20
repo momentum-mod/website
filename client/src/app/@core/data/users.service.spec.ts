fd4a56abb1317570f2548cee15a3be18d74af5e0
lubna Isaac <lisaac@oakland.edu>
2018-10-20 16:51:56 -0400

    Unit Tests
    - Created Unit Test for Profile Service
    - Created Unit Test for Users Service

--- client/src/app/@core/data/users.service.spec.ts
+++ client/src/app/@core/data/users.service.spec.ts
@@ -1,17 +1,17 @@
 import {of} from 'rxjs';
 import {User, UsersService} from './users.service';
 
-let httpClientSpy: { get: jasmine.Spy };
+let httpClientSpy: { get: jasmine.Spy, put: jasmine.Spy };
 let usersService: UsersService;
 let expectedUsers: User[];
+let expectedUser: User;
 
 describe('UsersService', () => {
   beforeEach(() => {
     // TODO: spy on other methods too
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'put']);
     usersService = new UsersService(<any> httpClientSpy);
-    expectedUsers = [
-      {
+    expectedUser = {
         id: '1',
         permissions: 0,
         profile: {
@@ -19,9 +19,11 @@ describe('UsersService', () => {
           alias: 'test1',
           avatarURL: 'url',
         },
-      },
+      };
+    expectedUsers = [
+      expectedUser,
       {
-        id: '1',
+        id: '2',
         permissions: 1,
         profile: {
           id: '1',
@@ -48,6 +50,22 @@ describe('UsersService', () => {
         fail,
       );
       expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#getUser() should return expected user ID', () => {
+      httpClientSpy.get.and.returnValue(of(expectedUsers));
+      usersService.getUser('userID').subscribe(value =>
+          expect(value).toEqual(expectedUsers, 'expected user'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#updateUser() should return updated user account', () => {
+      httpClientSpy.put.and.returnValue(of(expectedUser));
+      usersService.updateUser(expectedUser).subscribe(value =>
+          expect(value).toEqual(expectedUser, 'expected user'),
+        fail,
+      );
+      expect(httpClientSpy.put.calls.count()).toBe(1, 'one call');
     });
   });
 });
