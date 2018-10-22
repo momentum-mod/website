b2ddfcff5d5a8dfc73c7bc03772b46b48a082982
Gocnak <Gocnak@users.noreply.github.com>
2018-10-21 21:33:31 -0400

    Update layout of global search
    Still pretty rough for now

--- client/src/app/@theme/components/search/search-results.component.ts
+++ client/src/app/@theme/components/search/search-results.component.ts
@@ -3,9 +3,12 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'search-results',
   templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent {
   @Input('user') usersResults: boolean;
+  @Input('onlyUsers') onlyUsers: boolean;
+  @Input('onlyMaps') onlyMaps: boolean;
   @Input('elems') elems: any[];
   constructor() {
   }
@@ -21,5 +24,20 @@ export class SearchResultsComponent {
       return '/dashboard/profile/' + elem.id;
     else
       return '/dashboard/maps/' + elem.id;
+  }
+
+  getElemPicture(elem: any) {
+    if (this.usersResults)
+      return elem.profile.avatarURL;
+    else
+      return elem.info.avatarURL; // TODO make sure this is right
+  }
+
+  shouldShowEmpty(): boolean {
+    if (this.usersResults) {
+      return !this.onlyMaps && this.elems && this.elems.length === 0;
+    } else {
+      return !this.onlyUsers && this.elems && this.elems.length === 0;
+    }
   }
 }
