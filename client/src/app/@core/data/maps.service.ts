e4cde3bb235bff7d784dce8ed0a8b7df12304168
lubna Isaac <lisaac@oakland.edu>
2018-10-30 19:44:29 -0400

    added code comments
    map service

--- client/src/app/@core/data/maps.service.ts
+++ client/src/app/@core/data/maps.service.ts
@@ -8,26 +8,47 @@ export class MapsService {
   constructor(private http: HttpClient) {
   }
 
+  /**
+   * @param query
+   * @return a list of maps
+   */
   searchMaps(query: string): Observable<any> {
     return this.http.get('/api/maps?expand=info&search=' + query);
   }
 
+  /**
+   * @param id
+   * @return Retrieves a specific map
+   */
   getMap(id: string): Observable<any> {
     return this.http.get('/api/maps/' + id + '?expand=info,credits');
   }
 
+  /**
+   * @param mapData
+   * @return Create a map
+   */
   createMap(mapData: object): Observable<any> {
     return this.http.post('/api/maps', mapData, {
       observe: 'response',
     });
   }
 
+  /**
+   * @param id
+   * @return map file upload location
+   */
   getMapFileUploadLocation(id: string): Observable<any> {
     return this.http.get('/api/maps/' + id + '/upload', {
       observe: 'response',
     });
   }
 
+  /**
+   * @param uploadLocation
+   * @param mapFile
+   * @return uploads a map file of a map
+   */
   uploadMapFile(uploadLocation: string, mapFile: File): Observable<any> {
     const formData = new FormData();
     formData.append('mapFile', mapFile, mapFile.name);
@@ -38,6 +59,11 @@ export class MapsService {
     });
   }
 
+  /**
+   * @param id
+   * @param avatarFile
+   * @return updated map avatar
+   */
   updateMapAvatar(id: string, avatarFile: File): Observable<any> {
     const formData = new FormData();
     formData.append('avatarFile', avatarFile, avatarFile.name);
