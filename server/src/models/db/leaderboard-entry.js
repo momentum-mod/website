542b62c176c98d7e705d86e89bbc9b9e7755744d
Corey Piasecki <faulk_128@hotmail.com>
2018-10-30 00:47:36 -0400

    More server work into run submission and leaderboards

---
+++ server/src/models/db/leaderboard-entry.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (sequelize, type) => {
+	return sequelize.define('leaderboardEntry', {
+		id: {
+			type: type.BIGINT.UNSIGNED,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		tickrate: type.SMALLINT.UNSIGNED,
+		dateAchieved: type.DATE,
+		time: type.FLOAT.UNSIGNED,
+		flags: type.INTEGER.UNSIGNED,
+	})
+};
