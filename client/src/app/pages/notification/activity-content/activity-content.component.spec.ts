1ab223e75a82cc027d81576187c7513e6c5992de
Gocnak <Gocnak@users.noreply.github.com>
2018-11-03 00:42:09 -0400

    Make tests work

--- client/src/app/pages/notification/activity-content/activity-content.component.spec.ts
+++ client/src/app/pages/notification/activity-content/activity-content.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ActivityContentComponent } from './activity-content.component';
+import {ActivityContentComponent} from './activity-content.component';
+import {NbUserModule} from '@nebular/theme';
+import {Activity_Type} from '../../../@core/models/activity-type.model';
 
 describe('ActivityContentComponent', () => {
   let component: ActivityContentComponent;
@@ -8,6 +10,7 @@ describe('ActivityContentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NbUserModule],
       declarations: [ ActivityContentComponent ],
     })
     .compileComponents();
@@ -16,6 +19,21 @@ describe('ActivityContentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ActivityContentComponent);
     component = fixture.componentInstance;
+    component.activity = {
+      id: 1,
+      user: {
+        id: '1',
+        permissions: 0,
+        profile: {
+          id: '1',
+          alias: 'Ninja',
+          bio: '',
+          avatarURL: '/assets/images/caution.png',
+        },
+      },
+      type: Activity_Type.USER_JOIN,
+      data: 'lol',
+    };
     fixture.detectChanges();
   });
 
--- client/src/app/pages/notification/notification.component.spec.ts
+++ client/src/app/pages/notification/notification.component.spec.ts
@@ -1,6 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NotificationComponent } from './notification.component';
+import {NotificationComponent} from './notification.component';
+import {NbListModule, NbUserModule} from '@nebular/theme';
+import {ActivityContentComponent} from './activity-content/activity-content.component';
+import {CoreModule} from '../../@core/core.module';
+import {RouterModule} from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ToasterModule} from 'angular2-toaster';
+import {Activity_Type} from '../../@core/models/activity-type.model';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
@@ -8,7 +16,12 @@ describe('NotificationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotificationComponent ],
+      imports: [NbListModule, NbUserModule, RouterModule.forRoot([]), HttpClientTestingModule,
+        ToasterModule.forRoot(), CoreModule.forRoot()],
+      declarations: [ NotificationComponent, ActivityContentComponent ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+      ],
     })
     .compileComponents();
   }));
@@ -16,6 +29,54 @@ describe('NotificationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
+    component.notifications = [
+      {
+        id: 1,
+        forUser: {
+          id: '1',
+          permissions: 0,
+        },
+        activity: {
+          id: 1,
+          user: {
+            id: '1',
+            permissions: 0,
+            profile: {
+              id: '1',
+              alias: 'Ninja',
+              bio: '',
+              avatarURL: '/assets/images/caution.png',
+            },
+          },
+          type: Activity_Type.USER_JOIN,
+          data: 'lol',
+        },
+        read: false,
+      },
+      {
+        id: 1,
+        forUser: {
+          id: '1',
+          permissions: 0,
+        },
+        activity: {
+          id: 1,
+          user: {
+            id: '1',
+            permissions: 0,
+            profile: {
+              id: '1',
+              alias: 'TESSSSSSSSSSSSSSSSSSST',
+              bio: '',
+              avatarURL: '/assets/images/caution.png',
+            },
+          },
+          type: Activity_Type.PB_ACHIEVED,
+          data: 'lol',
+        },
+        read: false,
+      },
+    ];
     fixture.detectChanges();
   });
 
