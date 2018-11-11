d326d49c408bc3f2a54ce2a5e68b3ba19d3ad033
Gocnak <Gocnak@users.noreply.github.com>
2018-11-10 21:59:20 -0500

    Fixed background to properly center and take up entire screen

--- client/src/app/pages/dashboard/community/community-list.component.ts
+++ client/src/app/pages/dashboard/community/community-list.component.ts
@@ -1,63 +1,11 @@
 import {Component} from '@angular/core';
-import {LocalDataSource} from 'ng2-smart-table';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
-import {UsersService} from '../../../@core/data/users.service';
-import { CommunityHomeComponent } from './community-home/community-home.component';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './community-list.component.html',
-  styles: [`
-    nb-card {
-      transform: translate3d(0, 0, 0);
-    }
-  `],
+  styleUrls: ['community-list.component.scss'],
 })
 export class CommunityListComponent {
-
-  settings = {
-    mode: 'external',
-    actions: {
-      add: false,
-      edit: false,
-      delete: false,
-    },
-    columns: {
-
-      avatarUrl: {
-        title: '',
-        type: 'html',
-        valuePrepareFunction: (photo: string) => `<img width="50px" src="${photo}" />`,
-        filter: false,
-      },
-      alias: {
-        title: 'Alias',
-        type: 'string',
-      },
-    },
-  };
-
-  source: LocalDataSource = new LocalDataSource();
-
-  constructor(private usersService: UsersService, private modalService: NgbModal) {
-    this.usersService.getUsers().subscribe(data => {
-      if (data) {
-        this.source.load(data);
-      }
-    }, error => {
-      this.source.load([{
-        avatarURL: '../assets/images/kate.png', alias: 'Kate',
-      }, {avatarURL: '../assets/images/jack.png', alias: 'Jack',
-      }, {avatarURL: '../assets/images/eva.png', alias: 'Eva',
-      },
-      ]);
-    });
-  }
-
-
-  onEdit(event): void {
-    const activeModal = this.modalService.open(CommunityHomeComponent, { size: 'lg' });
-    activeModal.componentInstance.loadUserData(event.data);
+  constructor() {
   }
 }
