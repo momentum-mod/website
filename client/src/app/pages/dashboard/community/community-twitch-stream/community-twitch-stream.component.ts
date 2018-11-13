c3ece6a788a887231f82a4ddd0e2e6f580caee55
Gocnak <Gocnak@users.noreply.github.com>
2018-11-12 20:43:59 -0500

    Hook up Twitch API service to twitch component

--- client/src/app/pages/dashboard/community/community-twitch-stream/community-twitch-stream.component.ts
+++ client/src/app/pages/dashboard/community/community-twitch-stream/community-twitch-stream.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TwitchAPIService} from '../../../../@core/data/twitch-api.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'community-twitch-stream',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunityTwitchStreamComponent implements OnInit {
 
-  constructor() { }
+  streams: any[];
+  videos: any[];
+  queriedStreams: boolean;
+  queriedVideos: boolean;
+  constructor(private twitchAPI: TwitchAPIService) {
+    this.streams = [];
+    this.videos = [];
+    this.queriedStreams = false;
+    this.queriedVideos = false;
+  }
 
   ngOnInit() {
+    this.twitchAPI.getGameStreams()
+      .pipe(finalize(() => this.queriedStreams = true))
+      .subscribe(resp => {
+        this.streams = resp.data;
+      });
+    this.twitchAPI.getGameVideos()
+      .pipe(finalize(() => this.queriedVideos = true))
+      .subscribe(resp => {
+        this.videos = resp.data;
+      });
   }
 
 }
