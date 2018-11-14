fffae8c3d5c98ce02445d2cb4aa00190d85ed4ea
Gocnak <Gocnak@users.noreply.github.com>
2018-11-14 14:50:01 -0500

    Fix map library layout

--- client/src/app/pages/dashboard/maps/map-library/map-library.component.ts
+++ client/src/app/pages/dashboard/maps/map-library/map-library.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {LocalUserService} from '../../../../@core/data/local-user.service';
 import {MapLibraryEntry} from '../../../../@core/models/map-library-entry';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'map-library',
@@ -10,11 +11,16 @@ import {MapLibraryEntry} from '../../../../@core/models/map-library-entry';
 export class MapLibraryComponent implements OnInit {
 
   entries: MapLibraryEntry[];
+  sentRequest: boolean;
   constructor(private locUsrService: LocalUserService) {
+    this.entries = [];
+    this.sentRequest = false;
   }
 
   ngOnInit() {
-    this.locUsrService.getMapLibrary().subscribe(resp => {
+    this.locUsrService.getMapLibrary()
+      .pipe(finalize(() => this.sentRequest = true))
+      .subscribe(resp => {
       this.entries = resp.entries;
     });
   }
