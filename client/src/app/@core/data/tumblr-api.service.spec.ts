31b470ada15ce87d74d6fd8924dbc0aef12dfb3e
ssyasoni <ssyasoni@oakland.edu>
2018-11-17 11:11:01 -0500

    Unit Test ActivityService

---
+++ client/src/app/@core/data/tumblr-api.service.spec.ts
@@ -0,0 +1,23 @@
+import {of} from 'rxjs';
+import {TumblrAPIService} from './tumblr-api.service';
+
+let httpClientSpy: { get: jasmine.Spy };
+let tumblrAPIService: TumblrAPIService;
+
+describe('TumblrAPIService', () => {
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    tumblrAPIService = new TumblrAPIService(<any> httpClientSpy);
+  });
+
+  describe('Unit Tests', () => {
+    it('#getRecentBlogPosts() should return followed recent posts', () => {
+      httpClientSpy.get.and.returnValue(of(tumblrAPIService));
+      tumblrAPIService.getRecentBlogPosts().subscribe(value =>
+          expect(value).toEqual(tumblrAPIService, 'expected activity'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+  });
+});
