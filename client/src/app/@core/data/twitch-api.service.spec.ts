a374cf43b3a1469b4ad189bc9d8bc83aaf0a8985
ssyasoni <ssyasoni@oakland.edu>
2018-11-17 11:11:01 -0500

    Unit Test ActivityService

---
+++ client/src/app/@core/data/twitch-api.service.spec.ts
@@ -0,0 +1,39 @@
+import {of} from 'rxjs';
+import {TwitchAPIService} from './twitch-api.service';
+
+let httpClientSpy: { get: jasmine.Spy };
+let twitchAPIService: TwitchAPIService;
+
+describe('ActivityService', () => {
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    twitchAPIService = new TwitchAPIService(<any> httpClientSpy);
+  });
+
+  describe('Unit Tests', () => {
+    it('#getGameStreams() should return streams', () => {
+      httpClientSpy.get.and.returnValue(of(twitchAPIService));
+      twitchAPIService.getGameStreams().subscribe(value =>
+          expect(value).toEqual(twitchAPIService, 'expected activity'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#isUserLive() should return live user', () => {
+      httpClientSpy.get.and.returnValue(of(twitchAPIService));
+      twitchAPIService.isUserLive('userID').subscribe(value =>
+          expect(value).toEqual(twitchAPIService, 'expected activity'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#getGameVideos() should return videos', () => {
+      httpClientSpy.get.and.returnValue(of(twitchAPIService));
+      twitchAPIService.getGameVideos().subscribe(value =>
+          expect(value).toEqual(twitchAPIService, 'expected activity'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+  });
+});
