41c005ee333fd8f03edeefbfa685755e34cbd3a1
lubna Isaac <lisaac@oakland.edu>
2018-11-17 14:53:26 -0500

    Stats - Filter

---
+++ client/src/app/pages/dashboard/stats/global-stats/global-stats-filter/global-stats-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'global-stats-filter',
+  templateUrl: './global-stats-filter.component.html',
+  styleUrls: ['./global-stats-filter.component.scss'],
+})
+export class GlobalStatsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
