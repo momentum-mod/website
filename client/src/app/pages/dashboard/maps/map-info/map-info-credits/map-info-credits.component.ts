f12c68a9cdc5f4ad6d358e138dad3ca898e600fe
lubna Isaac <lisaac@oakland.edu>
2018-11-24 19:46:52 -0500

    Map Info and leaderboard filter redesign

---
+++ client/src/app/pages/dashboard/maps/map-info/map-info-credits/map-info-credits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'map-info-credits',
+  templateUrl: './map-info-credits.component.html',
+  styleUrls: ['./map-info-credits.component.scss'],
+})
+export class MapInfoCreditsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/pages/dashboard/maps/map-info/map-info-description/map-info-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'map-info-description',
+  templateUrl: './map-info-description.component.html',
+  styleUrls: ['./map-info-description.component.scss'],
+})
+export class MapInfoDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
