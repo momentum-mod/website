1c24ddf591d1c936295a15c23a9585f4392c775a
Corey Piasecki <faulk_128@hotmail.com>
2018-11-27 23:05:45 -0500

    Increased profile bio & map description DB length cap

--- server/src/models/db/map-info.js
+++ server/src/models/db/map-info.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, type) => {
 			autoIncrement: true
 		},
 		avatarURL: type.STRING,
-		description: type.STRING,
+		description: type.STRING(1000),
 		numBonuses: type.INTEGER,
 		numCheckpoints: type.INTEGER,
 		numStages: type.INTEGER,
--- server/src/models/db/profile.js
+++ server/src/models/db/profile.js
@@ -4,6 +4,6 @@ module.exports = (sequelize, type) => {
 	return sequelize.define('profile', {
 		alias: type.STRING,
 		avatarURL: type.STRING,
-		bio: type.STRING,
+		bio: type.STRING(1000),
 	})
 };
