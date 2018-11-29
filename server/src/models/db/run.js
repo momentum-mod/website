fa5d2a46a384d26e99d138785f99d3644bd703c1
Gocnak <Gocnak@users.noreply.github.com>
2018-11-29 05:05:37 -0500

    Fix tickrate for run submission
    Case sensitivity yay

--- server/src/models/db/run.js
+++ server/src/models/db/run.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, type) => {
 			autoIncrement: true
 		},
 		isPersonalBest: type.BOOLEAN,
-		tickrate: type.SMALLINT.UNSIGNED,
+		tickRate: type.SMALLINT.UNSIGNED,
 		dateAchieved: type.DATE,
 		time: type.DOUBLE.UNSIGNED, // TODO: investigate decimal, float and double (got weird up results with float)
 		flags: type.INTEGER.UNSIGNED,
