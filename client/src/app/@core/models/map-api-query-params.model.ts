ac52fb267ae79c1ec25af55681fcbf7af660b522
Corey Piasecki <faulk_128@hotmail.com>
2018-12-02 15:58:58 -0500

    Working browse maps search
    Fixes #145

---
+++ client/src/app/@core/models/map-api-query-params.model.ts
@@ -0,0 +1,9 @@
+export interface MapAPIQueryParams {
+  limit?: number;
+  offset?: number;
+  search?: string;
+  submitterID?: string;
+  expand?: string;
+  status?: string;
+  priority?: boolean;
+}
