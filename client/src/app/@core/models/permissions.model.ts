f173622263f877937b4e8c11af6f4d37fb148ed6
Gocnak <Gocnak@users.noreply.github.com>
2018-12-04 03:26:14 -0500

    Add verified permission
    Added new badges by Trazox

--- client/src/app/@core/models/permissions.model.ts
+++ client/src/app/@core/models/permissions.model.ts
@@ -1,9 +1,10 @@
 export enum Permission {
-  MAPPER = 1 << 0,
-  MODERATOR = 1 << 1,
-  ADMIN = 1 << 2,
-  BANNED_LEADERBOARDS = 1 << 3,
-  BANNED_ALIAS = 1 << 4,
-  BANNED_AVATAR = 1 << 5,
-  BANNED_BIO = 1 << 6,
+  VERIFIED = 1 << 0,
+  MAPPER = 1 << 1,
+  MODERATOR = 1 << 2,
+  ADMIN = 1 << 3,
+  BANNED_LEADERBOARDS = 1 << 4,
+  BANNED_ALIAS = 1 << 5,
+  BANNED_AVATAR = 1 << 6,
+  BANNED_BIO = 1 << 7,
 }
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -10,13 +10,14 @@ const { sequelize, Op, User, UserAuth, UserStats, Profile, UserFollows, Notifica
 module.exports = {
 
 	Permission: Object.freeze({
-		MAPPER: 1 << 0,
-		MODERATOR: 1 << 1,
-		ADMIN: 1 << 2,
-		BANNED_LEADERBOARDS: 1 << 3,
-		BANNED_ALIAS: 1 << 4,
-		BANNED_AVATAR: 1 << 5,
-		BANNED_BIO: 1 << 6,
+		VERIFIED: 1 << 0,
+		MAPPER: 1 << 1,
+		MODERATOR: 1 << 2,
+		ADMIN: 1 << 3,
+		BANNED_LEADERBOARDS: 1 << 4,
+		BANNED_ALIAS: 1 << 5,
+		BANNED_AVATAR: 1 << 6,
+		BANNED_BIO: 1 << 7,
 	}),
 
 	updateSteamInfo: (usr, newProfile) => {
