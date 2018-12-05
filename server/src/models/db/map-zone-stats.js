c17e480c8e77914e8b47b56ae2cd1216a1acf65f
Gocnak <Gocnak@users.noreply.github.com>
2018-12-05 18:41:40 -0500

    Fill in map zone stats
    Closes #175

--- server/src/models/db/map-zone-stats.js
+++ server/src/models/db/map-zone-stats.js
@@ -2,8 +2,65 @@
 
 module.exports = (sequelize, type) => {
 	return sequelize.define('mapZoneStats', {
-		zoneNum: type.INTEGER.UNSIGNED,
-		timePlayed: type.BIGINT.UNSIGNED,
-		// TODO finishme!
+		zoneNum: {
+			type: type.SMALLINT.UNSIGNED,
+			defaultValue: 0,
+		},
+		totalJumps: {
+			type: type.INTEGER.UNSIGNED,
+			defaultValue: 0,
+		},
+		totalStrafes: {
+			type: type.INTEGER.UNSIGNED,
+			defaultValue: 0,
+		},
+		totalTime: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgStrafeSync: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgStrafeSync2: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgEnterTime: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVel3D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVel2D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelMax3D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelMax2D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelEnter3D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelEnter2D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelExit3D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
+		avgVelExit2D: {
+			type: type.FLOAT,
+			defaultValue: 0.0,
+		},
 	})
 };
