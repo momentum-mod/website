0fbdd9c6a92e486f7b72ba2531922a6a43f1b024
Gocnak <Gocnak@users.noreply.github.com>
2018-12-10 01:57:55 -0500

    Delete unused files and disassociate from ngx-admin
    Closes #168
    Added MIT license to entire project

--- client/src/app/pages/dashboard/dashboard.module.ts
+++ client/src/app/pages/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NgxEchartsModule} from 'ngx-echarts';
-import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 import {ThemeModule} from '../../@theme/theme.module';
 import {DashboardComponent} from './dashboard.component';
@@ -9,7 +8,6 @@ import {DashboardComponent} from './dashboard.component';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardHomeComponent} from './home/dashboard-home.component';
 import {NotFoundModule} from '../not-found/not-found.module';
-import {SmartTableModule} from './smart-table/smart-table.module';
 import {ToasterModule} from 'angular2-toaster';
 import {MarkdownModule} from 'ngx-markdown';
 import {NbAccordionModule, NbDialogModule} from '@nebular/theme';
@@ -25,8 +23,6 @@ import {DisqusModule} from 'ngx-disqus';
     ThemeModule,
     NbAccordionModule,
     NgxEchartsModule,
-    SmartTableModule,
-    Ng2SmartTableModule,
     NbDialogModule.forChild(),
     ToasterModule.forChild(),
     MarkdownModule.forChild(),
