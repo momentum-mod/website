a25d8400ec29726fa4d700dbff84cd6c3a44f2de
Gocnak <Gocnak@users.noreply.github.com>
2018-12-10 20:20:16 -0500

    Fix map-leaderboard component tests
    Closes #181

--- client/src/app/pages/dashboard/maps/map-info/map-leaderboard/map-leaderboard.component.spec.ts
+++ client/src/app/pages/dashboard/maps/map-info/map-leaderboard/map-leaderboard.component.spec.ts
@@ -1,7 +1,11 @@
-/*
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapLeaderboardComponent } from './map-leaderboard.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NbUserModule} from '@nebular/theme';
+import {NumberTrimPipe} from '../../../../../@theme/pipes';
+import {TimeAgoPipe} from 'time-ago-pipe';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('MapLeaderboardComponent', () => {
   let component: MapLeaderboardComponent;
@@ -9,7 +13,12 @@ describe('MapLeaderboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapLeaderboardComponent ],
+      imports: [
+        NbUserModule,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule,
+      ],
+      declarations: [ MapLeaderboardComponent, NumberTrimPipe, TimeAgoPipe ],
     })
     .compileComponents();
   }));
@@ -24,6 +33,3 @@ describe('MapLeaderboardComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
-TODO: Fix tests
-*/
