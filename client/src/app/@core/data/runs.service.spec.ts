a7c9dc1d8e755df1e951eac68d54f58f4fde64f2
Gocnak <Gocnak@users.noreply.github.com>
2018-12-10 20:13:07 -0500

    Add simple runs service unit tests
    Closes #173

--- client/src/app/@core/data/runs.service.spec.ts
+++ client/src/app/@core/data/runs.service.spec.ts
@@ -1,13 +1,35 @@
-// import { TestBed } from '@angular/core/testing';
-//
-// import { RunsService } from './runs.service';
-//
-// describe('RunsService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
-//
-//   it('should be created', () => {
-//     const service: RunsService = TestBed.get(RunsService);
-//     expect(service).toBeTruthy();
-//   });
-// });
-// TODO: Implement tests
+import {of} from 'rxjs';
+import {RunsService} from './runs.service';
+
+let httpClientSpy: { get: jasmine.Spy };
+let runsServiceMock: RunsService;
+
+describe('RunsService', () => {
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    runsServiceMock = new RunsService(<any>httpClientSpy);
+  });
+
+  describe('Unit Tests', () => {
+    it('#getMapRuns() should return map runs', () => {
+      httpClientSpy.get.and.returnValue(of(runsServiceMock));
+      runsServiceMock.getMapRuns(12).subscribe(value =>
+          expect(value).toEqual(runsServiceMock, 'expected runs'),
+        fail,
+      );
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#getRun should return run info', () => {
+      httpClientSpy.get.and.returnValue(of(runsServiceMock));
+      runsServiceMock.getRun('125').subscribe(value =>
+        expect(value).toEqual(runsServiceMock, 'expected run'), fail);
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+    it('#getRuns should return run info', () => {
+      httpClientSpy.get.and.returnValue(of(runsServiceMock));
+      runsServiceMock.getRuns().subscribe(value =>
+        expect(value).toEqual(runsServiceMock, 'expected runs'), fail);
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+  });
+});
