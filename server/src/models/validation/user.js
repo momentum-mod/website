919a902d7274a13a466f55f05fd4a7e42e007773
Gocnak <Gocnak@users.noreply.github.com>
2019-01-24 02:12:26 -0500

    Fix user ID validation
    We'll be having low-digit users with the ghost user (#202 ) feature

--- server/src/models/validation/user.js
+++ server/src/models/validation/user.js
@@ -2,7 +2,7 @@
 const Joi = require('joi');
 
 module.exports = {
-	id: Joi.string().regex(/^[0-9]{17}$/),
+	id: Joi.string().regex(/^[0-9]{1,17}$/),
 	alias: Joi.string().min(1).max(32),
 	permissions: Joi.number().integer().min(0),
 };
