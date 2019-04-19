53d6af3ac2f911e25a04bd48ba8ba33f1494fade
Gocnak <Gocnak@users.noreply.github.com>
2019-04-18 23:25:39 -0400

    Use default root user for MySQL for Travis CI
    See if this helps

--- server/setup-db.sh
+++ server/setup-db.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ev
-mysql -e "CREATE DATABASE momentum_test;"
-mysql -e "CREATE USER mom_test@localhost;"
-mysql -e "GRANT ALL PRIVILEGES ON momentum_test.* TO mom_test@localhost;"
+mysql -u "root" --password="" -e "CREATE DATABASE momentum_test;"
+mysql -u "root" --password="" -e "CREATE USER mom_test@localhost;"
+mysql -u "root" --password="" -e "GRANT ALL PRIVILEGES ON momentum_test.* TO mom_test@localhost;"
