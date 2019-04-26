8f047cb5dfdfc7e62c9ee4b76daa70b592b2f9ef
Gocnak <Gocnak@users.noreply.github.com>
2019-04-26 00:36:01 -0400

    Improve displaying run time
    Closes #315
    Follows the same display logic found in game. Shoutouts to javascript not having a sprintf function...

--- client/src/app/@theme/pipes/timing.pipe.ts
+++ client/src/app/@theme/pipes/timing.pipe.ts
@@ -2,17 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'timing' })
 export class TimingPipe implements PipeTransform {
-  transform(time: number): string {
-    if (time) {
-      const minutes = Math.floor(time / 60);
-      const seconds = Math.floor(time % 60);
-      return `${this.initZero(minutes)}${minutes}:${this.initZero(seconds)}${seconds}`;
-    }
-
-    return '00:00';
+  private static padNum(num: number): string {
+    return num.toString().padStart(2, '0');
   }
-
-  private initZero(time: number): string {
-    return time < 10 ? '0' : '';
+  // Input is given as seconds
+  transform(input: number): string {
+    const hours = Math.trunc(input / 3600);
+    const minutes = Math.trunc((input / 60) % 60);
+    const seconds = Math.trunc(input % 60);
+    const millis = input.toFixed(2).toString().split('.')[1];
+    if (hours > 0) {
+      return `${TimingPipe.padNum(hours)}:${TimingPipe.padNum(minutes)}:${TimingPipe.padNum(seconds)}`;
+    } else if (minutes > 0) {
+      return `${TimingPipe.padNum(minutes)}:${TimingPipe.padNum(seconds)}.${millis}`;
+    } else {
+      return `${seconds}.${millis}`;
+    }
   }
 }
