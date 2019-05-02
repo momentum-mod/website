d9498876eaf6129626cc7d0f46a205a8279b80f9
Gocnak <Gocnak@users.noreply.github.com>
2019-05-02 05:16:39 -0400

    Update the main page
    Updated with more credits, text, and utilizes the nebular layout
    Added Go to Dashboard button if already logged in
    Fixed scrollbar disappearing upon logout
    Fixed bug that caused header/sidebar to levitate when opening an nb-select while scrolled down at all

--- client/src/app/pages/main/header/header.component.ts
+++ client/src/app/pages/main/header/header.component.ts
@@ -1,12 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'ngx-header',
+  selector: 'main-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  constructor() {
+    this.isLoggedIn = false;
+  }
 
+  ngOnInit(): void {
+    if (localStorage.getItem('user')) {
+      this.isLoggedIn = true;
+    }
+  }
 }
