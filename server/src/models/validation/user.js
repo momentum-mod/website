e4d6d4ae97f2bda4c92ff8b7268efcfd5a429ed7
sneak_it <darkblackhawk@gmail.com>
2019-05-18 19:18:12 -0400

    update validator alias max to 64

--- server/src/models/validation/user.js
+++ server/src/models/validation/user.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 module.exports = {
 	id: Joi.number().integer().positive(),
 	steamID: Joi.string().regex(/^[0-9]{1,20}$/),
-	alias: Joi.string().min(1).max(32),
+	alias: Joi.string().min(1).max(64),
 	roles: Joi.number().integer().min(0),
 	bans: Joi.number().integer().min(0),
 };
