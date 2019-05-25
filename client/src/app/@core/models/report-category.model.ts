1a52c7ceffe13fcb059f45c1fd68dc00a3767892
Corey28 <faulk_128@hotmail.com>
2019-05-24 23:05:39 -0400

    Temp fix for nb-select bug when nb-option value is 0
    There is currently a little nb-select bug which has been fixed, but not released yet - see the issue here: https://github.com/akveo/nebular/issues/1235
    Be aware of this issue when using the nb-select component until the fix is released
    Closes #326

--- client/src/app/@core/models/report-category.model.ts
+++ client/src/app/@core/models/report-category.model.ts
@@ -2,5 +2,5 @@ export enum ReportCategory {
   INAPPROPRIATE_CONTENT = 1,
   PLAGIARISM = 2,
   SPAM = 3,
-  OTHER = 0,
+  OTHER = '0', // TODO: Change to num type when nb-select fix is released - https://github.com/akveo/nebular/pull/1282
 }
