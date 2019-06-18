6ac6b66b9901e67e9cdd8f20a3e3404e7202aa8e
Gocnak <Gocnak@users.noreply.github.com>
2019-06-18 01:03:49 -0400

    We can use 0-values now for nb-select
    Fixed in Nebular 3.6 and 4.0
    Properly fixes #326

--- client/src/app/@core/models/map-type.model.ts
+++ client/src/app/@core/models/map-type.model.ts
@@ -1,5 +1,5 @@
 export enum MomentumMapType {
-  UNKNOWN = '0', // TODO: Change to num type when nb-select fix is released - https://github.com/akveo/nebular/pull/1282
+  UNKNOWN = 0,
   SURF = 1,
   BHOP = 2,
   KZ = 3,
--- client/src/app/@core/models/report-category.model.ts
+++ client/src/app/@core/models/report-category.model.ts
@@ -1,6 +1,6 @@
 export enum ReportCategory {
+  OTHER = 0,
   INAPPROPRIATE_CONTENT = 1,
   PLAGIARISM = 2,
   SPAM = 3,
-  OTHER = '0', // TODO: Change to num type when nb-select fix is released - https://github.com/akveo/nebular/pull/1282
 }
