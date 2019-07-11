924db0fb6e2f074a6a5112f54f4835c908e37bcd
MantasAD <mantasadomav@gmail.com>
2019-07-11 18:27:44 +0300

    Fix validation error

--- server/src/models/validation/map-info.js
+++ server/src/models/validation/map-info.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 
 module.exports = {
 	description: Joi.string().max(1000),
-	youtubeID: Joi.string().max(11),
+	youtubeID: Joi.string().max(11).empty(null),
 	numTracks: Joi.number().integer().min(1).max(64),
 	creationDate: Joi.date().iso(),
 };
