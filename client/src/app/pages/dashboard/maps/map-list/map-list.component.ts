741953dc9228a4bcb316b2a96e60e1943f7870a9
MantasAD <mantasadomav@gmail.com>
2019-08-17 01:19:07 +0300

    Fix map list search not changing page

--- client/src/app/pages/dashboard/maps/map-list/map-list.component.ts
+++ client/src/app/pages/dashboard/maps/map-list/map-list.component.ts
@@ -53,7 +53,11 @@ export class MapListComponent implements OnInit {
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((paramMap: ParamMap) => {
+      this.searchOptions.setValue({search: paramMap.get('search') || ''});
       this.currentPage = +paramMap.get('page') || 1;
+      const count = this.pageLimit * this.currentPage;
+      if (count > this.mapCount)
+        this.mapCount = count;
       this.loadMaps();
     });
   }
@@ -103,6 +107,7 @@ export class MapListComponent implements OnInit {
       {
         relativeTo: this.route,
         queryParams: {
+          search: this.searchOptions.value.search || undefined,
           page: pageNum === 1 ? undefined : pageNum,
         },
         queryParamsHandling: 'merge',
