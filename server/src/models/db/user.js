f23c8375b6aba5a9ce9031e3798743d1ecb8df6b
Corey28 <faulk_128@hotmail.com>
2019-08-25 13:25:05 -0400

    Fix whitespace inconsistencies

--- server/src/models/db/user.js
+++ server/src/models/db/user.js
@@ -15,9 +15,9 @@ module.exports = (sequelize, type) => {
 		},
 		alias: type.STRING(64),
 		aliasLocked: { // whether or not on to update alias to match latest steam alias on login
-            type: type.BOOLEAN,
-            defaultValue: false,
-        },
+			type: type.BOOLEAN,
+			defaultValue: false,
+		},
 		avatar: {
 			type: type.STRING,
 			get() {}, // hidden (use avatarURL)
@@ -28,7 +28,7 @@ module.exports = (sequelize, type) => {
 				const bans = this.getDataValue('bans');
 				const isAvatarBanned = bans & 1 << 2; // TODO: Refactor however needed to use Ban 'enum' (cyclic dep issue occurs when requiring user model in this file)
 				if (isAvatarBanned) {
-                    return config.baseUrl + '/assets/images/blank_avatar.jpg';
+					return config.baseUrl + '/assets/images/blank_avatar.jpg';
 				} else {
 					const avatar = this.getDataValue('avatar');
 					return avatar ? `https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/${avatar}` : null;
