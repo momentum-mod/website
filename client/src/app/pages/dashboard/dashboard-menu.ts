56ec551103a1bda53aee452dd2f616059b35b2f8
MantasAD <mantasadomav@gmail.com>
2019-09-03 17:51:28 +0300

    Small style changes
    Made background fixed
    Made scrollbar visible all the time to prevent page content jumping
    Changed black color to a little bit lighter color
    Changed blue shadow to small black shadow
    Changed sidebar icons to outline variants
    Added icons to maps menu submenus
    Added theme-color for mobile browsers to use
    Also fixed loading spinner not disappearing from some pages

--- client/src/app/pages/dashboard/dashboard-menu.ts
+++ client/src/app/pages/dashboard/dashboard-menu.ts
@@ -4,35 +4,36 @@ import {Role} from '../../@core/models/role.model';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Dashboard',
-    icon: 'home',
+    icon: 'home-outline',
     link: '/dashboard',
     home: true,
   },
   {
     title: 'Community',
-    icon: 'people',
+    icon: 'people-outline',
     children: [
       {
-        'title': 'Momentum Official',
-        'link': '/dashboard/community/news',
+        title: 'Momentum Official',
+        link: '/dashboard/community/news',
       },
       {
-        'title': 'Twitch',
-        'link': '/dashboard/community/twitch',
+        title: 'Twitch',
+        link: '/dashboard/community/twitch',
       },
       {
-        'title': 'Recent Activity',
-        'link': '/dashboard/community/activity',
+        title: 'Recent Activity',
+        link: '/dashboard/community/activity',
       },
     ],
   },
   {
     title: 'Maps',
-    icon: 'cloud-download',
+    icon: 'map-outline',
     link: '/dashboard/maps',
     children: [
       {
         title: 'Map Uploads',
+        icon: 'upload-outline',
         link: '/dashboard/maps/uploads',
         pathMatch: 'partial',
         data: {
@@ -44,14 +45,17 @@ export const MENU_ITEMS: NbMenuItem[] = [
       },
       {
         title: 'My Library',
+        icon: 'hard-drive-outline',
         link: '/dashboard/maps/library',
       },
       {
         title: 'Favorite Maps',
+        icon: 'star-outline',
         link: '/dashboard/maps/favorites',
       },
       {
         title: 'Browse Maps',
+        icon: 'cloud-download-outline',
         pathMatch: 'partial',
         link: '/dashboard/maps',
       },
@@ -59,12 +63,12 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Statistics',
-    icon: 'bar-chart',
+    icon: 'bar-chart-outline',
     link: '/dashboard/stats',
   },
   {
     title: 'Administration',
-    icon: 'lock',
+    icon: 'lock-outline',
     link: '/dashboard/admin',
     data: {
       roles: [
