03e0c2f9ac3c565fec92f6b00c9b606c60dbb639
MantasAD <mantasadomav@gmail.com>
2019-10-02 03:21:32 +0300

    Some layout changes to main page
    Changed about section
    Added gamemodes component

---
+++ client/src/app/pages/main/gamemodes/gamemodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-gamemodes',
+  templateUrl: './gamemodes.component.html',
+  styleUrls: ['./gamemodes.component.scss'],
+})
+export class GamemodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
