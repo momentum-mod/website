d6c5f7209010838e4db152a39bf19aba06e76744
Gocnak <Gocnak@users.noreply.github.com>
2019-10-08 23:42:39 -0400

    Show info dialog upon trying to sign in
    This will help convey the fact that signing into the site will not be doing anything without a key

--- client/src/app/pages/main/header/header.component.ts
+++ client/src/app/pages/main/header/header.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {NbDialogService} from '@nebular/theme';
+import {ConfirmDialogComponent} from '../../../@theme/components/confirm-dialog/confirm-dialog.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'main-header',
@@ -8,7 +11,8 @@ import {Component, OnInit} from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   isLoggedIn: boolean;
-  constructor() {
+  constructor(private dialogService: NbDialogService,
+              private routerService: Router) {
     this.isLoggedIn = false;
   }
 
@@ -16,5 +20,23 @@ export class HeaderComponent implements OnInit {
     if (localStorage.getItem('user')) {
       this.isLoggedIn = true;
     }
+  }
+
+  showPrivateAlphaWarning(): void {
+    this.dialogService.open(ConfirmDialogComponent, {
+      context: {
+        title: 'Important Note!',
+        message: 'Momentum Mod is currently in a private Alpha until its public 1.0.0 release.\n\n' +
+          'Logging into the site works for everyone, but the features found on it will only' +
+          ' serve a purpose for those with a key.\n\n' +
+          'Keys are given to those who help contribute to the game, so consider ' +
+          'joining the discord and letting the team know if you can help!\n\n' +
+          'Do you understand and want to proceed?',
+      },
+    }).onClose.subscribe((agree) => {
+      if (agree) {
+        this.routerService.navigate(['/dashboard']);
+      }
+    });
   }
 }
