70f629340b0ebcf6957d36af8934f329a9af6033
Gocnak <Gocnak@users.noreply.github.com>
2019-11-10 17:12:48 -0500

    Fix zone properties improperly saving
    Closes #362

--- client/src/app/pages/dashboard/maps/upload-form/map-upload-form.component.ts
+++ client/src/app/pages/dashboard/maps/upload-form/map-upload-form.component.ts
@@ -172,7 +172,7 @@ export class MapUploadFormComponent implements OnInit, AfterViewInit {
               pointsHeight: triggerObj.pointsHeight,
             };
             if (triggerObj.zoneProps)
-              zoneMdlTrigger.zoneProps = {properties: triggerObj.zoneProps};
+              zoneMdlTrigger.zoneProps = {properties: triggerObj.zoneProps.properties};
             zoneMdl.triggers.push(zoneMdlTrigger);
           }
         }
