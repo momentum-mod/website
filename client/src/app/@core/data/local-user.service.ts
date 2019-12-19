53964bb92ca5f33a11f961cf587480f2ab9c6f43
murglegurgle <yetterbrian@gmail.com>
2019-12-19 18:28:59 -0500

    Added frontend logic to handle map notification info and methods in user service to call api

--- client/src/app/@core/data/local-user.service.ts
+++ client/src/app/@core/data/local-user.service.ts
@@ -14,6 +14,8 @@ import {MapFavorite} from '../models/map-favorite.model';
 import {MomentumMaps} from '../models/momentum-maps.model';
 import {MapSubmissionSummaryElement} from '../models/map-submission-summary-element.model';
 import {UserCredits} from '../models/user-credits.model';
+import {MapNotify} from '../models/map-notify.model';
+
 
 @Injectable({
   providedIn: 'root',
@@ -150,6 +152,36 @@ export class LocalUserService {
     return this.http.delete('/api/user/maps/favorites/' + mapID);
   }
 
+    /**
+   * @param mapID Map to receive notifications for
+   * @param notifyOn The flags to notify the followee on
+   * @return Update map notifications
+   */
+
+  public updateMapNotify(mapID: number, notifyOn: number): Observable<any> {
+    return this.http.put('/api/user/maps/notifyMap/' + mapID, {
+      notifyOn: notifyOn,
+    });
+  }
+
+ /**
+   * @param mapID The map to check if notifications are enabled
+   * @return A json object with the potential map and the activity type for notificaions
+   */
+  public checkMapNotify(mapID: number): Observable<MapNotify> {
+    return this.http.get<MapNotify>('/api/user/maps/notifyMap/' + mapID);
+  }
+
+  /**
+   * @param mapID The map to disable notificaions for
+   * @return Notifications disabled for map
+   */
+  public disableMapNotify(mapID: number): Observable<any> {
+    return this.http.delete('/api/user/maps/notifyMap/' + mapID, {
+      responseType: 'text',
+    });
+  }
+  
   /**
    * @param options An object of options
    * @return A list of credits featuring the local user
