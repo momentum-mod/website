82eb369587a11cfb0d97267b61642b3dda43da4e
Patrick M (XB) <49064563+patrickmoc@users.noreply.github.com>
2020-03-22 19:01:26 -0300

    update gamemodes imports

--- client/src/app/pages/main/gamemodes/gamemodes.component.ts
+++ client/src/app/pages/main/gamemodes/gamemodes.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Gallery, GalleryItem, GalleryState } from '@ngx-gallery/core';
+import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { GameModeDetails } from '../../../@core/models/gamemode-details.model';
 
 @Component({
   selector: 'ngx-gamemodes',
