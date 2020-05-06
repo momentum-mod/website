6e5288e25d486f4d54e9b2a4f3224668dc9780c0
Luke Parker <lukemparker@outlook.com>
2020-05-06 12:03:27 +1000

    Mount persistent data on docker run command

--- runDocker.sh
+++ runDocker.sh
@@ -5,4 +5,11 @@ echo -e '-= Stopping MMoD Website Server Production Container =-\n'
 docker container stop mmod-website-server-production
 
 echo -e '-= Runnning the MMoD Website Server Image =-\n'
-docker run --network host --name "mmod-website-server-production" --env-file env-vars.list -d mmod-website-server-production
+docker run -v ./server/public/img/maps:/app/server/public/img/maps \
+           -v ./server/public/maps:/app/server/public/maps \
+           -v ./server/public/runs:/app/server/public/runs \
+           --network host \
+           --name "mmod-website-server-production" \
+           --env-file env-vars.list \
+           -d \
+           mmod-website-server-production
