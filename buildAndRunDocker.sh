28128c0617167a39941d3d25f0c738ea971055eb
Luke Parker <10430890+TheRealHona@users.noreply.github.com>
2020-05-13 09:03:30 +1000

    Move runDocker.sh to buildAndRunDocker.sh, and remove -production

--- buildAndRunDocker.sh
+++ buildAndRunDocker.sh
@@ -1,15 +1,15 @@
 echo -e '-= Building MMoD Website Server Docker Image from Dockerfile =-\n'
-docker build -t mmod-website-server-production -f ./Dockerfile .
+docker build -t mmod-website-server -f ./Dockerfile .
 
-echo -e '-= Stopping MMoD Website Server Production Container =-\n'
-docker container stop mmod-website-server-production
+echo -e '-= Stopping MMoD Website Server Container =-\n'
+docker container stop mmod-website-server
 
 echo -e '-= Runnning the MMoD Website Server Image =-\n'
 docker run -v ./server/public/img/maps:/app/server/public/img/maps \
            -v ./server/public/maps:/app/server/public/maps \
            -v ./server/public/runs:/app/server/public/runs \
            --network host \
-           --name "mmod-website-server-production" \
+           --name "mmod-website-server" \
            --env-file env-vars.list \
            -d \
-           mmod-website-server-production
+           mmod-website-server
