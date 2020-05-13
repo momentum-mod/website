bd7b46c5524ec376b4621547cd741cb32243bb58
Luke Parker <lukemparker@outlook.com>
2020-05-13 17:29:28 +1000

    Fix local build file with $PWD instead of .

--- buildAndRunDocker.sh
+++ buildAndRunDocker.sh
@@ -5,9 +5,9 @@ echo -e '-= Stopping MMoD Website Server Container =-\n'
 docker container stop mmod-website-server
 
 echo -e '-= Runnning the MMoD Website Server Image =-\n'
-docker run -v ./server/public/img/maps:/app/server/public/img/maps \
-           -v ./server/public/maps:/app/server/public/maps \
-           -v ./server/public/runs:/app/server/public/runs \
+docker run -v $PWD/server/public/img/maps:/app/server/public/img/maps \
+           -v $PWD/server/public/maps:/app/server/public/maps \
+           -v $PWD/server/public/runs:/app/server/public/runs \
            --network host \
            --name "mmod-website-server" \
            --env-file env-vars.list \
