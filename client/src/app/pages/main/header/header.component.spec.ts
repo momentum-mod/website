05537836bbc463b177e8680233cb512755626b92
Gocnak <Gocnak@users.noreply.github.com>
2020-06-28 18:40:36 -0400

    Move "Sign in" / "Go to Dashboard" to nav bar
    Making room for something else...

--- client/src/app/pages/main/header/header.component.spec.ts
+++ client/src/app/pages/main/header/header.component.spec.ts
@@ -1,25 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import {NbDialogConfig, NbDialogRef, NbDialogService} from '@nebular/theme';
-import {TemplateRef, Type} from '@angular/core';
-import {RouterTestingModule} from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-  let dialogDummy: Partial<NbDialogService>;
+
   beforeEach(async(() => {
-    dialogDummy = {
-      open<T>(content: Type<T> | TemplateRef<T>, userConfig?: Partial<NbDialogConfig<Partial<T> | string>>):
-        NbDialogRef<T> {
-        return null;
-      },
-    };
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
       declarations: [ HeaderComponent ],
-      providers: [ {provide: NbDialogService, useValue: dialogDummy } ],
     })
     .compileComponents();
   }));
