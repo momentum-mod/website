57ba36fcf35e659b10efe03df87f27a7e4c2013c
Gocnak <Gocnak@users.noreply.github.com>
2020-06-28 19:39:36 -0400

    Prefer nb-icon and fontawesome icons
    As updating ionicons was going to eventually need to change all of the existing icons anyways, we're going to consolidate down into nb-icon for mostly everything and use fontawesome for branding / icons not in nb-icons

--- client/src/app/@theme/components/activity/activity-content/activity-content.component.ts
+++ client/src/app/@theme/components/activity/activity-content/activity-content.component.ts
@@ -33,34 +33,34 @@ export class ActivityContentComponent implements OnInit {
       case Activity_Type.MAP_UPLOADED:
         this.actionText = ' uploaded a new ';
         this.eventText = 'map';
-        this.activityIcon = 'ion ion-map';
+        this.activityIcon = 'map-outline';
         this.eventColor = 'purple';
         break;
       case Activity_Type.MAP_APPROVED:
         this.actionText = ' added a new ';
         this.eventText = 'map';
-        this.activityIcon = 'ion ion-map';
+        this.activityIcon = 'map-outline';
         this.eventColor = 'green';
         this.eventLink = '/dashboard/maps/' + this.activity.data;
         break;
       case Activity_Type.WR_ACHIEVED:
         this.actionText = ' achieved a ';
         this.eventText = 'world record';
-        this.activityIcon = 'ion ion-android-globe';
+        this.activityIcon = 'globe-outline';
         this.eventColor = 'yellow';
         this.eventLink = '/dashboard/runs/' + this.activity.data;
         break;
       case Activity_Type.PB_ACHIEVED:
         this.actionText = ' achieved a ';
         this.eventText = 'personal best';
-        this.activityIcon = 'ion ion-trophy';
+        this.activityIcon = 'award-outline';
         this.eventColor = 'silver';
         this.eventLink = '/dashboard/runs/' + this.activity.data;
         break;
       case Activity_Type.USER_JOINED:
         this.actionText = ' has ';
         this.eventText = 'joined';
-        this.activityIcon = 'ion ion-android-contacts';
+        this.activityIcon = 'people-outline';
         this.eventColor = 'orange';
         break;
       default:
