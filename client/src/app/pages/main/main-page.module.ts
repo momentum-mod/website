11cc716709cbff203dc9b96b8dca61b39362f8fe
Gocnak <Gocnak@users.noreply.github.com>
2020-06-28 20:46:39 -0400

    Update links
    New discord invite link
    Important milestone link for the game also added to the main page / dashboard

--- client/src/app/pages/main/main-page.module.ts
+++ client/src/app/pages/main/main-page.module.ts
@@ -9,10 +9,11 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {GalleryModule} from '@ngx-gallery/core';
 import {NbButtonModule, NbLayoutModule} from '@nebular/theme';
-import { GamemodesComponent } from './gamemodes/gamemodes.component';
+import {GamemodesComponent} from './gamemodes/gamemodes.component';
+import {ThemeModule} from '../../@theme/theme.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NbLayoutModule, NbButtonModule, GalleryModule],
+  imports: [CommonModule, RouterModule, NbLayoutModule, NbButtonModule, GalleryModule, ThemeModule],
   declarations: [
     MainPageComponent,
     AboutComponent,
