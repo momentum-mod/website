1f7d77b9c87b2a828f19aaa98b13c74b09dc795e
Gocnak <Gocnak@users.noreply.github.com>
2020-06-28 20:21:06 -0400

    Remove dashboard footer component
    No longer needed nor used

--- client/src/app/@theme/components/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
-  templateUrl: './footer.component.html',
-})
-export class FooterComponent {
-}
--- client/src/app/@theme/components/index.ts
+++ client/src/app/@theme/components/index.ts
@@ -1,5 +1,4 @@
 export * from './header/header.component';
-export * from './footer/footer.component';
 export * from './search-input/search-input.component';
 export * from './search/search.component';
 export * from './notification/notification.component';
