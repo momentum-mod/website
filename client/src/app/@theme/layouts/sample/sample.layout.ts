5d743fe2b35650be0f3b7cc8e69f14d1ac4781f1
Gocnak <Gocnak@users.noreply.github.com>
2020-06-28 20:16:44 -0400

    Move sample layout template to HTML file

--- client/src/app/@theme/layouts/sample/sample.layout.ts
+++ client/src/app/@theme/layouts/sample/sample.layout.ts
@@ -3,27 +3,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'ngx-sample-layout',
   styleUrls: ['./sample.layout.scss'],
-  template: `
-    <nb-layout>
-      <nb-layout-header fixed>
-        <ngx-header></ngx-header>
-      </nb-layout-header>
-
-      <nb-sidebar class="menu-sidebar"
-                  tag="menu-sidebar"
-                  responsive
-                  [containerFixed]="false">
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
-
-      <nb-layout-column class="main-content">
-        <ng-content select="router-outlet"></ng-content>
-      </nb-layout-column>
-
-      <nb-layout-footer fixed>
-        <ngx-footer></ngx-footer>
-      </nb-layout-footer>
-    </nb-layout>
-  `,
+  templateUrl: './sample.layout.html',
 })
 export class SampleLayoutComponent {}
