768e933db0e7a42dd05f0789335319b696f1334c
Patrick M (XB) <49064563+patrickmoc@users.noreply.github.com>
2020-08-01 16:10:18 -0300

    add new gamemode section

--- client/src/app/pages/main/gamemodes/gamemodes.component.ts
+++ client/src/app/pages/main/gamemodes/gamemodes.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Gallery, GalleryItem, GalleryState } from '@ngx-gallery/core';
-import { map, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Component } from '@angular/core';
 import { GameModeDetails } from '../../../@core/models/gamemode-details.model';
 
 @Component({
@@ -9,133 +6,113 @@ import { GameModeDetails } from '../../../@core/models/gamemode-details.model';
   templateUrl: './gamemodes.component.html',
   styleUrls: ['./gamemodes.component.scss'],
 })
-export class GamemodesComponent implements OnInit, OnDestroy {
+export class GamemodesComponent {
 
-  private ngUnsub = new Subject();
-
-  currentGamemodesItems: GalleryItem[];
-  futureGamemodesItems: GalleryItem[];
-
-  currentGameModes: GameModeDetails[] = [
+  gameModes: GameModeDetails[] = [
     {
-      url: 'https://www.youtube.com/watch?v=UxQ0x-AiJOY',
+      url: '/assets/images/surf_loop.webm',
       modeTitle: 'Surf',
       modeDescription: 'Based off of the movement from Counter-Strike: Source, ' +
         'players \"surf\" by gliding along triangular prisms called ramps in order ' +
         'to complete maps.',
       imageUrl: '/assets/images/gamemode_surf.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_skillsurf.png',
+      isImplemented: true,
     },
     {
-      url: 'https://www.youtube.com/watch?v=8IvXWjjqHiA',
+      url: '/assets/images/bhop_loop.webm',
       modeTitle: 'Bunny Hop',
       modeDescription: 'Based off of the movement from Counter-Strike: Source, ' +
         'players repeatedly jump while strafing through the air to continuously ' +
         'gain speed to complete maps.',
       imageUrl: '/assets/images/gamemode_bhop.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_bhop.png',
+      isImplemented: true,
     },
     {
-      url: 'https://www.youtube.com/watch?v=TCDV5aZNYEY',
+      url: '',
       modeTitle: 'Rocket Jump',
       modeDescription: 'Based off of the Soldier class from Team Fortress 2, ' +
         'players shoot unidirectional, fixed-speed rockets and take advantage of the explosion ' +
         'they create to propel themselves throughout maps.',
       imageUrl: '/assets/images/gamemode_rj.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_rocketjump.png',
+      isImplemented: true,
     },
     {
-      url: 'https://www.youtube.com/watch?v=ubH5tw0ZLBc',
+      url: '',
       modeTitle: 'Sticky Jump',
       modeDescription: 'Based off of the Demoman class from Team Fortress 2, ' +
         'players shoot explosives that stick to surfaces and have control over ' +
         'their detonation which propels themselves throughout maps.',
       imageUrl: '/assets/images/gamemode_stickyjump.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_stickyjump.png',
+      isImplemented: true,
     },
     {
-      url: 'https://www.youtube.com/watch?v=8M_GbJSU1Rw',
+      url: '/assets/images/ahop_loop.webm',
       modeTitle: 'Accelerated Hop',
       modeDescription: 'Based off of the movement found in Half-Life 2, ' +
         'Accelerated Hop comes from Valve\'s attempt to remove bunnyhopping in an older version of the game. ' +
         'By either hopping completely backwards or holding/pressing the back movement key while hopping, ' +
         'players can gain extreme amounts of velocity.',
       imageUrl: '/assets/images/gamemode_ahop.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_ahop.png',
+      isImplemented: true,
     },
-  ];
-
-  futureGameModes: GameModeDetails[] = [
     {
-      url: 'https://www.youtube.com/watch?v=k9jAlYmNK5A',
+      url: '',
       modeTitle: 'Parkour',
       modeDescription: 'Based off of the movement from Titanfall 2, ' +
         'Parkour sees players wall-running, slide hopping, and preserving momentum ' +
         'throughout maps.',
       imageUrl: '/assets/images/gamemode_pk.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_parkour.png',
+      isImplemented: false,
     },
     {
-      url: 'https://www.youtube.com/watch?v=XYNRNrC5o5Q',
+      url: '',
       modeTitle: 'Conc',
       modeDescription: 'A classic game mode from the classic Team Fortress, concussion grenade boosting involves ' +
         'priming a concussion grenade to explode at just the right time to propel the player throughout the map.',
       imageUrl: '/assets/images/gamemode_conc.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_conc.png',
+      isImplemented: false,
     },
     {
-      url: 'https://www.youtube.com/watch?v=J6kYb_O-XFk',
+      url: '',
       modeTitle: 'Climb (KZ/XC)',
       modeDescription: 'Players climb through various obstacles with well-timed jumps and strafes, ' +
         'progressing through maps to reach the end.',
       imageUrl: '/assets/images/gamemode_kz.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_climb.png',
+      isImplemented: false,
     },
     {
-      url: 'https://www.youtube.com/watch?v=Bcl27Y8pk4A',
+      url: '',
       modeTitle: 'Tricksurf',
       modeDescription: 'A spinoff mode formed from the Surf game mode, ' +
       'players can create and complete sequences of locations to surf to, ' +
       'known as tricks, for points and bragging rights.',
       imageUrl: '/assets/images/gamemode_tricksurf.jpg',
+      iconUrl: '/assets/images/gamemode_icons/site_tricksurf.png',
+      isImplemented: false,
     },
   ];
 
   currentGameMode?: GameModeDetails;
-  futureGameMode?: GameModeDetails;
 
-  constructor(public gallery: Gallery) { }
+  gameModeIndex: number;
 
-  ngOnInit() {
-    // Map images for gallery
-    this.currentGamemodesItems = this.currentGameModes.map(
-        item => new CustomGalleryItem({ src: item.imageUrl, thumb: item.imageUrl, youtubeLink: item.url }),
-      );
-    this.futureGamemodesItems = this.futureGameModes.map(
-        item => new CustomGalleryItem({ src: item.imageUrl, thumb: item.imageUrl, youtubeLink: item.url }),
-      );
-
-    // Get gallery references
-    const currentGalleryRef = this.gallery.ref('current-gamemodes');
-    const futureGalleryRef = this.gallery.ref('future-gamemodes');
-
-    // Init galleries
-    this.currentGameMode = this.currentGameModes[0];
-    this.futureGameMode = this.futureGameModes[0];
-
-    // Subscribe to gallery index changed event
-    currentGalleryRef.indexChanged.pipe(takeUntil(this.ngUnsub)).subscribe((galleryState: GalleryState) => {
-        this.currentGameMode = this.currentGameModes[galleryState.currIndex];
-    });
-    futureGalleryRef.indexChanged.pipe(takeUntil(this.ngUnsub)).subscribe((galleryState: GalleryState) => {
-      this.futureGameMode = this.futureGameModes[galleryState.currIndex];
-    });
+  constructor() {
+    this.currentGameMode = this.gameModes[0];
+    this.gameModeIndex = 0;
   }
 
-  ngOnDestroy(): void {
-    this.ngUnsub.next();
-    this.ngUnsub.complete();
-  }
-}
-
-export class CustomGalleryItem implements GalleryItem {
-
-  readonly type = 'custom-item';
-  readonly data: any;
-
-  constructor(data: any) {
-    this.data = data;
+  setCurrentGamemode(index: number) {
+    if (index < this.gameModes.length && index >= 0) {
+      this.currentGameMode = this.gameModes[index];
+      this.gameModeIndex = index;
+    }
   }
 }
