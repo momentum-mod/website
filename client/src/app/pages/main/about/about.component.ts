96ab9387b49d85d7aaddd4de42be1fcebd287517
Gocnak <Gocnak@users.noreply.github.com>
2020-08-05 15:52:34 -0400

    Use content load deferring in main page
    Defer the "What is Momentum Mod" video load and the gamemode section load until they are scrolled to for more optimized main page load

--- client/src/app/pages/main/about/about.component.ts
+++ client/src/app/pages/main/about/about.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  showAboutVideo: boolean;
+  constructor() {
+    this.showAboutVideo = false;
+  }
 
   ngOnInit() {
   }
