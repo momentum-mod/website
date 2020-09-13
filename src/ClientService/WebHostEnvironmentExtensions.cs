bbb813210c49b11263e5b007747f53c0b499becd
Luke Parker <lukemparker@outlook.com>
2020-09-13 13:39:36 +1000

    Create WebHostEnvironmentExtensions.cs

---
+++ src/ClientService/WebHostEnvironmentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ClientService
+{
+    internal static class WebHostEnvironmentExtensions
+    {
+    }
+}
