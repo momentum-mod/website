e577ac59265e515845d72493a515ce8784531302
Gocnak <Gocnak@users.noreply.github.com>
2020-09-24 00:23:35 -0400

    Update links

--- client/src/app/pages/dashboard/dashboard-menu.ts
+++ client/src/app/pages/dashboard/dashboard-menu.ts
@@ -100,9 +100,4 @@ export const MENU_ITEMS: NbMenuItem[] = [
     icon: 'file-text-outline',
     url: 'https://docs.momentum-mod.org',
   },
-  /*{
-    title: 'Steam Store Page',
-    icon: 'shopping-cart-outline',
-    url: 'https://store.steampowered.com/app/669270/Momentum_Mod/',
-  },*/
 ];
