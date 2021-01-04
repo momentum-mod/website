6b5fed16fccf0824f7d2ba7a2b4f2fc9b935652b
Luke Parker <lukemparker@outlook.com>
2021-01-05 06:02:56 +1000

    My PR got merged, move steam API call to strong typing

--- api/src/Modules/Auth/Momentum.Auth.Application/Queries/SteamUserTicketValidQuery.cs
+++ api/src/Modules/Auth/Momentum.Auth.Application/Queries/SteamUserTicketValidQuery.cs
@@ -12,6 +12,9 @@ namespace Momentum.Auth.Application.Queries
 {
     public class SteamUserTicketValidQuery : IRequest<bool>
     {
+        /// <summary>
+        /// Hex encoded string - if coming from the game, you need to convert from UTF-8
+        /// </summary>
         public string Ticket { get; set; }
         public string UserId { get; set; }
     }
@@ -32,9 +35,9 @@ namespace Momentum.Auth.Application.Queries
             var steamUserAuthInterface = _steamWebInterfaceFactory.CreateSteamWebInterface<SteamUserAuth>(_httpClient);
             var ticketValidResponse = await steamUserAuthInterface.AuthenticateUserTicket(Constants.MomentumModSteamId, request.Ticket);
             
-            // TODO: Submit a PR making this an object and not resorting to dynamic objects
-            return (string)ticketValidResponse.Data.response.@params.result == "OK" && 
-                   request.UserId == (string)ticketValidResponse.Data.response.@params.steamid;
+            return ticketValidResponse.Data.Response.Success &&
+                   ticketValidResponse.Data.Response.Params.Result == "OK" &&
+                   request.UserId == ticketValidResponse.Data.Response.Params.SteamId;
         }
     }
 }
