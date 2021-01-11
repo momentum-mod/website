39215c34fe2869116badb9a2fb1160c1a7eb29f9
Luke Parker <10430890+TheRealHona@users.noreply.github.com>
2021-01-11 16:35:41 +1000

    Add mapping profiles for auths -> viewmodels

--- API/src/Modules/Auth/Momentum.Auth.Api/AutoMapping/ApiProfile.cs
+++ API/src/Modules/Auth/Momentum.Auth.Api/AutoMapping/ApiProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Momentum.Auth.Api.ViewModels;
+using Momentum.Auth.Application.DTOs;
 using Momentum.Users.Application.DTOs;
 
 namespace Momentum.Auth.Api.AutoMapping
@@ -10,6 +11,9 @@ namespace Momentum.Auth.Api.AutoMapping
         {
             CreateMap<UserDto, UserViewModel>();
             CreateMap<UserProfileDto, UserProfileViewModel>();
+            CreateMap<UserTwitterDto, UserTwitterAuthViewModel>();
+            CreateMap<UserTwitchDto, UserTwitchAuthViewModel>();
+            CreateMap<UserDiscordDto, UserDiscordAuthViewModel>();
         }
     }
 }
