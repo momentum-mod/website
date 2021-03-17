9f1503ebe2a04b28617f244681b2214459b7495d
Alvin Yang <alvin.yang73@gmail.com>
2021-03-17 12:29:31 -0700

    Removed redundant break statements

--- client/src/app/@core/models/map-upload-status.model.ts
+++ client/src/app/@core/models/map-upload-status.model.ts
@@ -15,25 +15,18 @@ export function getStatusFromEnum(value: MapUploadStatus): string {
         return 'Approved';
       case MapUploadStatus.PENDING:
         return 'Pending';
-        break;
       case MapUploadStatus.NEEDS_REVISION:
         return 'Needs Revision';
-        break;
       case MapUploadStatus.PRIVATE_TESTING:
         return 'Private Testing';
-        break;
       case MapUploadStatus.PUBLIC_TESTING:
         return 'Public Testing';
-        break;
       case MapUploadStatus.READY_FOR_RELEASE:
         return 'Ready for Release';
-        break;
       case MapUploadStatus.REJECTED:
         return 'Rejected';
-        break;
       case MapUploadStatus.REMOVED:
         return 'Removed';
-        break;
       default:
         return MapUploadStatus[value];
   }
