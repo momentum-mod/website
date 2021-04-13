89e4f4c7b7b999e2aad18b87af704e389e796f04
murglegurgle <yetterbrian@gmail.com>
2021-04-12 22:08:52 -0400

    Draft controller

---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/Controllers/XpSystemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MediatR;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Momentum.XpSystems.Api.Controllers
+{
+    [Route("api/admin/xpsys")]
+    [ApiController]
+    public class XpSystemController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public XpSystemController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetXPSystems()
+        {
+
+
+        }
+
+    }
+}
