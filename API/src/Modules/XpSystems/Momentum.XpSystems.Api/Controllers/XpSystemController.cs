7cab8fd58983fc81053f5193dc239d24e410bba5
murglegurgle <yetterbrian@gmail.com>
2021-04-13 20:54:04 -0400

    Implement xpsystem controller methods

--- API/src/Modules/XpSystems/Momentum.XpSystems.Api/Controllers/XpSystemController.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/Controllers/XpSystemController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using MediatR;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Momentum.XpSystems.Api.ViewModels;
+using Momentum.XpSystems.Application.Queries;
 
 namespace Momentum.XpSystems.Api.Controllers
 {
@@ -12,18 +15,25 @@ namespace Momentum.XpSystems.Api.Controllers
     public class XpSystemController : Controller
     {
         private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public XpSystemController(IMediator mediator)
+        public XpSystemController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetXPSystems()
+        public async Task<IActionResult> GetXpSystems()
         {
+            var xpSystem = await _mediator.Send(new GetXpSystemQuery { });
 
+            var xpSystemViewModel = _mapper.Map<XpSystemViewModel>(xpSystem);
 
+            return Ok(xpSystemViewModel);
         }
+
+
 
     }
 }
