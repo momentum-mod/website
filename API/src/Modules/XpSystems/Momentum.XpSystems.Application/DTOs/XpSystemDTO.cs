6565c7546e6e2bde7d137ee125d73e7aba91536d
murglegurgle <yetterbrian@gmail.com>
2021-04-13 20:52:52 -0400

    Create DTO

---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Application/DTOs/XpSystemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Momentum.XpSystems.Application.DTOs
+{
+    public class XpSystemDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string RankXP { get; set; }
+        public string CosmeticXP { get; set; }
+    }
+}
