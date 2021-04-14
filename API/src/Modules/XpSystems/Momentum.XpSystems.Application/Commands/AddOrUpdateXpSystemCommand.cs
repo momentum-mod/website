5276f424ceb629daedb0eea72159c5ebabac535d
murglegurgle <yetterbrian@gmail.com>
2021-04-13 20:51:48 -0400

    Handle xpsystem requests

---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Application/Commands/AddOrUpdateXpSystemCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Momentum.XpSystems.Core.Repositories;
+
+namespace Momentum.XpSystems.Application.Commands
+{
+    public class AddOrUpdateXpSystemCommand : IRequest { }
+
+    public class AddOrUpdateXpSystemCommandHandler : IRequestHandler<AddOrUpdateXpSystemCommand>
+    {
+        private readonly IXpSystemRepository _xpSystemRepository;
+
+        public AddOrUpdateXpSystemCommandHandler(IXpSystemRepository xpSystemRepository)
+        {
+            _xpSystemRepository = xpSystemRepository;
+        }
+
+        public async Task<Unit> Handle(AddOrUpdateXpSystemCommand request, CancellationToken cancellationToken)
+        {
+            // await _xpSystemRepository.AddOrUpdate();
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Application/Queries/GetXpSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Momentum.XpSystems.Application.DTOs;
+using Momentum.XpSystems.Core.Repositories;
+
+namespace Momentum.XpSystems.Application.Queries
+{
+    public class GetXpSystemQuery : IRequest<XpSystemDTO> { }
+    
+    public class GetXpSystemHandlerHandler : IRequestHandler<GetXpSystemQuery, XpSystemDTO>
+    {
+        private readonly IMapper _mapper;
+        private readonly IXpSystemRepository _xpSystemRepository;
+
+        public async Task<XpSystemDTO> Handle(GetXpSystemQuery request, CancellationToken cancellationToken)
+        {
+            var xpSystem = await _xpSystemRepository.Get();
+
+            return _mapper.Map<XpSystemDTO>(xpSystem);
+        }
+    }
+}
