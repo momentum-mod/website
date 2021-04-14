ed83286193773ca5a2e21a4b12dd68bbd4675def
murglegurgle <yetterbrian@gmail.com>
2021-04-13 21:33:11 -0400

    Create maps

---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/AutoMapping/ApiProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Momentum.XpSystems.Api.ViewModels;
+using Momentum.XpSystems.Application.DTOs;
+
+namespace Momentum.XpSystems.Api.AutoMapping
+{
+    public class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<XpSystemDto, XpSystemViewModel>();
+            CreateMap<XpSystemViewModel, XpSystemDto>();
+        }
+    }
+}
---
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Application/AutoMapping/ApplicationProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Momentum.XpSystems.Application.DTOs;
+using Momentum.XpSystems.Core.Models;
+
+
+namespace Momentum.XpSystems.Application.AutoMapping
+{
+    public class ApplicationProfile : AutoMapper.Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<XpSystem, XpSystemDto>();
+            CreateMap<XpSystemDto, XpSystem>();
+        }
+    }
+}
