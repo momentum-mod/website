3c9a5e3e65b074edc2e40911d8d05b448c71e39a
murglegurgle <yetterbrian@gmail.com>
2021-04-13 22:31:56 -0400

    Implement AddOrUpdateXpSYstemCommand

--- API/src/Modules/XpSystems/Momentum.XpSystems.Application/Commands/AddOrUpdateXpSystemCommand.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Application/Commands/AddOrUpdateXpSystemCommand.cs
@@ -4,25 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using MediatR;
+using Momentum.XpSystems.Application.DTOs;
+using Momentum.XpSystems.Core.Models;
 using Momentum.XpSystems.Core.Repositories;
 
 namespace Momentum.XpSystems.Application.Commands
 {
-    public class AddOrUpdateXpSystemCommand : IRequest { }
+    public class AddOrUpdateXpSystemCommand : IRequest
+    {
+        public Func<Task<XpSystemDto>> BuildXpSystemDto { get; set; }
+    }
 
     public class AddOrUpdateXpSystemCommandHandler : IRequestHandler<AddOrUpdateXpSystemCommand>
     {
+        private readonly IMapper _mapper;
         private readonly IXpSystemRepository _xpSystemRepository;
 
-        public AddOrUpdateXpSystemCommandHandler(IXpSystemRepository xpSystemRepository)
+        public AddOrUpdateXpSystemCommandHandler(IMapper mapper, IXpSystemRepository xpSystemRepository)
         {
+            _mapper = mapper;
             _xpSystemRepository = xpSystemRepository;
         }
 
         public async Task<Unit> Handle(AddOrUpdateXpSystemCommand request, CancellationToken cancellationToken)
         {
-            // await _xpSystemRepository.AddOrUpdate();
+            var xpSystem = _mapper.Map<XpSystem>(await request.BuildXpSystemDto.Invoke());
+
+            await _xpSystemRepository.AddOrUpdate(xpSystem);
 
             return Unit.Value;
         }
