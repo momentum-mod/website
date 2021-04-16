4b9dbe7ee167d120f71208a5001d173a7970fcca
murglegurgle <yetterbrian@gmail.com>
2021-04-16 19:42:59 -0400

    Clean up comments

--- API/src/Modules/XpSystems/Momentum.XpSystems.Api/AutoMapping/ApiProfile.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/AutoMapping/ApiProfile.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Momentum.XpSystems.Api.ViewModels;
 using Momentum.XpSystems.Application.DTOs;
+using Newtonsoft.Json.Linq;
 
 namespace Momentum.XpSystems.Api.AutoMapping
 {
@@ -13,14 +14,9 @@ namespace Momentum.XpSystems.Api.AutoMapping
     {
         public ApiProfile()
         {
-            // AutoMapper automatically applies .toString() if result is string
             CreateMap<XpSystemDto, XpSystemViewModel>().ForMember(
                 x => x.CosXP, x => x.MapFrom(x => x.CosmeticXP)
             );
-
-            // AutoMapper checks type result at runtime
-            // This will break because cannot implicitly convert from string to JObject
-            //CreateMap<XpSystemViewModel, XpSystemDto>();
         }
     }
 }
