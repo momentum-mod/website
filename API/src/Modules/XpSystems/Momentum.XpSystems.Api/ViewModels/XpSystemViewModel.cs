f71bd91c75e1395508275fab364e6c945c975fdd
murglegurgle <yetterbrian@gmail.com>
2021-04-18 10:36:38 -0400

    Capitalize properties

--- API/src/Modules/XpSystems/Momentum.XpSystems.Api/ViewModels/XpSystemViewModel.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/ViewModels/XpSystemViewModel.cs
@@ -15,7 +15,7 @@ namespace Momentum.XpSystems.Api.ViewModels
     public class Top10
     {
         public int WRPoints { get; set; }
-        public List<double> rankPercentages { get; set; }
+        public List<double> RankPercentages { get; set; }
     }
 
     public class Formula
@@ -26,60 +26,60 @@ namespace Momentum.XpSystems.Api.ViewModels
 
     public class Groups
     {
-        public int maxGroups { get; set; }
-        public List<double> groupScaleFactors { get; set; }
-        public List<double> groupExponents { get; set; }
-        public List<int> groupMinSizes { get; set; }
-        public List<double> groupPointPcts { get; set; }
+        public int MaxGroups { get; set; }
+        public List<double> GroupScaleFactors { get; set; }
+        public List<double> GroupExponents { get; set; }
+        public List<int> GroupMinSizes { get; set; }
+        public List<double> GroupPointPcts { get; set; }
     }
 
     public class RankXP
     {
-        public Top10 top10 { get; set; }
-        public Formula formula { get; set; }
-        public Groups groups { get; set; }
+        public Top10 Top10 { get; set; }
+        public Formula Formula { get; set; }
+        public Groups Groups { get; set; }
     }
 
     public class Levels
     {
-        public int maxLevels { get; set; }
-        public int startingValue { get; set; }
-        public int linearScaleBaseIncrease { get; set; }
-        public int linearScaleInterval { get; set; }
-        public double linearScaleIntervalMultiplier { get; set; }
-        public int staticScaleStart { get; set; }
-        public double staticScaleBaseMultiplier { get; set; }
-        public int staticScaleInterval { get; set; }
-        public double staticScaleIntervalMultiplier { get; set; }
+        public int MaxLevels { get; set; }
+        public int StartingValue { get; set; }
+        public int LinearScaleBaseIncrease { get; set; }
+        public int LinearScaleInterval { get; set; }
+        public double LinearScaleIntervalMultiplier { get; set; }
+        public int StaticScaleStart { get; set; }
+        public double StaticScaleBaseMultiplier { get; set; }
+        public int StaticScaleInterval { get; set; }
+        public double StaticScaleIntervalMultiplier { get; set; }
     }
 
     public class TierScale
     {
-        public int linear { get; set; }
-        public int staged { get; set; }
-        public int? stages { get; set; }
-        public int? bonus { get; set; }
+        public int Linear { get; set; }
+        public int Staged { get; set; }
+        public int? Stages { get; set; }
+        public int? Bonus { get; set; }
     }
 
     public class Unique
     {
-        public TierScale tierScale { get; set; }
+        public TierScale TierScale { get; set; }
     }
 
     public class Repeat
     {
-        public TierScale tierScale { get; set; }
+        public TierScale TierScale { get; set; }
     }
 
     public class Completions
     {
-        public Unique unique { get; set; }
-        public Repeat repeat { get; set; }
+        public Unique Unique { get; set; }
+        public Repeat Repeat { get; set; }
     }
 
     public class CosXP
     {
-        public Levels levels { get; set; }
-        public Completions completions { get; set; }
+        public Levels Levels { get; set; }
+        public Completions Completions { get; set; }
     }
 }
