0b53c5e0aa420427c92907a0bbd28b01503764a1
murglegurgle <yetterbrian@gmail.com>
2021-04-20 14:12:53 -0400

    Map between xpsystem DTO and ViewModel

--- API/src/Modules/XpSystems/Momentum.XpSystems.Api/AutoMapping/ApiProfile.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Api/AutoMapping/ApiProfile.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using Momentum.XpSystems.Api.ViewModels;
+using Momentum.XpSystems.Api.ViewModels.Rank;
+using Momentum.XpSystems.Api.ViewModels.Cosmetic;
 using Momentum.XpSystems.Application.DTOs;
+using Momentum.XpSystems.Application.DTOs.Rank;
+using Momentum.XpSystems.Application.DTOs.Cosmetic;
 
 namespace Momentum.XpSystems.Api.AutoMapping
 {
@@ -8,9 +12,41 @@ namespace Momentum.XpSystems.Api.AutoMapping
     {
         public ApiProfile()
         {
-            CreateMap<XpSystemDto, XpSystemViewModel>().ForMember(
-                x => x.CosXp, x => x.MapFrom(x => x.CosmeticXp)
-            );
+            CreateMap<XpSystemDto, XpSystemViewModel>();
+            CreateMap<XpSystemViewModel, XpSystemDto>();
+
+            CreateMap<RankXpViewModel, RankXpDto>();
+            CreateMap<RankXpDto, RankXpViewModel>();
+
+            CreateMap<Top10ViewModel, Top10Dto>();
+            CreateMap<Top10Dto, Top10ViewModel>();
+
+            CreateMap<FormulaViewModel, FormulaDto>();
+            CreateMap<FormulaDto, FormulaViewModel>();
+
+            CreateMap<GroupsViewModel, GroupsDto>();
+            CreateMap<GroupsDto, GroupsViewModel>();
+
+            CreateMap<CosmeticXpViewModel, CosmeticXpDto>();
+            CreateMap<CosmeticXpDto, CosmeticXpViewModel>();
+
+            CreateMap<LevelsViewModel, LevelsDto>();
+            CreateMap<LevelsDto, LevelsViewModel>();
+
+            CreateMap<CompletionsViewModel, CompletionsDto>();
+            CreateMap<CompletionsDto, CompletionsViewModel>();
+
+            CreateMap<UniqueViewModel, UniqueDto>();
+            CreateMap<UniqueDto, UniqueViewModel>();
+
+            CreateMap<UniqueTierScaleViewModel, UniqueTierScaleDto>();
+            CreateMap<UniqueTierScaleDto, UniqueTierScaleViewModel>();
+
+            CreateMap<RepeatViewModel, RepeatDto>();
+            CreateMap<RepeatDto, RepeatViewModel>();
+
+            CreateMap<RepeatTierScaleViewModel, RepeatTierScaleDto>();
+            CreateMap<RepeatTierScaleDto, RepeatTierScaleViewModel>();
         }
     }
 }
