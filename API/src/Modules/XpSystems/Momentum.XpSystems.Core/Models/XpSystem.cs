b083254426eb0a63f8cedd9b08d266fea665cf91
murglegurgle <yetterbrian@gmail.com>
2021-04-21 18:07:40 -0400

    Replace tab with 4 spaces

--- API/src/Modules/XpSystems/Momentum.XpSystems.Core/Models/XpSystem.cs
+++ API/src/Modules/XpSystems/Momentum.XpSystems.Core/Models/XpSystem.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using Momentum.Framework.Core.Models;
+﻿using Momentum.Framework.Core.Models;
 using Momentum.XpSystems.Core.Models.Cosmetic;
 using Momentum.XpSystems.Core.Models.Rank;
+using System;
 
 namespace Momentum.XpSystems.Core.Models
 {
     public class XpSystem : TimeTrackedModel
     {
         public Guid Id { get; set; }
-		public RankXp RankXp { get; set; }
-		public CosmeticXp CosmeticXp { get; set; }
-	}
+        public RankXp RankXp { get; set; }
+        public CosmeticXp CosmeticXp { get; set; }
+    }
 }
