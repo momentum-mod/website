1921ddb280af1dbcda207e8cfaba752d6c54f414
murglegurgle <yetterbrian@gmail.com>
2021-04-22 17:52:43 -0400

    Add report repo to container

---
+++ API/src/Modules/Reports/Momentum.Reports.Api/Module.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Momentum.Framework.Core.DependencyInjection;
+using Momentum.Reports.Core.Repositories;
+using Momentum.Reports.Infrastructure.Repositories;
+
+namespace Momentum.Reports.Api
+{
+    public class Module : IModuleInitializer
+    {
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IReportRepository, ReportRepository>();
+        }
+    }
+}
