22a9fbcdebcbb502a98c6292d00ce29f5d804e12
murglegurgle <yetterbrian@gmail.com>
2021-04-22 17:51:05 -0400

    Outline project

---
+++ API/src/Modules/Reports/Momentum.Reports.Infrastructure/Repositories/ReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using Momentum.Reports.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momentum.Reports.Infrastructure.Repositories
+{
+    public class ReportRepository : IReportRepository
+    {
+    }
+}
