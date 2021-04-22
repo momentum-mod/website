4e9d4233fe92cbb9b6eb71594a4dab5f7e1e719f
murglegurgle <yetterbrian@gmail.com>
2021-04-22 17:52:17 -0400

    Create report maps

---
+++ API/src/Modules/Reports/Momentum.Reports.Api/AutoMapping/ApiProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Momentum.Reports.Api.ViewModels;
+using Momentum.Reports.Application.DTOs;
+
+namespace Momentum.Reports.Api.AutoMapping
+{
+    public class ApiProfile : Profile
+    {
+        public ApiProfile()
+        {
+            CreateMap<ReportDto, ReportViewModel>();
+            CreateMap<ReportViewModel, ReportDto>();
+        }
+    }
+}
