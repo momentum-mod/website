2dd5e29a9413a8af3080e46c0b2531b2fed9b0a8
murglegurgle <yetterbrian@gmail.com>
2021-04-22 20:38:44 -0400

    Add db call for numOfReportsSubmittedToday

---
+++ API/src/Modules/Reports/Momentum.Reports.Core/Repositories/IReportRepository.cs
@@ -0,0 +1,15 @@
+﻿using Momentum.Framework.Core.Repositories;
+using Momentum.Reports.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momentum.Reports.Core.Repositories
+{
+    public interface IReportRepository : IGenericRepository<Report>
+    {
+        Task<int> GetTodayReportCount(Guid submitterId);
+    }
+}
--- API/src/Modules/Reports/Momentum.Reports.Infrastructure/Repositories/ReportRepository.cs
+++ API/src/Modules/Reports/Momentum.Reports.Infrastructure/Repositories/ReportRepository.cs
@@ -1,13 +1,28 @@
-﻿using Momentum.Reports.Core.Repositories;
+﻿using Marten;
+using Momentum.Framework.Infrastructure.Repositories;
+using Momentum.Reports.Core.Models;
+using Momentum.Reports.Core.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Momentum.Reports.Infrastructure.Repositories
 {
-    public class ReportRepository : IReportRepository
+    public class ReportRepository : GenericTimeTrackedRepository<Report>, IReportRepository
     {
+        private readonly IDocumentStore _store;
+
+        public ReportRepository(IDocumentStore store) : base(store)
+        {
+            _store = store;
+        }
+
+        public async Task<int> GetTodayReportCount (Guid submitterId)
+        {
+            using var session = _store.QuerySession();
+
+            var numOfReportsSubmittedToday = await session.Query<Report>().CountAsync(x => x.SubmitterId == submitterId && x.CreatedAt > DateTime.Today);
+
+            return numOfReportsSubmittedToday;
+        }
     }
 }
