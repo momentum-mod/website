798c8bc873ddd3f437c54d9bdde38e02714556b2
murglegurgle <yetterbrian@gmail.com>
2021-04-23 16:34:36 -0400

    Configure reports module

--- API/src/Gateway/Momentum.Gateway.Api/Helpers/ModulesUtility.cs
+++ API/src/Gateway/Momentum.Gateway.Api/Helpers/ModulesUtility.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using AutoMapper.QueryableExtensions;
 using Microsoft.Extensions.DependencyInjection;
 using Momentum.Framework.Core.DependencyInjection;
+using Momentum.Reports.Application.Commands;
 using Momentum.Users.Application.Commands;
 using Momentum.XpSystems.Application.Commands;
 
@@ -24,7 +25,9 @@ namespace Momentum.Gateway.Api.Helpers
                 // User
                 typeof(GetOrCreateNewUserCommand).GetTypeInfo().Assembly,
                 // XpSystem
-                typeof(CreateOrUpdateXpSystemCommand).GetTypeInfo().Assembly
+                typeof(CreateOrUpdateXpSystemCommand).GetTypeInfo().Assembly,
+                // Report
+                typeof(CreateReportCommand).GetTypeInfo().Assembly
             };
 
         public static IEnumerable<Assembly> GetApiLayerAssemblies()
@@ -39,6 +42,8 @@ namespace Momentum.Gateway.Api.Helpers
                 new XpSystems.Infrastructure.MartenInitializer(),
                 // Maps
                 new Maps.Infrastructure.MartenInitializer(),
+                // Report
+                new Reports.Infrastructure.MartenInitializer()
             };
 
         public static void AddModuleControllers(this IMvcBuilder mvcBuilder, IEnumerable<IModuleInitializer> modules)
---
+++ API/src/Modules/Reports/Momentum.Reports.Infrastructure/MartenInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Marten;
+using Momentum.Framework.Core.DependencyInjection;
+using Momentum.Reports.Core.Models;
+
+namespace Momentum.Reports.Infrastructure
+{
+    public class MartenInitializer : IMartenInitializer
+    {
+        public void SetupDomainIdentities(StoreOptions options)
+        {
+            options.Schema.For<Report>().Identity(x => x.Id);
+        }
+    }
+}
