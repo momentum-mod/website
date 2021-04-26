7999f77067352f2b06358c5d1583520cd14886e5
Brian Y <yetterbrian@gmail.com>
2021-04-26 15:54:33 -0400

    Add json attributes to report viewmodels

--- API/src/Modules/Reports/Momentum.Reports.Api/ViewModels/CreateReportViewModel.cs
+++ API/src/Modules/Reports/Momentum.Reports.Api/ViewModels/CreateReportViewModel.cs
@@ -1,19 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Momentum.Reports.Api.ViewModels
 {
     public class CreateReportViewModel
     {
+        [JsonPropertyName("data")]
         public string Data { get; set; }
         /// <summary>
         /// USER_PROFILE_REPORT: 0, MAP_REPORT: 1, MAP_COMMENT_REPORT: 2,
         /// </summary>
+        [JsonPropertyName("type")]
         public ushort Type { get; set; }
         /// <summary>
         /// INAPPROPRIATE_CONTENT: 1, PLAGIARSIM: 2, SPAM: 3, OTHER: 0,
         /// </summary>
+        [JsonPropertyName("category")]
         public uint Category { get; set; }
         [StringLength(1000)]
+        [JsonPropertyName("message")]
         public string Message { get; set; }
     }
 }
--- API/src/Modules/Reports/Momentum.Reports.Api/ViewModels/UpdateReportViewModel.cs
+++ API/src/Modules/Reports/Momentum.Reports.Api/ViewModels/UpdateReportViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Momentum.Reports.Api.ViewModels
 {
     public class UpdateReportViewModel
     {
+        [JsonPropertyName("resolved")]
         public bool Resolved { get; set; }
+        [JsonPropertyName("resolutionMessage")]
         [StringLength(1000)]
         public string ResolutionMessage { get; set; }
     }
