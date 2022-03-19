b7702f6ec4aaaab42830cce7578ad5f060d74df0
Alex Wilson <alex.dave999@gmail.com>
2022-03-19 18:33:55 +0000

    ✨ -  Added HTTP logging middleware

---
+++ server/src/middlewares/http-logger.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class HTTPLoggerMiddleware implements NestMiddleware {
+    private logger = new Logger('HTTP');
+
+    use(request: Request, response: Response, next: NextFunction): void {
+        const { ip, method, originalUrl } = request;
+        const userAgent = request.get('user-agent') || '';
+
+        response.on('finish', () => {
+            const { statusCode } = response;
+            const contentLength = response.get('content-length');
+
+            this.logger.log(`${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`);
+        });
+
+        next();
+    }
+}
