e07fd4833472d75b61d64f775f3b35a3941e315b
tsa96 <tom@trumpetdust.org>
2022-06-03 16:35:23 +0100

    feat: Remove Prettier squiggly lines

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,21 +2,20 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: 'tsconfig.json',
-        sourceType: 'module',
+        sourceType: 'module'
     },
     plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
     extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
     root: true,
     env: {
         node: true,
-        jest: true,
+        jest: true
     },
     ignorePatterns: ['.eslintrc.js'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'prettier/prettier': ['error'],
-    },
+        '@typescript-eslint/no-explicit-any': 'off'
+    }
 };
