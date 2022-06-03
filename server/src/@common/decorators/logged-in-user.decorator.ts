de178af02450bd57b078c943c8cb1d9c0e21b674
tsa96 <tom@trumpetdust.org>
2022-06-03 09:26:06 +0100

    feat: Custom decorator and validators

---
+++ server/src/@common/decorators/logged-in-user.decorator.ts
@@ -0,0 +1,7 @@
+﻿import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+// This will always be behind auth guard so user should always been in request
+export const LoggedInUser = createParamDecorator((data: string | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return data ? request.user[data] : request.user;
+});
