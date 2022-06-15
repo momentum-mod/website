972ac2f4b9f09701fadb4df33319498f3c1aa843
tsa96 <tom@trumpetdust.org>
2022-06-15 10:43:06 +0100

    feat: Port old tests
    God, these things are fucking gross. We'll want to write a LOT more. Ran jest-codemods on everything, not great but best we're gonna get I reckon.

---
+++ server/tests/_reports.e2e-spec.ts
@@ -0,0 +1,99 @@
+// noinspection DuplicatedCode
+
+describe('reports', () => {
+    let adminAccessToken, adminGameAccessToken;
+    const testUser = {
+        id: 1,
+        steamID: '00000000000000001',
+        roles: user.Role.VERIFIED,
+        bans: 0
+    };
+    const testAdmin = {
+        id: 2,
+        steamID: '00000000000000002',
+        roles: user.Role.ADMIN,
+        bans: 0
+    };
+    const testAdminGame = {
+        id: 3,
+        steamID: '00000000000000003',
+        roles: user.Role.ADMIN,
+        bans: 0
+    };
+
+    const testMap = {
+        id: 1,
+        name: 'test_map',
+        type: map.MAP_TYPE.UNKNOWN,
+        statusFlag: map.STATUS.APPROVED,
+        submitterID: testUser.id,
+        info: {
+            description: 'newmap_5',
+            numTracks: 1,
+            creationDate: new Date()
+        },
+        tracks: [
+            {
+                trackNum: 0,
+                numZones: 1,
+                isLinear: false,
+                difficulty: 5
+            }
+        ],
+        credits: {
+            id: 1,
+            type: map.CreditType.AUTHOR,
+            userID: testUser.id
+        }
+    };
+
+    beforeAll(() => {
+        // return forceSyncDB().then(() => {
+        //     return auth.genAccessToken(testUser);
+        // }).then((token) => {
+        //     accessToken = token;
+        //     return User.create(testUser);
+        // }).then(() => {
+        //     testAdmin.roles |= user.Role.ADMIN;
+        //     return auth.genAccessToken(testAdmin);
+        // }).then((token) => {
+        //     adminAccessToken = token;
+        //     return User.create(testAdmin);
+        // }).then(() => {
+        //     testAdminGame.roles |= user.Role.ADMIN;
+        //     return auth.genAccessToken(testAdminGame, true);
+        // }).then((token) => {
+        //     adminGameAccessToken = token;
+        //     return User.create(testAdminGame);
+        // }).then(user => {
+        //     return Map.create(testMap, {
+        //         include: [
+        //             {  model: MapInfo, as: 'info',},
+        //             {  model: MapCredit, as: 'credits'}
+        //         ],
+        //     });
+        // });
+    });
+
+    describe('endpoints', () => {
+        //
+        // describe('POST /api/reports', () => {
+        //     it('should create a new report', () => {
+        //         return chai.request(server)
+        //             .post('/api/reports')
+        //             .set('Authorization', 'Bearer ' + accessToken)
+        //             .send({
+        //                 data: testMap.id.toString(),
+        //                 type: report.ReportType.MAP_REPORT,
+        //                 category: report.ReportCategory.PLAGIARSIM,
+        //                 message: 'I created this map :(',
+        //             })
+        //         .then(res => {
+        //             expect(res).to.have.status(200);
+        //             expect(res).to.be.json;
+        //             expect(res.body).toHaveProperty('id');
+        //         });
+        //     });
+        // });
+    });
+});
