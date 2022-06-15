df619c409e0a90b8d96206bfb8a4f07a2abd307e
tsa96 <tom@trumpetdust.org>
2022-06-15 02:15:11 +0100

    feat: Activities/ endpoint stub

--- server/src/modules/activities/activities.controller.ts
+++ server/src/modules/activities/activities.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
+import { ApiOkPaginatedResponse, PagedResponseDto } from '../../@common/dto/common/api-response.dto';
+import { ActivityDto } from '../../@common/dto/user/activity.dto';
+import { ActivitiesGetQuery } from './queries/get.query.dto';
 
 @ApiBearerAuth()
 @Controller('api/v1/activities')
@@ -9,4 +12,11 @@ import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 export class ActivitiesController {
     constructor(private readonly activitiesService: ActivitiesService) {}
+
+    @Get()
+    @ApiOperation({ summary: 'Returns a list of activities' })
+    @ApiOkPaginatedResponse(ActivityDto, { description: 'Paginated list of activities' })
+    public async GetActivities(@Query() query?: ActivitiesGetQuery): Promise<PagedResponseDto<ActivityDto>> {
+        return void 0;
+    }
 }
---
+++ server/src/modules/activities/queries/get.query.dto.ts
@@ -0,0 +1,38 @@
+﻿import { PaginationQueryDto } from '../../../@common/dto/common/pagination.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsInt, IsOptional } from 'class-validator';
+import { EActivityTypes } from '../../../@common/enums/activity.enum';
+import { Transform, Type } from 'class-transformer';
+
+export class ActivitiesGetQuery extends PaginationQueryDto {
+    @ApiPropertyOptional({
+        name: 'userID',
+        type: String,
+        description: 'Filter by user ID'
+    })
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    userID: number;
+
+    @ApiPropertyOptional({
+        name: 'type',
+        enum: EActivityTypes,
+        type: String,
+        description: 'Types of activities to include'
+    })
+    @IsOptional()
+    @Type(() => Number)
+    // TODO: are these flags?
+    @IsEnum(EActivityTypes)
+    activityType: EActivityTypes;
+
+    @ApiPropertyOptional({
+        name: 'data',
+        type: BigInt,
+        description: 'The ID into the table of the corresponding activity'
+    })
+    @IsOptional()
+    @Transform(({ value }) => BigInt(value))
+    data: bigint;
+}
