660d33c5294473a28ea135bb2813160e108dd293
tsa96 <tom@trumpetdust.org>
2022-06-23 13:10:35 +0100

    feat: Admin auth guard
    Uses enum flags for auth. Should allow combined flags if we ever needed that

--- server/src/modules/admin/admin.module.ts
+++ server/src/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AdminRepo } from './admin.repo';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from '../auth/guard/roles.guard';
 
 @Module({
     imports: [PrismaModule],
     controllers: [AdminController],
-    providers: [AdminService, AdminRepo],
+    providers: [AdminService, UsersRepo, MapsRepo, { provide: APP_GUARD, useClass: RolesGuard }],
     exports: [AdminService]
 })
 export class AdminModule {}
