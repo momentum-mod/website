0a265953f721522786fb4e1a32c7b4cb0d4651a1
tsa96 <tom@trumpetdust.org>
2022-06-23 13:25:11 +0100

    feat: Make ESLint allow unused vars and args starting w/ _
    Find this useful in small places to indicate we're ignoring some argument

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -16,6 +16,15 @@ module.exports = {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
+        '@typescript-eslint/no-explicit-any': 'off',
+        "no-unused-vars": "off",
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_'
+            }
+        ]
     }
 };
