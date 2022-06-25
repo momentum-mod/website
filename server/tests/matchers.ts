a15eb4914f972953eb2986673db8a2bf3c5f75ab
tsa96 <tom@trumpetdust.org>
2022-06-25 03:30:24 +0100

    feat: Custom Jest matchers for DTOs
    Adds custom matchers for checking DTOs, reconstructs the DTO from the received JSON and validates it. This lets us do super strong validation in E2E, though need major refactors of tests and some tweaks to DTOs, about halfway through that, will work on it more tomrrow.

---
+++ server/tests/matchers.ts
@@ -0,0 +1,105 @@
+﻿import { plainToInstance } from 'class-transformer';
+import { validateSync, ValidationError } from 'class-validator';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace jest {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        interface Matchers<R> {
+            toBeValidDto: (type: any) => CustomMatcherResult;
+            toBeValidPagedDto: (type: any) => CustomMatcherResult;
+        }
+    }
+}
+
+const formatValidationErrors = (errors: ValidationError[]): string =>
+    errors
+        .map(
+            (err) =>
+                `\n\tProperty:      ${err.property}` +
+                `\n\tValue:         ${JSON.stringify(err.value)}` +
+                `\n\tConstaints:    ` +
+                Object.entries(err.constraints ?? {})
+                    .map(([conName, conMessage]) => `${conName}: ${conMessage}`)
+                    .join('\n                   ')
+        )
+        .join('\n');
+
+expect.extend({
+    toBeValidDto(received, type) {
+        // Use class-transformer to create an instance of the DTO type with the passed in data
+        const instance = plainToInstance(type, received);
+
+        // This instance has all the validators attached, so this lets us validate all the *outgoing* DTOs in tests.
+        const errors = validateSync(instance, { forbidNonWhitelisted: true });
+
+        const pass = errors.length === 0;
+
+        if (pass) {
+            return {
+                message: () => `expected DTO data not to validate the ${type.name} DTO`,
+                pass: true
+            };
+        } else {
+            const plural = errors.length > 1;
+            return {
+                message: () =>
+                    `expected DTO data ${JSON.stringify(received)} to validate ${type.name}. It threw th${
+                        plural ? 'ese' : 'is'
+                    } error${plural ? 's' : ''}` + formatValidationErrors(errors),
+                pass: false
+            };
+        }
+    },
+
+    toBeValidPagedDto(received, type) {
+        if (!received || !received.hasOwnProperty('response') || !Array.isArray(received.response)) {
+            return {
+                message: () => `expected paged DTO with response array, data was invalid`,
+                pass: false
+            };
+        }
+
+        if (received.response.length === 0) {
+            return {
+                message: () => `expected paged DTO with populated response array, array was empty`,
+                pass: false
+            };
+        }
+
+        const totalErrors: [any, ValidationError[]][] = [];
+
+        for (const item of received.response) {
+            const instance = plainToInstance(type, item);
+
+            const errors = validateSync(instance, {
+                forbidNonWhitelisted: true
+            });
+
+            if (errors.length > 0) {
+                totalErrors.push([item, errors]);
+            }
+        }
+
+        const pass = totalErrors.length === 0;
+
+        if (pass) {
+            return {
+                message: () => `expected at least one paged DTO data not to validate the ${type.name} DTO`,
+                pass: true
+            };
+        } else {
+            const plural = totalErrors.length > 1;
+            return {
+                message: () =>
+                    `expected paged DTO to contain valid DTOs of type ${type.name}. Th${plural ? 'ese' : 'is'} item${
+                        plural ? 's' : ''
+                    } errored:\n` +
+                    totalErrors
+                        .map((err) => `${JSON.stringify(err[0])} errored with: ${formatValidationErrors(err[1])}`)
+                        .join('\n\n'),
+                pass: false
+            };
+        }
+    }
+});
