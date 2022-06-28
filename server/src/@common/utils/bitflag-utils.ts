f6a432ca50520f6682cf444c5f8d68bb2e16e43e
tsa96 <tom@trumpetdust.org>
2022-06-28 03:51:05 +0100

    feat: Add static bitflag util methods

---
+++ server/src/@common/utils/bitflag-utils.ts
@@ -0,0 +1,9 @@
+﻿/**
+ * Tiny bitwise operations for enum flags
+ */
+export const Bitflags = {
+    has: (flags: number, check: number): boolean => (flags & check) === check,
+    add: (flags: number, add: number): number => flags | add,
+    remove: (flags: number, remove: number): number => flags & ~remove,
+    toggle: (flags: number, toggle: number): number => flags ^ toggle
+};
