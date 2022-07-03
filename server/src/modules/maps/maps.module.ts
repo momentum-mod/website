e889485e7ce04dd4a2402624b700802bbcc94da1
tsa96 <tom@trumpetdust.org>
2022-07-03 20:40:40 +0100

    feat: Maps/ progress

--- server/src/modules/maps/maps.module.ts
+++ server/src/modules/maps/maps.module.ts
@@ -5,11 +5,13 @@ import { MapsService } from './maps.service';
 import { RepoModule } from '../repo/repo.module';
 import { AuthModule } from '../auth/auth.module';
 import { FileStoreModule } from '../filestore/file-store.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from '../auth/guard/roles.guard';
 
 @Module({
     imports: [RepoModule, AuthModule, FileStoreModule], // TODO: why is auth needed?
     controllers: [MapsController],
-    providers: [MapsService, MapsRepoService],
+    providers: [{ provide: APP_GUARD, useClass: RolesGuard }, MapsService, MapsRepoService],
     exports: [MapsService]
 })
 export class MapsModule {}
