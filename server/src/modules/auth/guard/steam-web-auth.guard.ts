1f6c896ac1846f5e21b5fc6ea11968cf8e5356de
tsa96 <tom@trumpetdust.org>
2022-07-09 23:24:30 +0100

    fix: Public auth decorator not on /steam/return
    SteamWebAuthGuard doesn't need to be affected by @Public, the purpose of Public is to override routes that are set to require a JWT (i.e. the whole app).
    
    So just have @Public just affect JWTAuthGuard, then make everything on auth/ public - /steam/return is still protected by the SteamWebAuthGuard since @Public no long affects it.

--- server/src/modules/auth/guard/steam-web-auth.guard.ts
+++ server/src/modules/auth/guard/steam-web-auth.guard.ts
@@ -1,22 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { IS_PUBLIC_KEY } from '../../../@common/decorators/public.decorator';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SteamWebAuthGuard extends AuthGuard('steam') {
-    constructor(private reflector: Reflector) {
-        super();
-    }
-
-    canActivate(context: ExecutionContext) {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-            context.getHandler(),
-            context.getClass()
-        ]);
-
-        if (isPublic) return true;
-
-        return super.canActivate(context);
-    }
-}
+export class SteamWebAuthGuard extends AuthGuard('steam') {}
