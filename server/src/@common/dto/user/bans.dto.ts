8372991a7ce793410beebe32d7b645da8be30906
tsa96 <tom@trumpetdust.org>
2022-07-13 08:56:42 +0100

    feat: Update DTOs to handle new Roles and Bans

---
+++ server/src/@common/dto/user/bans.dto.ts
@@ -0,0 +1,27 @@
+﻿import { Bans } from '@prisma/client';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { IsBoolean } from 'class-validator';
+
+export class BansDto implements Bans {
+    @ApiProperty({ description: 'If the user is banned from submitting runs', type: Boolean })
+    @IsBoolean()
+    leaderboards: boolean;
+
+    @ApiProperty({ description: 'If the user is banned from updating their alias', type: Boolean })
+    @IsBoolean()
+    alias: boolean;
+
+    @ApiProperty({ description: 'If the user is banned from updating their avatar', type: Boolean })
+    @IsBoolean()
+    avatar: boolean;
+
+    @ApiProperty({ description: 'If the user is banned from updating their bio', type: Boolean })
+    @IsBoolean()
+    bio: boolean;
+
+    @Exclude()
+    userID: number;
+}
+
+export class UpdateBansDto extends PartialType(OmitType(BansDto, ['userID'] as const)) {}
