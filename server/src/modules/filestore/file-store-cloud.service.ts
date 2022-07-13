24adcfeffe284b972d5b7daf39b6b56b2398c852
tsa96 <tom@trumpetdust.org>
2022-07-13 09:08:34 +0100

    chore: Clean up FileStoreCloud service

--- server/src/modules/filestore/file-store-cloud.service.ts
+++ server/src/modules/filestore/file-store-cloud.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { appConfig } from '../../../config/config';
 import { IFileStoreCloudFile } from './file-store.interface';
-import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { FileStoreUtils } from './file-store.utility';
 @Injectable()
 export class FileStoreCloudService {
@@ -19,7 +19,7 @@ export class FileStoreCloudService {
     }
 
     async storeFileCloud(fileBuffer: Buffer, fileKey: string): Promise<IFileStoreCloudFile> {
-        const results = await this.s3Client.send(
+        await this.s3Client.send(
             new PutObjectCommand({
                 Bucket: appConfig.storage.bucketName,
                 Key: fileKey,
@@ -27,28 +27,22 @@ export class FileStoreCloudService {
             })
         );
 
-        Logger.debug(`UPLOAD SUCCESS! Uploaded file ${fileKey} to bucket ${appConfig.storage.bucketName}`);
-        const downloadURL = `${appConfig.baseURL_CDN}/${appConfig.storage.bucketName}/${fileKey}`;
-        Logger.debug(`File should be accessible at ${downloadURL}`);
-        Logger.debug(results);
+        Logger.log(`UPLOAD SUCCESS! Uploaded file ${fileKey} to bucket ${appConfig.storage.bucketName}`);
 
-        const hash = this.fsUtil.getBufferHash(fileBuffer);
         return {
-            fileName: fileKey,
-            downloadURL: downloadURL,
-            hash: hash
+            fileKey: fileKey,
+            hash: FileStoreUtils.getBufferHash(fileBuffer)
         };
     }
 
     async deleteFileCloud(fileKey: string): Promise<void> {
-        const results = await this.s3Client.send(
+        await this.s3Client.send(
             new DeleteObjectCommand({
                 Bucket: appConfig.storage.bucketName,
                 Key: fileKey
             })
         );
 
-        Logger.debug(`DELETE SUCCESS! Deleted file ${fileKey} from bucket ${appConfig.storage.bucketName}`);
-        Logger.debug(results);
+        Logger.log(`DELETE SUCCESS! Deleted file ${fileKey} from bucket ${appConfig.storage.bucketName}`);
     }
 }
