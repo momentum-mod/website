e7da778269b4c34ed56064e22694eaf0337dd171
tsa96 <tom@trumpetdust.org>
2022-07-13 09:07:34 +0100

    chore: Refactor FileStoreUtil
    This has no reason to be a service, it's literally just static methods.

---
+++ server/src/modules/filestore/file-store.utility.ts
@@ -0,0 +1,15 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+
+export const FileStoreUtils = {
+    getFileHash: (filePath): Promise<string> =>
+        new Promise((resolve, reject) => {
+            const hash = createHash('sha1').setEncoding('hex');
+            createReadStream(filePath)
+                .pipe(hash)
+                .on('error', (err) => reject(err))
+                .on('finish', () => resolve(hash.read()));
+        }),
+
+    getBufferHash: (buf): string => createHash('sha1').update(buf).digest('hex')
+};
