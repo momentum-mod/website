f54f7f4a600bc70ff94351fcfba9a9d5a8aa555a
tsa96 <tom@trumpetdust.org>
2022-08-05 23:41:15 +0100

    feat: Rework exception handler AGAIN
    We still want this to pass on any objects we throw to a HttpException-based error. Also, even if it's not a Prisma error we still probably don't want to include messages when it 500s.

--- server/src/@common/filters/exception-handler.filter.ts
+++ server/src/@common/filters/exception-handler.filter.ts
@@ -10,32 +10,41 @@ export class ExceptionHandlerFilter implements ExceptionFilter {
         private readonly sentryService: SentryExceptionService
     ) {}
 
-    catch(e: any, host: ArgumentsHost): void {
+    private readonly logger = new Logger('Exception Filter');
+
+    catch(exception: any, host: ArgumentsHost): void {
         const { httpAdapter } = this.httpAdapterHost;
         const context = host.switchToHttp();
 
-        let sentryErrorCode;
-        const statusCode = e instanceof HttpException ? e.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+        let sentryErrorCode, status, responseBody;
 
-        const response = {
-            statusCode: statusCode,
-            // Don't send the specifics of any database-related error
-            message: e instanceof PrismaClientKnownRequestError ? 'Database Error' : e.message
-        };
+        if (exception instanceof HttpException) {
+            status = exception.getStatus();
+            const res = exception.getResponse();
+            responseBody = typeof res == 'string' ? { message: res } : res;
+        }
+        // If it's not a Nest HttpException just 500 and don't include any extra data
+        else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            responseBody = {
+                message: exception instanceof PrismaClientKnownRequestError ? 'Database Error' : 'Internal Server Error'
+            };
+        }
 
         // We don't care about logging 400s
-        if (!(statusCode && statusCode >= 400 && statusCode < 500)) {
-            sentryErrorCode = this.sentryService.sendError(e);
-            Logger.error(
-                `${e.name ?? 'Error'}\n` +
+        if (!status || status < 400 || status >= 500) {
+            sentryErrorCode = this.sentryService.sendError(exception);
+            this.logger.error(
+                `${exception.name ?? 'Error'}\n` +
                     `Sentry Code: ${sentryErrorCode}\n` + // TODO: These seem to all just be DEV-ERROR, can we get the Sentry service to give us something unique?
-                    `Message: ${e.message}\n` +
-                    `Stack: ${e.stack}`
+                    `Stack: ${exception.stack}`
             );
         }
 
-        if (sentryErrorCode) response['errorCode'] = sentryErrorCode;
+        responseBody.timestamp = new Date().toISOString();
+        responseBody.path = httpAdapter.getRequestUrl(context.getRequest());
+        if (sentryErrorCode) responseBody.errorCode = sentryErrorCode;
 
-        httpAdapter.reply(context.getResponse(), response, statusCode);
+        httpAdapter.reply(context.getResponse(), responseBody, status);
     }
 }
