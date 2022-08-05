8bdd18327e1e46a12877faa03cdb538405ac8ad2
tsa96 <tom@trumpetdust.org>
2022-08-06 00:10:32 +0100

    feat: DbError for general repo errors

---
+++ server/src/modules/repo/repo.error.ts
@@ -0,0 +1,13 @@
+﻿export class DbError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = 'DbError';
+    }
+}
+
+export class DbNotFoundError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = 'DbNotFoundError';
+    }
+}
