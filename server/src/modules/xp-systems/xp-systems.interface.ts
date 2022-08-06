3edb08081047cf53b2cc795c2006b1cccb094011
tsa96 <tom@trumpetdust.org>
2022-08-06 05:44:47 +0100

    feat: XP Systems module

---
+++ server/src/modules/xp-systems/xp-systems.interface.ts
@@ -0,0 +1,65 @@
+﻿import { Prisma } from '@prisma/client';
+
+export interface RankXpParams extends Prisma.JsonObject {
+    top10: {
+        WRPoints: number;
+        rankPercentages: number[];
+    };
+    formula: {
+        A: number;
+        B: number;
+    };
+    groups: {
+        maxGroups: number;
+        groupScaleFactors: number[];
+        groupExponents: number[];
+        groupMinSizes: number[];
+        groupPointPcts: number[]; // How much, of a % of WRPoints, does each group get
+    };
+}
+
+export interface CosXpParams extends Prisma.JsonObject {
+    levels: {
+        maxLevels: number;
+        startingValue: number;
+        linearScaleBaseIncrease: number;
+        linearScaleInterval: number;
+        linearScaleIntervalMultiplier: number;
+        staticScaleStart: number;
+        staticScaleBaseMultiplier: number;
+        staticScaleInterval: number;
+        staticScaleIntervalMultiplier: number;
+    };
+    completions: {
+        unique: {
+            tierScale: {
+                linear: number;
+                staged: number;
+                // bonus is static, as (tierScale.linear * (initialScale(tier3)) + tierScale.linear * (initialScale(tier4))) / 2
+            };
+        };
+        repeat: {
+            tierScale: {
+                linear: number;
+                staged: number;
+                stages: number;
+                bonus: number; // = staged
+            };
+        };
+    };
+}
+
+export interface XpParams {
+    rankXP: RankXpParams;
+    cosXP: CosXpParams;
+}
+
+export interface RankXpGain {
+    rankXP: number; // The total XP gained
+    formula: number; // The XP gained from formula points
+    top10: number; // The XP gained from Top10 points
+    group: {
+        groupXP: number; // The XP gained from group calculation
+        groupNum: number; // What group they're in
+    };
+}
---
+++ server/src/modules/xp-systems/xp-systems.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { XpSystemsService } from './xp-systems.service';
+import { RepoModule } from '../repo/repo.module';
+
+@Module({
+    imports: [RepoModule],
+    providers: [XpSystemsService],
+    exports: [XpSystemsService]
+})
+export class XpSystemsModule {}
