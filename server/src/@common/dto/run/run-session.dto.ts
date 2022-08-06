3f19f0f3bcfaf9c1b5a05dc5a6d844ec4aa93700
tsa96 <tom@trumpetdust.org>
2022-08-06 05:38:51 +0100

    feat: Add new run session and UMR DTOs

---
+++ server/src/@common/dto/run/run-session.dto.ts
@@ -0,0 +1,81 @@
+﻿import { ApiProperty, PickType } from '@nestjs/swagger';
+import { RunSession } from '@prisma/client';
+import { IsDateString, IsDefined, IsInt, IsPositive } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class RunSessionDto implements RunSession {
+    @ApiProperty({
+        type: String,
+        description: 'The ID of the run run'
+    })
+    @Transform(({ value }) => BigInt(value))
+    @IsDefined()
+    id: bigint; // TODO: Do we need BigInt here? Can we have IDs be reused when autoincrementing, after old sessions are deleted?
+
+    @ApiProperty({
+        description: 'The number of the track the run is on, 0 is main track',
+        type: Number,
+        required: true
+    })
+    @IsInt()
+    trackNum: number;
+
+    @ApiProperty({
+        description: 'The zone the run is on. Non-zero will imply an IL run, which are not yet implemented',
+        type: Number,
+        required: true
+    })
+    @IsInt()
+    zoneNum: number;
+
+    @ApiProperty({
+        description: 'The ID of the user submitting the run',
+        type: Number,
+        required: true
+    })
+    userID: number;
+
+    @ApiProperty({
+        description: 'The ID of the MapTrack the run is on',
+        type: Number,
+        required: true
+    })
+    trackID: number;
+
+    @ApiProperty()
+    @IsDateString()
+    createdAt: Date;
+
+    @ApiProperty()
+    @IsDateString()
+    updatedAt: Date;
+}
+
+export class CreateRunSessionDto extends PickType(RunSessionDto, ['trackNum', 'zoneNum' as const]) {
+    @ApiProperty({
+        description: 'The map the run is on',
+        type: Number,
+        required: true
+    })
+    @IsInt()
+    mapID: number;
+}
+
+export class UpdateRunSessionDto {
+    @ApiProperty({
+        description: 'The zone the run is on',
+        type: Number,
+        required: true
+    })
+    @IsInt()
+    zoneNum: number;
+
+    @ApiProperty({
+        description: 'The run tick at time of request',
+        type: Number,
+        required: true
+    })
+    @IsInt()
+    @IsPositive()
+    tick: number;
+}
---
+++ server/src/@common/dto/run/user-map-rank.dto.ts
@@ -0,0 +1,74 @@
+﻿import { User, UserMapRank, Map as MapDB } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsDefined, IsInt, ValidateNested } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { DtoFactory } from '../../utils/dto.utility';
+import { RunDto } from './runs.dto';
+import { UserDto } from '../user/user.dto';
+import { MapDto } from '../map/map.dto';
+
+export class UserMapRankDto implements UserMapRank {
+    @ApiProperty({ description: 'The gamemode of the run' })
+    @IsDefined()
+    @IsInt()
+    gameType: number;
+
+    @ApiProperty({ description: 'Unimplemented' })
+    @IsInt()
+    flags: number;
+
+    @ApiProperty({ description: 'The track the run is on' })
+    @IsDefined()
+    @IsInt()
+    trackNum: number;
+
+    @ApiProperty({ description: 'The zone the run is on. > 0 is a IL run, not yet supported' })
+    @IsDefined()
+    @IsInt()
+    zoneNum: number;
+
+    @ApiProperty({ description: 'The leaderboard rank of the run' })
+    @IsDefined()
+    @IsInt()
+    rank: number;
+
+    @ApiProperty({ description: 'The ranked XP assigned for the run' })
+    @IsDefined()
+    @IsInt()
+    rankXP: number;
+
+    @ApiProperty()
+    @ValidateNested()
+    @Transform(({ value }) => DtoFactory(MapDto, value))
+    map: MapDB;
+
+    @ApiProperty()
+    @IsInt()
+    mapID: number;
+
+    @ApiProperty()
+    @ValidateNested()
+    @Transform(({ value }) => DtoFactory(UserDto, value))
+    user: User;
+
+    @ApiProperty()
+    @IsInt()
+    userID: number;
+
+    @ApiProperty()
+    @ValidateNested()
+    @Transform(({ value }) => DtoFactory(RunDto, value))
+    run: RunDto;
+
+    @ApiProperty()
+    @IsInt()
+    runID: bigint;
+
+    @ApiProperty()
+    @IsDateString()
+    createdAt: Date;
+
+    @ApiProperty()
+    @IsDateString()
+    updatedAt: Date;
+}
