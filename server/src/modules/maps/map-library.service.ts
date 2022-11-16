db91ed97ee731e5706b14714948c5669b1780d55
thejazzagent <etthewalrus@gmail.com>
2022-11-16 14:33:07 -0800

    feat: /nestJS/user/maps/library/mapid/GET

---
+++ server/src/modules/maps/map-library.service.ts
@@ -0,0 +1,40 @@
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException
+} from '@nestjs/common';
+import { PrismaService } from '../repo/prisma.service';
+import {
+    Prisma
+} from '@prisma/client';
+
+
+@Injectable()
+export class MapLibraryService {
+    constructor( private prisma: PrismaService ) {}
+
+    async isMapInLibrary(userID: number, mapID: number) {
+        const mapWhere: Prisma.MapWhereInput = {
+            id: mapID
+        };
+
+        const map = await this.prisma.map.findFirst({
+            where: mapWhere
+        });
+
+        if ( !map ) {
+            throw new BadRequestException('Map does not exist');
+        }
+
+        const where: Prisma.MapLibraryEntryWhereInput = {
+            userID: userID,
+            mapID: mapID
+        };
+
+        const dbResponse = await this.prisma.mapLibraryEntry.findFirst({
+            where: where,
+        });
+
+        if (!dbResponse) throw new NotFoundException('Map not found');
+    }
+}
