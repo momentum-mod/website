79f029ca5cd6e1ed778bed35e221cdc0edb93123
tsa96 <tom@trumpetdust.org>
2022-11-19 18:15:09 +0000

    fix: Broken passport import

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Req, Res, Controller, Get, Post, UseGuards, RawBodyRequest } from '@nestjs/common';
 import { Request } from 'express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import * as passport from 'passport';
+import passport from 'passport';
 import { Public } from '@common/decorators/public.decorator';
 import { AuthService } from './auth.service';
 import { SteamWebAuthGuard } from '@common/guards/steam-web-auth.guard';
