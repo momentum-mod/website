7127b000c5c59698115b9751a60aadcda1d05dd6
tsa96 <tom@trumpetdust.org>
2022-12-14 03:20:47 +0000

    feat: Raw body parsing

---
+++ server/src/middlewares/json-body.middleware.ts
@@ -0,0 +1,9 @@
+﻿import express, { Request, Response } from 'express';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class JsonBodyMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: () => any) {
+        express.json()(req, res, next);
+    }
+}
---
+++ server/src/middlewares/raw-body.middleware.ts
@@ -0,0 +1,9 @@
+﻿import { Injectable, NestMiddleware } from '@nestjs/common';
+import express, { Request, Response } from 'express';
+
+@Injectable()
+export class RawBodyMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: () => any) {
+        express.raw({ type: '*/*' })(req, res, next);
+    }
+}
