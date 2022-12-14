2d7f6386d3680b84d359e3b861b336ba17c820f8
tsa96 <tom@trumpetdust.org>
2022-12-14 03:54:52 +0000

    feat: Scaffold auth tests
    Doing the bulk of these after Fastify.

---
+++ server/src/modules/auth/auth.service.spec.ts
@@ -0,0 +1,20 @@
+﻿import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+    let service: AuthService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [AuthService]
+        }).compile();
+
+        service = module.get<AuthService>(AuthService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    // TODO: We should write unit tests for this service. I just don't have the energy atm :)
+});
---
+++ server/tests/e2e/auth.e2e-spec.ts
@@ -0,0 +1,19 @@
+﻿// noinspection DuplicatedCode
+
+import request from 'supertest';
+
+describe('Auth', () => {
+    describe('GET /auth', () => {
+        describe('GET /auth/steam', () => {
+            it('should redirect to steam login', async () => {
+                await request(global.server)
+                    .get('/auth/steam')
+                    .expect(302)
+                    .expect('Location', /^https:\/\/steamcommunity.com\/openid\/login.+/)
+            });
+        });
+    });
+
+    // TODO: We should try and add many more tests here, I'm just unsure how best to bypass the Passport
+    // IIRC there's some good info on SO so check that out in the future, once/if we've moved to Fastify.
+});
