39a4eec941716aea94c20fb2c3a673850bf406aa
tsa96 <tom@trumpetdust.org>
2023-01-31 14:13:29 +0000

    fix: Messed up session module

--- server/src/modules/session/session.module.ts
+++ server/src/modules/session/session.module.ts
@@ -3,9 +3,11 @@ import { RunSessionService } from './run/run-session.service';
 import { RepoModule } from '../repo/repo.module';
 import { FileStoreModule } from '../filestore/file-store.module';
 import { XpSystemsModule } from '../xp-systems/xp-systems.module';
+import { SessionController } from '@modules/session/session.controller';
 
 @Module({
     imports: [RepoModule, FileStoreModule, XpSystemsModule],
+    controllers: [SessionController],
     providers: [RunSessionService],
     exports: [RunSessionService]
 })
