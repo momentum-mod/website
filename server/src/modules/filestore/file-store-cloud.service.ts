0d7695c2c9e631a7d6eaeaff43953ddd09eabb8b
tsa96 <tom@trumpetdust.org>
2023-03-08 09:49:04 +0000

    chore: Remove pointless logging from file store service

--- server/src/modules/filestore/file-store-cloud.service.ts
+++ server/src/modules/filestore/file-store-cloud.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, StreamableFile } from '@nestjs/common';
+import { Injectable, StreamableFile } from '@nestjs/common';
 import { FileStoreCloudFile } from './file-store.interfaces';
 import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { FileStoreUtils } from './file-store.utility';
@@ -7,8 +7,6 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class FileStoreCloudService {
     s3Client: S3Client;
-
-    private readonly logger = new Logger('File Store Cloud');
 
     constructor(private readonly config: ConfigService) {
         this.s3Client = new S3Client({
@@ -31,8 +29,6 @@ export class FileStoreCloudService {
             })
         );
 
-        this.logger.log(`UPLOAD SUCCESS! Uploaded file ${fileKey} to bucket ${this.config.get('storage.bucketName')}`);
-
         return {
             fileKey: fileKey,
             hash: FileStoreUtils.getBufferHash(fileBuffer)
@@ -46,20 +42,18 @@ export class FileStoreCloudService {
                 Key: fileKey
             })
         );
-
-        this.logger.log(`DELETE SUCCESS! Deleted file ${fileKey} from bucket ${this.config.get('storage.bucketName')}`);
     }
 
     async getFileCloud(fileKey: string): Promise<StreamableFile | undefined> {
         try {
-            const cmdRes = await this.s3Client.send(
+            const commandResponse = await this.s3Client.send(
                 new GetObjectCommand({
                     Bucket: this.config.get('storage.bucketName'),
                     Key: fileKey
                 })
             );
 
-            return new StreamableFile(await cmdRes.Body.transformToByteArray());
+            return new StreamableFile(await commandResponse.Body.transformToByteArray());
         } catch (error) {
             // If file isn't found, just return undefined and let the service handle 404 behaviour.
             if (error?.Code === 'NoSuchKey') return;
