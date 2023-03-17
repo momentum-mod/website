9b3d230ffaab86bd775d2c2b0ec83238b10825d7
tsa96 <tom@trumpetdust.org>
2023-03-17 00:30:42 +0000

    refactor: Remove useless data in MapImage

--- server/src/common/dto/map/map-image.dto.ts
+++ server/src/common/dto/map/map-image.dto.ts
@@ -1,25 +1,34 @@
 import { MapImage } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl } from 'class-validator';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { CreatedAtProperty, IdProperty, UpdatedAtProperty } from '@lib/dto.lib';
+import { Config } from '@config/config';
 
 export class MapImageDto implements MapImage {
     @IdProperty()
     id: number;
 
-    // TODO: Specify exact resolutions
-    @ApiProperty({ type: String, description: 'URL to low resolution image file' })
-    @IsUrl()
-    small: string;
+    @ApiProperty({ type: String, description: 'URL to low resolution (480x360) JPEG image file' })
+    @Expose()
+    @IsUrl({ require_tld: false })
+    get small(): string {
+        return `${Config.url.cdn}/${Config.storage.bucketName}/${this.id}-small.jpg`;
+    }
 
-    @ApiProperty({ type: String, description: 'URL to medium resolution image file' })
-    @IsUrl()
-    medium: string;
+    @ApiProperty({ type: String, description: 'URL to medium resolution (1280x720) JPEG image file' })
+    @Expose()
+    @IsUrl({ require_tld: false })
+    get medium(): string {
+        return `${Config.url.cdn}/${Config.storage.bucketName}/${this.id}-medium.jpg`;
+    }
 
-    @ApiProperty({ type: String, description: 'URL to high resolution image file' })
-    @IsUrl()
-    large: string;
+    @ApiProperty({ type: String, description: 'URL to high resolution (1920x1080) JPEG image file' })
+    @Expose()
+    @IsUrl({ require_tld: false })
+    get large(): string {
+        return `${Config.url.cdn}/${Config.storage.bucketName}/${this.id}-large.jpg`;
+    }
 
     @Exclude()
     mapID: number;
