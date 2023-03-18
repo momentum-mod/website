7b05e2ec6b6791a6572e12f4b07d2f0c3e7f8243
tsa96 <tom@trumpetdust.org>
2023-03-18 01:31:27 +0000

    feat: Add UserStatsDto

---
+++ server/src/common/dto/user/user-stats.dto.ts
@@ -0,0 +1,44 @@
+﻿import { UserStats } from '@prisma/client';
+import { CreatedAtProperty, IdProperty, UpdatedAtProperty } from '@lib/dto.lib';
+import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt } from 'class-validator';
+import { IsPositiveNumberString } from '@common/validators/is-positive-number-string.validator';
+
+export class UserStatsDto implements UserStats {
+    @IdProperty()
+    id: number;
+
+    @Exclude()
+    userID: number;
+
+    @ApiProperty({ type: String, description: "The user's total cosmetic XP" })
+    @IsPositiveNumberString()
+    cosXP: bigint;
+
+    @ApiProperty({ type: String, description: "The user's level" })
+    @IsInt()
+    level: number;
+
+    @ApiProperty({ type: String, description: 'Total maps completed' })
+    @IsInt()
+    mapsCompleted: number;
+
+    @ApiProperty({ type: String, description: 'Total runs submitted' })
+    @IsInt()
+    runsSubmitted: number;
+
+    @ApiProperty({ type: String, description: "The user's total number of jumps" })
+    @IsPositiveNumberString()
+    totalJumps: bigint;
+
+    @ApiProperty({ type: String, description: "The user's total number of strafes" })
+    @IsPositiveNumberString()
+    totalStrafes: bigint;
+
+    @CreatedAtProperty()
+    createdAt: Date;
+
+    @UpdatedAtProperty()
+    updatedAt: Date;
+}
