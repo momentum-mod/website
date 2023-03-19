9935149116e77ad87c898180b40c355187d81f07
tsa96 <tom@trumpetdust.org>
2023-03-19 22:17:20 +0000

    refactor: Move runs logic in users.service to runs.service

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -4,11 +4,13 @@ import { UsersController } from './users.controller';
 import { RepoModule } from '../repo/repo.module';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { RunsModule } from '@modules/runs/runs.module';
+import { RunsService } from '@modules/runs/runs.service';
 
 @Module({
-    imports: [RepoModule, HttpModule, ConfigModule],
+    imports: [RepoModule, HttpModule, ConfigModule, RunsModule],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, RunsService],
     exports: [UsersService]
 })
 export class UsersModule {}
