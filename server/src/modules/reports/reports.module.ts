a8372e68f2fa92836d52e7b19b2657b21159e1e6
benjl <benluijkx@gmail.com>
2023-03-20 01:08:02 -0600

    feat: Add max daily report limit

--- server/src/modules/reports/reports.module.ts
+++ server/src/modules/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { RepoModule } from '../repo/repo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [RepoModule],
+    imports: [RepoModule, ConfigModule],
     controllers: [ReportsController],
     providers: [ReportsService],
     exports: [ReportsService]
