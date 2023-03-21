62791975bbb18cbe56576829aa13640354fe2ddf
tsa96 <tom@trumpetdust.org>
2023-03-21 08:39:59 +0000

    feat: Replace Express with Fastify
    Bye!!!

--- server/src/middlewares/http-logger.middleware.ts
+++ server/src/middlewares/http-logger.middleware.ts
@@ -1,19 +1,22 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { FastifyReply, FastifyRequest } from 'fastify';
 
 @Injectable()
 export class HTTPLoggerMiddleware implements NestMiddleware {
     private logger = new Logger('HTTP');
 
-    use(request: Request, response: Response, next: NextFunction): void {
-        const { ip, method, originalUrl } = request;
-        const userAgent = request.get('user-agent') || '';
+    use(request: FastifyRequest['raw'], response: FastifyReply['raw'], next: () => void): void {
+        const {
+            method,
+            url,
+            connection: { remoteAddress: ip },
+            headers: { ['user-agent']: userAgent }
+        } = request;
 
         response.on('finish', () => {
             const { statusCode } = response;
-            const contentLength = response.get('content-length');
 
-            this.logger.log(`${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`);
+            this.logger.log(`${method} ${url} ${statusCode} - ${userAgent} ${ip}`);
         });
 
         next();
