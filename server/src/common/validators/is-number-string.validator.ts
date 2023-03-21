891ae2633fb9e1a4210f1d060fb03d85c04bdbd2
tsa96 <tom@trumpetdust.org>
2023-03-21 15:46:43 +0000

    feat: IsNumberString validator

--- server/src/common/validators/is-number-string.validator.ts
+++ server/src/common/validators/is-number-string.validator.ts
@@ -1,23 +1,23 @@
 ﻿import { registerDecorator, ValidationOptions } from 'class-validator';
 
-export function IsPositiveNumberString(validationOptions?: ValidationOptions) {
+export function IsNumberString(validationOptions?: ValidationOptions) {
     return function (object: unknown, propertyName: string) {
         registerDecorator({
-            name: 'isPositiveNumberString',
+            name: 'isNumberString',
             target: object.constructor,
             propertyName: propertyName,
             options: validationOptions,
             validator: {
                 validate(value: any) {
                     try {
-                        return typeof value === 'string' && BigInt(value) > 0;
+                        return typeof value === 'string' && BigInt(value) >= 0;
                     } catch {
                         return false;
                     }
                 },
 
                 defaultMessage() {
-                    return `${propertyName} must be a string representing a positive number`;
+                    return `${propertyName} must be a string representing a number`;
                 }
             }
         });
