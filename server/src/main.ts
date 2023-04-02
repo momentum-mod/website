e2004314f824147949beb8efd88e0337104b0838
tsa96 <tom@trumpetdust.org>
2023-04-02 06:57:45 +0100

    chore: Commenting in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
     // Transforms `BigInt`s to strings in JSON.stringify, for cases that haven't been explicitly
-    // transformed to numbers using @NumberifyBigInt().
+    // transformed to numbers using @NumberifyBigInt()
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
     BigInt.prototype['toJSON'] = function () {
         return this.toString();
@@ -40,30 +40,35 @@ async function bootstrap() {
         });
 
     app.register(fastifyCookie, { secret: configService.get('sessionSecret') });
+
+    // Just for Swagger assets
     app.useStaticAssets({ root: join(__dirname, 'assets/') });
 
     // Forbidding unknown values here ensures any request containing unexpected data on the query/body (i.e. does not
     // have validators) will fail. Our tests even more strict: passing an unexpected value will throw an error.
-    // In effect, you MUST include validation decorators.
+    // In effect, you MUST include validation decorators
     app.useGlobalPipes(new ValidationPipe({ transform: true, forbidUnknownValues: true }));
     app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
+    // Prefix everything by auth with /apid
     app.setGlobalPrefix('api', { exclude: ['auth(.*)'] });
 
     // All routes (besides auth, which uses VERSION_NEUTRAL) are version 1 by default,
     // versions can be incremented on a per-route basis upon future versions
     app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1', prefix: 'v' });
 
+    // Hooks to ensure Nest and Prisma both shut down cleanly on exit
+    // https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
     const prismaDalc: PrismaService = app.get(PrismaService);
     await prismaDalc.enableShutdownHooks(app);
 
+    // Swagger stuff
     const swaggerConfig = new DocumentBuilder()
         .setTitle('Momentum Mod API')
         .setDescription('The Momentum Mod API - https://github.com/momentum-mod/website')
         .addBearerAuth()
         .setVersion('1.0')
         .build();
-
     const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
     await SwaggerModule.setup('api-docs', app, swaggerDocument, {
         customSiteTitle: 'Momentum Mod API Docs',
@@ -73,10 +78,8 @@ async function bootstrap() {
         }
     });
 
-    const configService = app.get(ConfigService);
-    const port = configService.get('port');
-
-    await app.listen(port);
+    // Here we fucking go!!!
+    await app.listen(configService.get('port'));
 }
 
 bootstrap();
