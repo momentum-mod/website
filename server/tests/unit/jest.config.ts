9d194b8ced5bfaa749f6f7cfddf02235c10f8834
tsa96 <tom@trumpetdust.org>
2023-04-03 08:42:37 +0100

    chore: Split up unit and e2e tests

---
+++ server/tests/unit/jest.config.ts
@@ -0,0 +1,16 @@
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from '../../tsconfig.json';
+
+const UnitTestConfig: JestConfigWithTsJest = {
+    displayName: 'Unit Tests',
+    preset: 'ts-jest',
+    rootDir: '../../',
+    roots: ['<rootDir>'],
+    setupFilesAfterEnv: ['<rootDir>/tests/matchers.ts'],
+    testTimeout: 1000,
+    testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+};
+
+export default UnitTestConfig;
