f789e66b78af0c8095c1ccf31553d4b1833302fe
tsa96 <tom@trumpetdust.org>
2023-04-11 06:30:42 +0100

    chore: Remove unused middlewares

--- server/src/middlewares/raw-body.middleware.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Injectable, NestMiddleware } from '@nestjs/common';
-import express, { Request, Response } from 'express';
-
-@Injectable()
-export class RawBodyMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: () => any) {
-        express.raw({ type: '*/*' })(req, res, next);
-    }
-}
