83ce929b0f80826beb158239837c43e4b9da6d9b
tsa96 <tom@trumpetdust.org>
2023-04-15 05:14:24 +0100

    feat: Add REPL mode
    This was useful for messing around with XpSystemsService, nice little feature Nest added recently.

---
+++ server/src/repl.ts
@@ -0,0 +1,13 @@
+﻿import { repl } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+/**
+ * Run this file via `npm run start:repl` launch the API in REPL (Read-Eval-Print-Loop) mode,
+ * enabling you to interact with running providers via the command line.
+ *
+ * See https://docs.nestjs.com/recipes/repl for more info.
+ *
+ * Note: Currently FastifyMulterModule fails to launch in this configuration. For now, just
+ * comment out the import in app.module.ts temporaily to use this.
+ */
+(async () => await repl(AppModule))();
