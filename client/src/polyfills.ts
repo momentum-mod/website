291f05e830e46d67b863d931a5ea99e4c1ac5b81
tsa96 <tom@trumpetdust.org>
2023-04-18 09:49:30 +0100

    refactor(front): remove classlist.js
    We do not remotely care about IE<=11 users lmao

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -38,9 +38,6 @@ import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/set';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
