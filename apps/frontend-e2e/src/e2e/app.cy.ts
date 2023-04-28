7404aad547f523de09260a5a4b04ff6f38107796
tsa96 <tom@trumpetdust.org>
2023-04-28 13:45:29 +0100

    test(front): stub Cypress E2E tests

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,10 @@
-import { getGreeting } from '../support/app.po';
-
 describe('frontend', () => {
-  beforeEach(() => cy.visit('/'));
-
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome frontend');
+    // Not much of a test, but serves to stub frontend E2E testing for now,
+    // and tests that the frontmatter site displays in a real browser
+    // without errors.
+    cy.visit('/');
+    cy.get('header h2').should('contain', 'movement');
+    cy.get('header h2').should('not.contain', 'smovement');
   });
 });
--- apps/frontend-e2e/src/support/commands.ts
+++ apps/frontend-e2e/src/support/commands.ts
@@ -10,7 +10,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line unused-imports/no-unused-vars
   interface Chainable<Subject> {
     login(email: string, password: string): void;
   }
