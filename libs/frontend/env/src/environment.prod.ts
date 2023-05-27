9cd1948d70252e169b37e372a1f2d5c11ba883ca
tsa96 <tom@trumpetdust.org>
2023-05-27 07:09:44 +0100

    feat(front): environment interface

--- libs/frontend/env/src/environment.prod.ts
+++ libs/frontend/env/src/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { Environment } from './environment.interface';
+
+export const env: Environment = {
   production: true,
   api: 'https://api.momentum-mod.org',
   auth: 'https://api.momentum-mod.org/auth'
--- libs/frontend/env/src/environment.ts
+++ libs/frontend/env/src/environment.ts
@@ -1,4 +1,6 @@
-export const env = {
+import { Environment } from './environment.interface';
+
+export const env: Environment = {
   production: false,
   api: 'http://localhost:3000',
   auth: 'http://localhost:3000/auth'
