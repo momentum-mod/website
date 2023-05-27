15d269a99c3f6bb80612f15a528e51fb5137709f
tsa96 <tom@trumpetdust.org>
2023-05-27 04:57:50 +0100

    refactor(front): remove outdated blog component

--- apps/frontend/src/app/pages/dashboard/community/community-news/community-news.component.ts
+++ apps/frontend/src/app/pages/dashboard/community/community-news/community-news.component.ts
@@ -1,48 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TumblrAPIService } from '../../../../@core/data/tumblr-api.service';
-import { BlogPost } from '../../../../@core/models/blog-post.model';
-import { finalize } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mom-community-news',
   templateUrl: './community-news.component.html',
   styleUrls: ['./community-news.component.scss']
 })
-export class CommunityNewsComponent implements OnInit {
-  blogPosts: BlogPost[];
-  loadedPosts: boolean;
-  twitterData;
-  twitterOpts;
-  constructor(private tumblrAPI: TumblrAPIService) {
-    this.blogPosts = [];
-    this.loadedPosts = false;
-    this.twitterData = {
-      screenName: 'MomentumMod',
-      sourceType: 'profile'
-    };
-    this.twitterOpts = {
-      tweetLimit: 4,
-      chrome: ['nofooter'],
-      theme: 'dark'
-    };
-  }
-
-  ngOnInit() {
-    this.tumblrAPI
-      .getRecentBlogPosts()
-      .pipe(finalize(() => (this.loadedPosts = true)))
-      .subscribe((response) => {
-        if (response.response && response.response.posts) {
-          for (const post of response.response.posts) {
-            this.blogPosts.push({
-              title: post.title,
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              post_url: post.post_url,
-              body: post.body.slice(0, 500) + '...',
-              timestamp: new Date(post.timestamp * 1000)
-            });
-          }
-        }
-      });
-  }
-}
+export class CommunityNewsComponent {}
