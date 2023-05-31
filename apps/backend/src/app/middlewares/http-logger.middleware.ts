e5e358d381292406f35a472a5054961ecb1c8532
tsa96 <tom@trumpetdust.org>
2023-05-31 07:43:46 +0100

    fix(back): show full path in request logs

--- apps/backend/src/app/middlewares/http-logger.middleware.ts
+++ apps/backend/src/app/middlewares/http-logger.middleware.ts
@@ -12,10 +12,13 @@ export class HTTPLoggerMiddleware implements NestMiddleware {
   ): void {
     const {
       method,
-      url,
       socket: { remoteAddress: ip },
       headers: { ['user-agent']: userAgent }
     } = request;
+
+    // request.url only contains params (e.g. '/?expand=info'  not the full URL
+    // path. Hack to access the full path (e.g. '/api/v1/maps?expand=info'
+    const url = (request as any).originalUrl;
 
     response.on('finish', () => {
       const { statusCode } = response;
