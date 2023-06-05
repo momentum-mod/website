8aeadebe03ac507de0bd1e027e126c43f00fd1d7
tsa96 <tom@trumpetdust.org>
2023-06-05 01:11:44 +0100

    feat(meta): add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'tmp'
+      ]
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'front',
+        'front-e2e',
+        'back',
+        'back-e2e',
+        'shared',
+        'meta',
+        'db',
+        'utils',
+        'discord',
+        'twitch'
+      ]
+    ],
+    'subject-case': [
+      1,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ]
+  }
+};
