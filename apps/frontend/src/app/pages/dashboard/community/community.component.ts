50607b365bf4d89aa22e954c0b4e718bc71bfa57
tsa96 <tom@trumpetdust.org>
2023-06-11 05:57:47 +0100

    fix(front): wrap lazy-loaded components in <nb-layout>

--- apps/frontend/src/app/pages/dashboard/community/community.component.ts
+++ apps/frontend/src/app/pages/dashboard/community/community.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
+import { NB_LAYOUT_WRAPPED_OUTLET } from '../page-outlet';
 
-@Component({
-  template: '<router-outlet></router-outlet>'
-})
+@Component({ template: NB_LAYOUT_WRAPPED_OUTLET })
 export class CommunityComponent {}
