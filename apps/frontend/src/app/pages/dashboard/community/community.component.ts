fcbddd29abe06a6f3819ea5f2664ea5c90e08bd4
tsa96 <tom@trumpetdust.org>
2023-07-02 21:43:58 +0100

    fix(front): add selectors to admin+community comps to avoid id collision

--- apps/frontend/src/app/pages/dashboard/community/community.component.ts
+++ apps/frontend/src/app/pages/dashboard/community/community.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
 import { NB_LAYOUT_WRAPPED_OUTLET } from '../page-outlet';
 
-@Component({ template: NB_LAYOUT_WRAPPED_OUTLET })
+@Component({ selector: 'mom-community', template: NB_LAYOUT_WRAPPED_OUTLET })
 export class CommunityComponent {}
