40f8a19b68472619202c4c00930911952a504fda
tsa96 <tom@trumpetdust.org>
2023-07-03 02:21:40 +0100

    feat(front): add momentum icon
    Super tiny single-path version I just made (is also on TS)

--- libs/frontend/icons/src/momentum.icons.ts
+++ libs/frontend/icons/src/momentum.icons.ts
@@ -1,14 +1,3 @@
-/**
- * List of SVG icons to bundle, as strings. We use icons from 3 sources:
- * 1. Material Design Icons [mdi] (https://pictogrammers.com/library/mdi/)
- *    For general purpose icons.
- * 2. Simple Icons [si] (https://simpleicons.org/)
- *    For brands.
- * 3. Custom made [mom]
- *    Anything unique to Momentum. Usually made by Traz or Tom, feel free
- *    to reach out to us if you need anything specific.
- *
- * Any icons used within the Angular application *must* be exported here so
- * it gets bundled properly. An Angular directive will yell at you if you don't.
- * */
-export const momArrowRight = 'M10.4 2 3.6 2 13.6 12 3.6 22 10.4 22 20.4 12z';
+export const logo =
+  'm4.1 12 4-4 4 4-4 4-4-4zm6.3-10H3.6l10 10-10 10h6.8l10-10-10-10z';
+export const arrowRight = 'M10.4 2 3.6 2 13.6 12 3.6 22 10.4 22 20.4 12z';
