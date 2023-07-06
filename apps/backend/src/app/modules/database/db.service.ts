974b15027d40e27deaeb70a15ea30371358f9ecc
tsa96 <tom@trumpetdust.org>
2023-07-06 06:26:36 +0100

    feat(db): add extended Prisma client

--- apps/backend/src/app/modules/database/db.service.ts
+++ apps/backend/src/app/modules/database/db.service.ts
@@ -1,14 +1,24 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { ExtendPrismaClient } from '@momentum/db';
+
+const prisma = new PrismaClient();
+
+// https://github.com/prisma/prisma/issues/18628#issuecomment-1601958220
+const ExtendedPrismaClientProxy = () =>
+  new Proxy(class {}, {
+    construct(target, args, newTarget) {
+      return Object.assign(
+        Reflect.construct(target, args, newTarget),
+        ExtendPrismaClient(prisma)
+      );
+    }
+  }) as new () => ReturnType<typeof ExtendPrismaClient>;
 
 @Injectable()
-export class DbService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-
+export class DbService extends ExtendedPrismaClientProxy() {
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
+    Prisma.getExtensionContext(prisma).$on('beforeExit', async () => {
       await app.close();
     });
   }
