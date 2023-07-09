479a8d51efa3240bf9b4c8356e6abbb713aaa22f
tsa96 <tom@trumpetdust.org>
2023-07-09 09:15:18 +0100

    feat(front): social icons

--- libs/frontend/icons/src/simple-icons.icons.ts
+++ libs/frontend/icons/src/simple-icons.icons.ts
@@ -4,7 +4,14 @@ import {
   siGithub,
   siTwitter,
   siTwitch,
-  siYoutube
+  siYoutube,
+  siMastodon,
+  siLinkedin,
+  siSpotify,
+  siPatreon,
+  siKofi,
+  siPaypal,
+  siInstagram
 } from 'simple-icons';
 
 export const steam = siSteam.path;
@@ -13,3 +20,10 @@ export const github = siGithub.path;
 export const twitter = siTwitter.path;
 export const twitch = siTwitch.path;
 export const youtube = siYoutube.path;
+export const mastodon = siMastodon.path;
+export const linkedin = siLinkedin.path;
+export const spotify = siSpotify.path;
+export const patreon = siPatreon.path;
+export const kofi = siKofi.path;
+export const paypal = siPaypal.path;
+export const instagram = siInstagram.path;
