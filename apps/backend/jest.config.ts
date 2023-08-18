c2531adce40ca511dfe24e6907e608df4a5d103c
tsa96 <tom@trumpetdust.org>
2023-08-18 18:11:34 +0100

    chore(back): move backend test files to own dir

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -2,7 +2,7 @@ export default {
   displayName: 'backend',
   preset: '../../jest.preset.js',
   roots: ['<rootDir>'],
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
