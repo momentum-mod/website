daae10b38e485efbf25ee0985ebfbcbbd3d013b2
tsa96 <tom@trumpetdust.org>
2023-08-20 19:47:55 +0100

    chore(utils): add util-fn library

---
+++ libs/util-fn/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'util-fn',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html']
+};
