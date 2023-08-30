3db924f0b6c52fd266e451b8da593501d3c83ff0
tsa96 <tom@trumpetdust.org>
2023-08-30 10:05:34 +0100

    feat(utils): is-object
    WHY does JS not have this natively

---
+++ libs/util-fn/src/is-object.spec.ts
@@ -0,0 +1,19 @@
+import { isObject } from './is-object';
+
+describe('isObject', () => {
+  it('should return true for objects', () => {
+    expect(isObject({})).toBe(true);
+    expect(isObject({ a: 1 })).toBe(true);
+    expect(isObject(new Date())).toBe(true);
+  });
+
+  it('should return false for non-objects', () => {
+    expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
+    expect(isObject(42)).toBe(false);
+    expect(isObject('string')).toBe(false);
+    expect(isObject(true)).toBe(false);
+    expect(isObject([1, 2, 3])).toBe(false);
+    expect(isObject(() => {})).toBe(false);
+  });
+});
