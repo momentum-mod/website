acf8ae8370121e11163187bc86315058d55f1c24
tsa96 <tom@trumpetdust.org>
2023-10-27 15:01:42 +0100

    test(formats): add zone file stubs
    These are some valid zone files that we're gonna use frequently in E2E and unit tests.

---
+++ libs/formats/src/zone/zones.stub.ts
@@ -0,0 +1,288 @@
+import { Gamemode as GM, MapZones, TrackType } from '@momentum/constants';
+
+/**
+ * An example MapZones object for use in testing.
+ *
+ * It contains
+ * - a main track with
+ *   - 2 major checkpoints, each with a
+ *    - start zone and
+ *    - 1 minor checkpoint
+ *   - end zone
+ * - 2 stage tracks, for both major checkpoints on main track
+ * - 1 bonus, with just a start and end zone
+ */
+// Warning: multiple tests rely on this, do NOT change. If you need a change,
+// go make another one!
+export const ZonesStub: MapZones = {
+  formatVersion: 1,
+  dataTimestamp: 1697451975363,
+  tracks: {
+    main: {
+      name: 'Main',
+      majorOrdered: true,
+      minorRequired: true,
+      zones: {
+        segments: [
+          {
+            limitStartGroundSpeed: true,
+            checkpoints: [{ volumeIndex: 0 }, { volumeIndex: 1 }]
+          },
+          {
+            limitStartGroundSpeed: true,
+            checkpoints: [{ volumeIndex: 2 }, { volumeIndex: 3 }]
+          }
+        ],
+        end: { volumeIndex: 4 }
+      }
+    },
+    stages: [
+      {
+        name: 'Stage 1',
+        minorRequired: true,
+        zones: {
+          segments: [
+            {
+              limitStartGroundSpeed: true,
+              checkpoints: [{ volumeIndex: 0 }, { volumeIndex: 1 }]
+            }
+          ],
+          end: { volumeIndex: 2 }
+        }
+      },
+      {
+        name: 'Stage 2',
+        minorRequired: true,
+        zones: {
+          segments: [
+            {
+              limitStartGroundSpeed: true,
+              checkpoints: [{ volumeIndex: 2 }, { volumeIndex: 3 }]
+            }
+          ],
+          end: { volumeIndex: 4 }
+        }
+      }
+    ],
+    bonuses: [
+      {
+        name: 'Bonus 1',
+        minorRequired: true,
+        zones: {
+          segments: [
+            {
+              limitStartGroundSpeed: true,
+              checkpoints: [{ volumeIndex: 5 }]
+            }
+          ],
+          end: { volumeIndex: 6 }
+        }
+      }
+    ]
+  },
+  // Not that geometry really matters, but these are layed out as 512hu cubes
+  // in the below shape in space,
+  //      Main/S2 End -> 4
+  //            S2 CP -> 3
+  //  S2 Start/S1 End -> 2
+  //            S1 CP -> 1 6 <- B2 End
+  //         S1 Start -> 0 5 <- B1 Start
+  volumes: [
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          teleportYaw: 0,
+          teleportPos: [256, 256, 0],
+          points: [
+            [0, 0],
+            [0, 512],
+            [512, 512],
+            [512, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [1024, 0],
+            [1024, 512],
+            [1536, 512],
+            [1536, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          teleportYaw: 0,
+          teleportPos: [2304, 256, 0],
+          points: [
+            [2048, 0],
+            [2048, 512],
+            [2560, 512],
+            [2560, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [3072, 0],
+            [3072, 512],
+            [3584, 512],
+            [3584, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [4096, 0],
+            [4096, 512],
+            [4608, 512],
+            [4608, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          teleportYaw: 0,
+          teleportPos: [256, 1028, 0],
+          points: [
+            [0, 1024],
+            [0, 1536],
+            [512, 1536],
+            [512, 1024]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [1024, 2048],
+            [1024, 2560],
+            [1536, 2560],
+            [1536, 2048]
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+/**
+ * Defining properties of all the leaderboards that *should* be generated from
+ * ZonesStub, for a main track on RJ and bonus on CPM Defrag.
+ * This'll break every time we add more submodes, but is very useful
+ * for tests where we want to be precise about leaderboard generation.
+ */
+// prettier-ignore
+export const ZonesStubLeaderboards = [
+  ...[ GM.RJ, GM.SJ, GM.CONC, GM.DEFRAG_CPM, GM.DEFRAG_VQ3 ]
+    .flatMap((gamemode) => [
+      { gamemode, trackType: TrackType.MAIN,  trackNum: 0, linear: false },
+      { gamemode, trackType: TrackType.STAGE, trackNum: 0, linear: null  },
+      { gamemode, trackType: TrackType.STAGE, trackNum: 1, linear: null  }
+  ]),
+  ...[ GM.RJ, GM.SJ, GM.AHOP, GM.BHOP, GM.CONC, GM.DEFRAG_CPM, GM.DEFRAG_VQ3 ]
+    .map((gamemode) => (
+      { gamemode, trackType: TrackType.BONUS, trackNum: 0, linear: null }))
+].sort();
+
+/**
+ * Simpler zones stub that's just a single linear track with no major
+ * checkpoints
+ */
+export const BabyZonesStub: MapZones = {
+  formatVersion: 1,
+  dataTimestamp: 1697451975363,
+  tracks: {
+    main: {
+      name: 'Main',
+      majorOrdered: true,
+      minorRequired: true,
+      zones: {
+        segments: [
+          {
+            limitStartGroundSpeed: true,
+            checkpoints: [{ volumeIndex: 0 }, { volumeIndex: 1 }]
+          }
+        ],
+        end: { volumeIndex: 2 }
+      }
+    },
+    stages: [],
+    bonuses: []
+  },
+  volumes: [
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          teleportYaw: 0,
+          teleportPos: [256, 256, 0],
+          points: [
+            [0, 0],
+            [0, 512],
+            [512, 512],
+            [512, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [1024, 0],
+            [1024, 512],
+            [1536, 512],
+            [1536, 0]
+          ]
+        }
+      ]
+    },
+    {
+      regions: [
+        {
+          bottom: 0,
+          height: 512,
+          points: [
+            [1024, 0],
+            [1024, 512],
+            [1536, 512],
+            [1536, 0]
+          ]
+        }
+      ]
+    }
+  ]
+};
