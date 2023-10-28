e09ea96b6d838661483f5f7eda3687f2e9a6a3f3
tsa96 <tom@trumpetdust.org>
2023-10-28 12:25:53 +0100

    refactor(shared): remove old map zone models/DTOs
    bye bitches

--- libs/constants/src/types/models/map/zone/map-zone-trigger-properties.model.ts
+++
@@ -1,8 +0,0 @@
-﻿import { MapZoneTriggerProperties as PrismaMapZoneTriggerProperties } from '@prisma/client';
-
-// Apparently there's some reason for this stupid table to exist, I can't remember what Goc said though
-// Think it's changing for 0.10 anyway ╚(•⌂•)╝
-export type MapZoneTriggerProperties = Omit<
-  PrismaMapZoneTriggerProperties,
-  'id' | 'triggerID'
->;
--- libs/constants/src/types/models/run/run-zone-stats.model.ts
+++
@@ -1,4 +0,0 @@
-import { RunZoneStats as PrismaRunZoneStats } from '@prisma/client';
-import { NumberifyBigInt } from '../../utils';
-
-export type RunZoneStats = NumberifyBigInt<PrismaRunZoneStats>;
