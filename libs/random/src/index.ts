95d9ca74fe53f6a022205e6c9863185465be97ab
tsa96 <tom@trumpetdust.org>
2023-10-31 21:25:50 +0000

    refactor(utils): remove pointless object wrapper in @momentum/random
    Since this is a module it's far better to just export each individually

--- libs/random/src/index.ts
+++ libs/random/src/index.ts
@@ -1,58 +1,66 @@
-export const Random = {
-  bool: (): boolean => Math.random() < 0.5,
+import { Enum, HeteroEnum } from '@momentum/enum';
 
-  weightedBool: (probabilityOfTrue: number): boolean =>
-    Math.random() < probabilityOfTrue,
+export function chance(probabilityOfTrue = 0.5): boolean {
+  return Math.random() < probabilityOfTrue;
+}
 
-  int: (max: number, min = 0): number =>
-    Math.floor(Math.random() * (max - min + 1) + min),
+export function int(max: number, min = 0): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-  float: (max: number, min = 0, decimalPlaces?: number): number => {
-    const float = Math.random() * (max - min) + min;
-    return decimalPlaces !== undefined
-      ? float
-      : Number.parseFloat(
-          (Math.random() * (max - min) + min).toFixed(decimalPlaces)
-        );
-  },
+export function float(max: number, min = 0, decimalPlaces?: number): number {
+  const float = Math.random() * (max - min) + min;
+  return decimalPlaces !== undefined
+    ? float
+    : Number.parseFloat(
+        (Math.random() * (max - min) + min).toFixed(decimalPlaces)
+      );
+}
 
-  element: <T>(array: readonly T[]): T =>
-    array[Math.floor(Math.random() * array.length)],
-
-  enumKey: <T extends object>(enum_: T): keyof T =>
-    Random.element(
-      Object.keys(enum_).filter((key) => !(Math.abs(Number.parseInt(key)) + 1))
-    ) as keyof T,
-
-  enumValue: <T extends object>(enum_: T): T[keyof T] =>
-    enum_[Random.enumKey(enum_)],
-
-  date: (
-    start: Date | number,
-    end: Date | number,
-    startHour = 0,
-    endHour = 24
-  ): Date => {
-    if (typeof start != 'number') start = +start;
-    if (typeof end != 'number') end = +start;
-    const date = new Date(start + Math.random() * (end - start));
-    date.setHours(Random.int(startHour, endHour));
-    return date;
-  },
-
-  pastDateInYears: (years = 1): Date =>
-    Random.date(Date.now() - years * 31_536_000, Date.now()),
-
-  pastDateSince: (refDate: Date | number): Date =>
-    Random.date(typeof refDate != 'number' ? +refDate : refDate, Date.now()),
-
-  createdUpdatedDates: (
-    startDateYearsBack?: number
-  ): { createdAt: Date; updatedAt: Date } => {
-    const createdAtDate = Random.pastDateInYears(startDateYearsBack);
-    return {
-      createdAt: createdAtDate,
-      updatedAt: Random.date(createdAtDate, Date.now())
-    };
+export function element<T>(array: readonly T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
+}
   }
-};
+
+  return choices[i][0];
+}
+
+export function enumKey<T extends HeteroEnum<T>>(enum_: T): keyof T {
+  return element(Enum.keys(enum_)) as keyof T;
+}
+
+export function enumValue<T extends HeteroEnum<T>>(enum_: T): T[keyof T] {
+  return enum_[enumKey(enum_)];
+}
+
+export function date(
+  start: Date | number,
+  end: Date | number,
+  startHour = 0,
+  endHour = 24
+): Date {
+  if (typeof start != 'number') start = +start;
+  if (typeof end != 'number') end = +start;
+  const date = new Date(start + Math.random() * (end - start));
+  date.setHours(int(startHour, endHour));
+  return date;
+}
+
+export function pastDateInYears(years = 1): Date {
+  return date(Date.now() - years * 31_536_000, Date.now());
+}
+
+export function pastDateSince(refDate: Date | number): Date {
+  return date(typeof refDate != 'number' ? +refDate : refDate, Date.now());
+}
+
+export function createdUpdatedDates(startDateYearsBack?: number): {
+  createdAt: Date;
+  updatedAt: Date;
+} {
+  const createdAtDate = pastDateInYears(startDateYearsBack);
+  return {
+    createdAt: createdAtDate,
+    updatedAt: date(createdAtDate, Date.now())
+  };
+}
