30109820e08b288d3f35aa6ef360f73845353325
tsa96 <tom@trumpetdust.org>
2023-11-07 14:51:50 +0000

    feat(front): brand new file-upload component(s)

---
+++ apps/frontend/src/app/components/file-upload/abstract-file-upload.component.ts
@@ -0,0 +1,144 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  ViewChild
+} from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import { Icon } from '@momentum/frontend/icons';
+
+/**
+ * Abstract class providing core implementation for FileUploadComponent and
+ * MultiFileUploadComponent.
+ *
+ * I split these up into two implemenations because I got annoyed with
+ * accessing the 0th value of `value` constantly for instances with `multiple`
+ * set to false.
+ */
+@Directive()
+export abstract class AbstractFileUploadComponent<T extends File | File[]>
+  implements ControlValueAccessor
+{
+  protected abstract value: T;
+  protected abstract readonly multiple: boolean;
+  protected abstract readonly max: number | null;
+
+  // Uses a numeric counter to handle dragging over child elements
+  // https://stackoverflow.com/a/21002544
+  protected dragOverCounter = 0;
+
+  /**
+   * Extensions that file select dialog will accept
+   * (use comma separation for multiple)
+   */
+  @Input({ required: true }) accept = '';
+
+  /**
+   * Name of the file type to use in "select a <type> string"
+   */
+  @Input() typeName = '';
+
+  protected disabled = false;
+  protected disabledBecauseReachedMax = false;
+
+  /**
+   * Whether to show a list of selected files within the component. If false,
+   * the "Select a file" stuff is always shown.
+   */
+  @Input() showSelected = true;
+
+  /**
+   * Icon to use, uses generic file icon if not given.
+   * Only supports MDI currently
+   */
+  @Input() icon: Icon = 'file-upload';
+
+  @ViewChild('fileInput') private input: ElementRef<HTMLInputElement>;
+
+  abstract onDrop(event: DragEvent): void;
+
+  abstract onFilesSelected(event: Event): void;
+
+  abstract hasSelection(): boolean;
+
+  abstract getIterableFiles(): File[];
+
+  abstract removeFile(file: File, event?: Event): void;
+
+  @HostListener('click')
+  onClick() {
+    if (!this.disabled) {
+      this.input.nativeElement.click();
+    }
+  }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  @HostListener('dragenter')
+  onDragEnter() {
+    if (this.disabled) return;
+
+    this.dragOverCounter++;
+  }
+
+  @HostListener('dragleave')
+  onDragLeave() {
+    if (this.disabled) return;
+
+    this.dragOverCounter--;
+  }
+
+  removeDragData(event: DragEvent) {
+    this.dragOverCounter = 0;
+
+    if (event.dataTransfer.items) {
+      event.dataTransfer.items.clear();
+    } else {
+      event.dataTransfer.clearData();
+    }
+  }
+
+  size(file: File): string {
+    const size = file.size;
+    const KiB = size < 1024 ** 2;
+    return `${(file.size / (KiB ? 1024 : 1024 * 1024)).toPrecision(3)} ${
+      KiB ? 'KiB' : 'MiB'
+    }`;
+  }
+
+  onChange: (value: T) => void = () => void 0;
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  onTouched = () => void 0;
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.dragOverCounter = 0;
+    if (isDisabled === false) {
+      this.disabledBecauseReachedMax = false;
+    }
+  }
+
+  abstract writeValue(value: T): void;
+
+  // This definitely isn't perfect but good enough for filtering out duplicates
+  // and in various bits of logic where I need this. Don't want the perf hit of
+  // comparing whole buffers!
+  static isIdenticalFile(file1: File, file2: File) {
+    return (
+      file1.name === file2.name &&
+      file1.lastModified === file2.lastModified &&
+      file1.size === file2.size &&
+      file1.type === file2.type
+    );
+  }
+}
---
+++ apps/frontend/src/app/components/file-upload/multi-file-upload.component.ts
@@ -0,0 +1,126 @@
+import { Component, forwardRef, HostListener, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AbstractFileUploadComponent } from './abstract-file-upload.component';
+
+/**
+ * A form control for multiple file selection/uploading with support for drag
+ * and drop.
+ */
+@Component({
+  selector: 'mom-multi-file-upload',
+  templateUrl: 'file-upload.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => MultiFileUploadComponent),
+      multi: true
+    }
+  ]
+})
+export class MultiFileUploadComponent extends AbstractFileUploadComponent<
+  File[]
+> {
+  public value: File[] = [];
+  protected readonly multiple = true;
+  @Input() max: number | null;
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    // Keeping this method in the abstract class as allows single file component
+    // to handle case
+
+    // Prevent default behavior (Prevent file from being opened)
+    event.preventDefault();
+    this.removeDragData(event);
+
+    if (this.disabled) return;
+
+    // `.items` seems to be the correct approach but try files if missing
+    const useItems = !!event.dataTransfer.items;
+    const items = event.dataTransfer.items ?? event.dataTransfer.files;
+    // DataTransferList doesn't have an iterator symbol on it according to
+    // TypeScript (???)
+    // eslint-disable-next-line unicorn/no-for-loop
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      // Ignore anything that isn't a file
+      if (useItems && (item as DataTransferItem).kind !== 'file') continue;
+
+      const file = useItems
+        ? (item as DataTransferItem).getAsFile()
+        : (item as File);
+
+      this.value.push(file);
+
+      if (this.value.length === this.max) {
+        // TODO: This sucks when multiple=false (try with single bsp)
+        this.disabled = true;
+        this.disabledBecauseReachedMax = true;
+        break;
+      }
+    }
+
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  onFilesSelected(event: Event) {
+    const elementFiles = (event.target as HTMLInputElement).files;
+
+    // eslint-disable-next-line unicorn/no-for-loop
+    for (let i = 0; i < elementFiles.length; i++) {
+      const file = elementFiles[i];
+
+      // Always ignore duplicates - can't imagine where we'd ever want this.
+      if (
+        !(this.value as File[]).some((f) =>
+          AbstractFileUploadComponent.isIdenticalFile(f, file)
+        )
+      )
+        this.value.push(file);
+
+      if (this.value.length === this.max) {
+        this.disabled = true;
+        this.disabledBecauseReachedMax = true;
+        break;
+      }
+    }
+
+    this.onChange(this.value);
+    this.onTouched();
+
+    (event.target as HTMLInputElement).value = '';
+  }
+
+  writeValue(value: File[]): void {
+    this.value = value == null || (value as any) === '' ? [] : value;
+    if (this.disabledBecauseReachedMax && value.length < this.max) {
+      this.disabledBecauseReachedMax = false;
+      this.disabled = false;
+    }
+  }
+
+  removeFile(file: File, event?: Event) {
+    event?.stopPropagation();
+
+    this.value.splice(
+      this.value.findIndex(({ name }) => name === file.name),
+      1
+    );
+
+    if (this.disabledBecauseReachedMax) {
+      this.disabled = false;
+      this.disabledBecauseReachedMax = false;
+    }
+
+    this.onChange(this.value);
+  }
+
+  hasSelection(): boolean {
+    return this.value.length > 0;
+  }
+
+  getIterableFiles(): File[] {
+    return this.value;
+  }
+}
