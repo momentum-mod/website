31fe9eedd142a5f8a17df0e8da53f4ea223e15e2
tsa96 <tom@trumpetdust.org>
2023-11-10 17:55:17 +0000

    docs(shared): docs for IncompatibleGamemodes

--- libs/constants/src/maps/incompatible-gamemodes.map.ts
+++ libs/constants/src/maps/incompatible-gamemodes.map.ts
@@ -1,5 +1,17 @@
 import { Gamemode as G } from '../';
 
+/**
+ * A non-reflexive map of gamemodes and collections of gamemodes they can
+ * *never* have compatibile leaderboards for.
+ *
+ * For example, there's *never* a possible case
+ * where a surf leaderboard should also have a bhop leaderboard, that'd just be
+ * jank 100-tick surf.
+ *
+ * Note the non-reflexivity, you might have a surf map that's worth a
+ * leaderboard in ahop (e.g. utopia), but a ahop map would never have a surf
+ * leaderboard, since that'd just be bhop with worse settings.
+ */
 export const IncompatibleGamemodes: ReadonlyMap<G, G[]> = new Map([
   [G.SURF, [G.BHOP]],
   [G.BHOP, [G.SURF]],
