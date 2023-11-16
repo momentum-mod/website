2334e3ced177785a309546dfc8bb78240bcdd543
tsa96 <tom@trumpetdust.org>
2023-11-16 14:21:10 +0000

    feat(front): tailwind classes for relative flex-grow values

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -15,6 +15,19 @@ module.exports = {
       fontFamily: {
         sans: ['Roboto', ...defaultTheme.fontFamily.sans],
         display: ['Bebas Neue']
+      },
+      flexGrow: {
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+        11: '11',
+        12: '12'
       }
     },
     //prettier-ignore
