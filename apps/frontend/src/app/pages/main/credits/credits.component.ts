92e9d6b909379aaaa0bd4c47c2a3dc1e42f09131
tsa96 <tom@trumpetdust.org>
2023-11-21 11:56:49 +0000

    refactor(front): delete pages/main/
    byeeeeeeee

--- apps/frontend/src/app/pages/main/credits/credits.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-credits',
-  templateUrl: './credits.component.html',
-  styleUrls: ['./credits.component.scss']
-})
-export class CreditsComponent {}
--- apps/frontend/src/app/pages/main/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mom-main-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderComponent {}
