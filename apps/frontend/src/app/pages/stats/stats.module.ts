11840718d5df60e1c141fcd740a6f164d3557c23
tsa96 <tom@trumpetdust.org>
2023-11-22 08:08:25 +0000

    refactor(front): delete stats module/component entirely
    There's literally nothing in this, and with standalone components removing all
    the excess fluff, I just don't see the reason to keep this stubbed - there's barely anything to keep!

--- apps/frontend/src/app/pages/stats/stats.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { StatsComponent } from './stats.component';
-import { StatsRoutingModule } from './stats-routing.module';
-import { SharedModule } from '../../shared.module';
-
-@NgModule({
-  imports: [SharedModule, StatsRoutingModule, StatsComponent]
-})
-export class StatsModule {}
