f01d74f508c5acbf25d9cddce05502d1a7c744da
tsa96 <tom@trumpetdust.org>
2023-11-23 13:08:28 +0000

    chore(front): remove DirectivesModule
    Don't really have a use for this, and I want to bring the Frontend Nx libraries into the main app soon anyway.

--- apps/frontend/src/app/shared.module.ts
+++ apps/frontend/src/app/shared.module.ts
@@ -2,9 +2,7 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ThemeModule } from '@momentum/frontend/theme';
-import { DirectivesModule } from '@momentum/frontend/directives';
 import { PipesModule } from '@momentum/frontend/pipes';
-import { ComponentsModule } from './components/components.module';
 import { NgxEchartsModule } from 'ngx-echarts';
 import * as echarts from 'echarts';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -12,8 +10,6 @@ import { RouterModule } from '@angular/router';
 
 const MODULES = [
   CommonModule,
-  ComponentsModule,
-  DirectivesModule,
   PipesModule,
   NgOptimizedImage,
   NgxPaginationModule,
