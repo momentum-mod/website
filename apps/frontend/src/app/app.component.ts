a96a445cd6117634e6f594f9ced2950f4849e914
tsa96 <tom@trumpetdust.org>
2023-11-27 16:49:59 +0000

    refactor(front): app.component layout without nebular wrapper

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,63 +1,23 @@
 import { Component } from '@angular/core';
-import {
-  NbMenuItem,
-  NbLayoutModule,
-  NbSidebarModule,
-  NbMenuModule
-} from '@nebular/theme';
-import { LocalUserService } from '@momentum/frontend/data';
 import { NotificationsService } from './services/notifications.service';
-import { Role } from '@momentum/constants';
-import { MENU_ITEMS } from './app-menu';
-import {
-  MaterialDesignIcons,
-  MomentumIcons,
-  SimpleIcons,
-  IconComponent
-} from '@momentum/frontend/icons';
-import { kebabCase } from '@momentum/util-fn';
+import { IconComponent } from '@momentum/frontend/icons';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @Component({
   selector: 'mom-app',
   templateUrl: 'app.component.html',
   standalone: true,
   imports: [
-    NbLayoutModule,
     HeaderComponent,
-    NbSidebarModule,
-    NbMenuModule,
     IconComponent,
-    RouterOutlet
+    RouterOutlet,
+    SidenavComponent
   ]
 })
 export class AppComponent {
-  constructor(
-    private userService: LocalUserService,
-    private notificationService: NotificationsService
-  ) {
+  constructor(private readonly notificationService: NotificationsService) {
     this.notificationService.inject();
-  }
-
-  menu = MENU_ITEMS;
-
-  ngOnInit() {
-    for (const item of this.menu) {
-      this.authMenuItem(item);
-    }
-  }
-
-  authMenuItem(menuItem: NbMenuItem) {
-    const allowedRoles = menuItem?.data?.roles as Role[];
-    menuItem.hidden =
-      allowedRoles &&
-      !allowedRoles.some((role) => this.userService.hasRole(role));
-
-    if (menuItem.children) {
-      for (const item of menuItem.children) {
-        this.authMenuItem(item);
-      }
-    }
   }
 }
