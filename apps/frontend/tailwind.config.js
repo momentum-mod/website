6b6e2f7fc2f1c70e36f24a1baa635247fde27d30
tsa96 <tom@trumpetdust.org>
2023-12-02 22:46:26 +0000

    feat(front): add h1-6 styling to tailwind config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -100,7 +100,10 @@ module.exports = {
       addBase({
         h1: { fontSize: theme('fontSize.6xl') },
         h2: { fontSize: theme('fontSize.4xl') },
-        h3: { fontSize: theme('fontSize.3lg') }
+        h3: { fontSize: theme('fontSize.3xl') },
+        h4: { fontSize: theme('fontSize.2xl') },
+        h5: { fontSize: theme('fontSize.xl') },
+        h6: { fontSize: theme('fontSize.lg') }
       });
     }),
     plugin(function ({ addVariant }) {
