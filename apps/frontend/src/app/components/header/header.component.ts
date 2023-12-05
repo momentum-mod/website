cbb06e0ca7ba10a6cbdff38fb462f7b534867616
tsa96 <tom@trumpetdust.org>
2023-12-05 12:29:05 +0000

    feat(front): custom header component part 2
    Sorry for messy commits, so much stuff happening at once that I can't fixup all this!

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-  NbMenuItem,
-  NbMenuService,
-  NbActionsModule,
-  NbPopoverModule,
-  NbUserModule,
-  NbContextMenuModule
-} from '@nebular/theme';
 import { NotificationsService } from '../../services/notifications.service';
 import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
 import { Notification, User } from '@momentum/constants';
 import { LocalUserService } from '@momentum/frontend/data';
 import { LayoutService } from '../../services/layout.service';
@@ -19,98 +10,77 @@ import { RouterLink } from '@angular/router';
 import { IconComponent } from '@momentum/frontend/icons';
 import { TooltipModule } from 'primeng/tooltip';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CommonModule } from '@angular/common';
+import { PlayerCardComponent } from '../player-card/player-card.component';
+import { Subject } from 'rxjs';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'm-header',
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
   standalone: true,
   imports: [
-    IconComponent,
     RouterLink,
-    NbActionsModule,
+    IconComponent,
     SearchComponent,
-    NbPopoverModule,
     NotificationComponent,
-    NbUserModule,
-    NbContextMenuModule,
     TooltipModule,
-    OverlayPanelModule
+    OverlayPanelModule,
+    CommonModule,
+    PlayerCardComponent,
+    MenuModule
   ]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  private ngUnsub = new Subject<void>();
-
-  userMenu: NbMenuItem[] = [
+  menu: MenuItem[] = [
     {
-      title: 'Profile',
+      label: 'Profile',
       // If `user` hasn't been fetched yet, just navigate to ProfileRedirect
       // component, which will await the user result then redirect to relatived URL.
-      link: this.user?.id ? `/profile/${this.user.id}` : '/profile'
+      routerLink: this.user?.id ? `/profile/${this.user.id}` : '/profile'
     },
     {
-      title: 'Edit Profile',
-      link: '/profile/edit'
+      label: 'Edit Profile',
+      routerLink: '/profile/edit'
     },
     {
-      title: 'Log out'
+      label: 'Log out',
+      command: () => this.localUserService.logout()
     }
   ];
 
-  user?: User;
-  notifications: Notification[];
-  numUnreadNotifs: number;
+  protected user?: User;
+  protected notifications: Notification[] = [];
+  protected unreadNotificationCount = 0;
+
+  private readonly ngUnsub = new Subject<void>();
 
   constructor(
-    private readonly menuService: NbMenuService,
-    private readonly userService: LocalUserService,
+    private readonly localUserService: LocalUserService,
     private readonly layoutService: LayoutService,
     private readonly notificationService: NotificationsService
-  ) {
-    this.notifications = [];
-    this.numUnreadNotifs = 0;
-    this.menuService
-      .onItemClick()
-      .subscribe((event) => this.onContextItemSelection(event.item.title));
-  }
+  ) {}
 
   ngOnInit() {
-    this.userService
-      .getLocal()
+    this.localUserService.localUserSubject
       .pipe(takeUntil(this.ngUnsub))
       .subscribe((user) => (this.user = user));
     this.notificationService.notifications.subscribe((notifs) => {
       this.notifications = notifs;
-      this.numUnreadNotifs = this.notifications.filter(
-        (notif) => notif.read === false
+      this.unreadNotificationCount = this.notifications.filter(
+        ({ read }) => read === false
       ).length;
     });
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.ngUnsub.next();
     this.ngUnsub.complete();
   }
 
-  onContextItemSelection(title) {
-    if (title === 'Log out') {
-      this.userService.logout();
-    }
-  }
-
-  logout() {
-    this.userService.logout();
-  }
-
   toggleSidenav() {
     this.layoutService.toggleSidenavState();
-  }
-
-  navigateHome() {
-    this.menuService.navigateHome();
-    return false;
-  }
-
-  getNotificationIconClass() {
-    return this.notifications.length === 0 ? 'bell-outline' : 'bell';
   }
 }
