77399b3b4f4ccf8aec7dbacdbaabba2c87138c4d
tsa96 <tom@trumpetdust.org>
2023-12-06 06:18:14 +0000

    chore(back): remove admin XP variables endpoints

--- apps/backend/src/app/modules/xp-systems/xp-systems.module.ts
+++ apps/backend/src/app/modules/xp-systems/xp-systems.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { XpSystemsService } from './xp-systems.service';
-import { DbModule } from '../database/db.module';
 
 @Module({
-  imports: [DbModule.forRoot()],
   providers: [XpSystemsService],
   exports: [XpSystemsService]
 })
