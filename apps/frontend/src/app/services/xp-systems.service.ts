c7d638c3396b55ed9865b78c845a06a77f7c24b6
tsa96 <tom@trumpetdust.org>
2023-12-06 09:41:25 +0000

    feat(front): xp-systems service
    Wow, what a complicated service

---
+++ apps/frontend/src/app/services/xp-systems.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { XpSystems } from '@momentum/xp-systems';
+
+@Injectable({ providedIn: 'root' })
+export class XpSystemsService extends XpSystems {}
