6d532de22b69932327c433deec93136847733a01
tsa96 <tom@trumpetdust.org>
2023-12-07 19:05:11 +0000

    fix(utils): better types on promiseAllAsync

--- libs/util-fn/src/promise-all-sync.ts
+++ libs/util-fn/src/promise-all-sync.ts
@@ -2,8 +2,8 @@
  * Promise.all, but synchronous!
  */
 export async function promiseAllSync<T>(
-  resolvables: (() => Promise<T>)[]
-): Promise<T[]> {
+  resolvables: Array<() => PromiseLike<T>>
+): Promise<Awaited<T[]>> {
   const results = [];
   for (const resolvable of resolvables) {
     results.push(await resolvable());
