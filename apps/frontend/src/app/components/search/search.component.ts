a58515ea60c0a1a2955e15593ec5f221f083e207
tsa96 <tom@trumpetdust.org>
2023-12-07 17:50:16 +0000

    refactor(front): rework search component part 1

---
+++ apps/frontend/src/app/components/search/search.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserSearchComponent } from './user-search/user-search.component';
+
+enum SearchType {
+  USER,
+  MAP
+}
+
+@Component({
+  selector: 'mom-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule, UserSearchComponent],
+  templateUrl: './search.component.html',
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        min-width: 24rem;
+      }
+    `
+  ]
+})
+export class SearchComponent {
+  protected readonly SearchType = SearchType;
+  protected activeType: SearchType = SearchType.MAP;
+}
