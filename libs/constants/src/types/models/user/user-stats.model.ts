f900933bf2be1fd72b6265c29ef44cc2b95d0777
tsa96 <tom@trumpetdust.org>
2023-12-08 16:36:20 +0000

    refactor(shared): hack to ignore number/bigint issues between front and backend temporarily

--- libs/constants/src/types/models/user/user-stats.model.ts
+++ libs/constants/src/types/models/user/user-stats.model.ts
@@ -1,3 +1,10 @@
 ﻿import { UserStats as PrismaUserStats } from '@prisma/client';
 
-export type UserStats = PrismaUserStats;
+export type UserStats = Omit<PrismaUserStats, 'cosXP'> & {
+  // TODO: TYPE HACK
+  // This is needed since both frontend and backend use this variable,
+  // and on backend it's a bigint, on frontend it's a number (class-transformer
+  // transforms it). It's a fucking terrible system that I need to do better,
+  // but I need to kill class-transformer first.
+  cosXP: number | bigint;
+};
