54633fbf735fd0692661ef79ebae94fee9a7e8b0
tsa96 <tom@trumpetdust.org>
2023-12-14 05:46:38 +0000

    refactor(front): remove further xp systems crap

--- apps/frontend/src/app/pages/admin/utilities/utilities.component.ts
+++ apps/frontend/src/app/pages/admin/utilities/utilities.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NbDialogService } from '@nebular/theme';
-import { ConfirmDialogComponent } from '../../../components/confirm-dialog/confirm-dialog.component';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AdminService } from '@momentum/frontend/data';
 import { SharedModule } from '../../../shared.module';
@@ -25,73 +23,6 @@ export class UtilitiesComponent {
     private readonly adminService: AdminService,
     private readonly messageService: MessageService
   ) {}
-
-  showResetCosXPConfigDialog() {
-    this.dialogService
-      .open(ConfirmDialogComponent, {
-        context: {
-          title: 'Are you sure?',
-          message:
-            'You are about to set the cosmetic XP to 0 for all users. Are you sure you want to proceed?'
-        }
-      })
-      .onClose.subscribe((response) => {
-        if (response) {
-          this.resetCosmeticXPGlobally();
-        }
-      });
-  }
-
-  showResetRankXPConfirmDialog() {
-    this.dialogService
-      .open(ConfirmDialogComponent, {
-        context: {
-          title: 'Are you sure?',
-          message:
-            'You are about to set the rank XP to 0 for all users. Are you sure you want to proceed?'
-        }
-      })
-      .onClose.subscribe((response) => {
-        if (response) {
-          this.resetRankXPGobally();
-        }
-      });
-  }
-
-  resetCosmeticXPGlobally() {
-    this.adminService
-      .updateAllUserStats({
-        level: 1,
-        cosXP: 0
-      })
-      .subscribe({
-        next: () =>
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Successfully reset cosmetic XP globally'
-          }),
-        error: () =>
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Failed to reset cosmetic XP globally'
-          })
-      });
-  }
-
-  resetRankXPGobally() {
-    this.adminService.updateAllUserStats({ rankXP: 0 }).subscribe({
-      next: () =>
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Successfully reset rank XP globally'
-        }),
-      error: () =>
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Failed to reset rank XP globally'
-        })
-    });
-  }
 
   createUser() {
     if (!this.userForm.valid) return;
