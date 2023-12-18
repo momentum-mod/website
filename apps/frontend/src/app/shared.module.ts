da1e64ce5ec7bcebbf98dfd968bd827a736d4a8d
tsa96 <tom@trumpetdust.org>
2023-12-18 22:00:43 +0000

    build(deps): remove Nebular :D

--- apps/frontend/src/app/shared.module.ts
+++ apps/frontend/src/app/shared.module.ts
@@ -1,10 +1,7 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ThemeModule } from '@momentum/frontend/theme';
 import { PipesModule } from '@momentum/frontend/pipes';
-import { NgxEchartsModule } from 'ngx-echarts';
-import * as echarts from 'echarts';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { RouterModule } from '@angular/router';
 import { IconComponent } from '@momentum/frontend/icons';
@@ -24,19 +21,7 @@ const MODULES = [
 const DIRECTIVES = [TooltipDirective];
 
 @NgModule({
-  imports: [
-    ...MODULES,
-    ...DIRECTIVES,
-    ThemeModule.forRoot(),
-    NgxEchartsModule.forRoot({ echarts })
-  ],
-  exports: [
-    ...MODULES,
-    ...DIRECTIVES,
-    ThemeModule,
-    NgxEchartsModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+  imports: [...MODULES, ...DIRECTIVES],
+  exports: [...MODULES, ...DIRECTIVES, FormsModule, ReactiveFormsModule]
 })
 export class SharedModule {}
