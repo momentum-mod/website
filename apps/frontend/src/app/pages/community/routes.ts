1744f68e394812306517540ae8268de12a38eb12
tsa96 <tom@trumpetdust.org>
2023-12-29 09:59:14 +0000

    chore(front): remove community activity page
    IMO this isn't justified in having a dedicated page, moving to homepage
    in next community.

--- apps/frontend/src/app/pages/community/routes.ts
+++ apps/frontend/src/app/pages/community/routes.ts
@@ -1,10 +1,8 @@
 import { Route } from '@angular/router';
 import { CommunityNewsComponent } from './community-news/community-news.component';
 import { CommunityTwitchStreamComponent } from './community-twitch-stream/community-twitch-stream.component';
-import { CommunityActivityComponent } from './community-activity/community-activity.component';
 
 export default [
   { path: 'news', component: CommunityNewsComponent },
-  { path: 'twitch', component: CommunityTwitchStreamComponent },
-  { path: 'activity', component: CommunityActivityComponent }
+  { path: 'twitch', component: CommunityTwitchStreamComponent }
 ] satisfies Route[];
