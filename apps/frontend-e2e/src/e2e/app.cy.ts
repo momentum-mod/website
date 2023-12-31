bb562a9a0131ab14ae5fa1481485a60a0a672688
tsa96 <tom@trumpetdust.org>
2023-12-31 14:14:27 +0000

    refactor(front-e2e): make stubbed front e2e test theoretically work
    Doesn't *actually* work, since LocalUserService errors make the test fail.
    No point fixing those til we support accessing the dashboard without a login though.

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,10 +1,11 @@
 describe('frontend', () => {
-  it('should display welcome message', () => {
+  it('should successfully load the dashboard home page', () => {
     // Not much of a test, but serves to stub frontend E2E testing for now,
-    // and tests that the frontmatter site displays in a real browser
-    // without errors.
+    // and tests that the dashboard loads in a real browser without errors.
     cy.visit('/');
-    cy.get('header h2').should('contain', 'movement');
-    cy.get('header h2').should('not.contain', 'smovement');
+
+    // This will fail untilwe  add support for viewing the dashboard whilst
+    // unauthenticated.
+    cy.get('m-app > div:last').should('have.class', 'chungus');
   });
 });
