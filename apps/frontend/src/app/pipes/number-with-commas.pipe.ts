5b3233fba59ec5e209a6fb37449c47f144135729
tsa96 <tom@trumpetdust.org>
2023-12-31 09:45:05 +0000

    refactor(front): make pipes standalone
    When this was a separate library having a module made some sense, but
    standalone is probably a bit better now. (Change to remove PipesModule was
    in last commit, annoying to dig it out, sorry!)

--- apps/frontend/src/app/pipes/number-with-commas.pipe.ts
+++ apps/frontend/src/app/pipes/number-with-commas.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'numberWithCommas' })
+@Pipe({ name: 'numberWithCommas', standalone: true })
 export class NumberWithCommasPipe implements PipeTransform {
   transform(input: number): string {
     return new Intl.NumberFormat().format(input);
