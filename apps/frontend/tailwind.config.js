68955f24d5d7341bff9b2bc90bab3856db5d7f87
tsa96 <tom@trumpetdust.org>
2024-01-11 12:59:30 +0000

    feat(front): use shared color vars in tailwind config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,7 +2,6 @@ const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('node:path');
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -30,65 +29,12 @@ module.exports = {
         12: '12'
       }
     },
-    //prettier-ignore
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
       black: '#000',
       white: '#fff',
-
-      // Tailwind defaults for now, do custom versions in the future!
-      red: colors.red,
-      indigo: colors.indigo,
-      orange: colors.orange,
-      green: colors.green,
-      yellow: colors.yellow,
-
-      // Our stuff - all created using https://palettte.app/
-      // Very neutral grays, no sat for dark tones, slightly blue for lighter
-      gray: {
-        50:  '#E7F0F4',
-        100: '#D3DFE6',
-        200: '#BDCAD1',
-        300: '#9BA8AE',
-        400: '#737C81',
-        500: '#53585B',
-        600: '#3C3D3E',
-        700: '#242424',
-        800: '#1C1C1C',
-        900: '#141414',
-        950: '#0B0B0B'
-      },
-      // Very pale blues
-      pale: {
-        50:  '#B9DEFF',
-        100: '#A6CBEF',
-        200: '#93B6DB',
-        300: '#7D9CC3',
-        400: '#6380AB',
-        500: '#4E658F',
-        600: '#364568',
-        700: '#252F48',
-        800: '#181D2C',
-        900: '#10121A',
-        950: '#0A0B0F'
-      },
-      // Based on our signature blue #1896d3 (sometimes #1795d2) which is 400,
-      // then darker tones have a slightly more purpleish hue based on colors
-      // in our dark 3D background render
-      blue: {
-        50:  '#DCEFFF',
-        100: '#C0E5FF',
-        200: '#A1DCFB',
-        300: '#7BCCF5',
-        400: '#52B8E9',
-        500: '#2A9BDB',
-        600: '#1980CA',
-        700: '#0E60B6',
-        800: '#104298',
-        900: '#14337F',
-        950: '#122556'
-      }
+      ...require('./src/app/theme/shared_styling/colors/colors-tailwind-vars')
     }
   },
   corePlugins: {
@@ -120,7 +66,7 @@ module.exports = {
     {
       // We set these dynamically in code in some places, treeshaker doesn't
       // pick them up.
-      pattern: /text-(red|green|blue|orange|indigo|yellow)/
+      pattern: /text-(red|green|blue|orange|purple|pink|pale|yellow)/
     }
   ]
 };
