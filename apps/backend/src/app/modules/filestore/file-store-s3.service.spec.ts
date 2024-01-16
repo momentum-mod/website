f634fb8e2cf5d256e0da34288cd1af6622119aea
tsa96 <tom@trumpetdust.org>
2024-01-16 18:46:31 +0000

    test(back): structure file-store-s3 tests better

--- apps/backend/src/app/modules/filestore/file-store-s3.service.spec.ts
+++ apps/backend/src/app/modules/filestore/file-store-s3.service.spec.ts
@@ -27,134 +27,147 @@ describe('FileStoreS3Service', () => {
     expect(service).toBeDefined();
   });
 
-  it('storeFile should store a file', async () => {
-    const fileBuffer = Buffer.from('elephants');
-    const fileKey = 'testKey';
+  describe('storeFile', () => {
+    it('should store a file', async () => {
+      const fileBuffer = Buffer.from('elephants');
+      const fileKey = 'testKey';
 
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.resolve());
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.resolve());
 
-    const result = await service.storeFile(fileBuffer, fileKey);
+      const result = await service.storeFile(fileBuffer, fileKey);
 
-    expect(result.fileKey).toEqual(fileKey);
-    expect(result.hash).toEqual(
-      createHash('sha1').update(fileBuffer).digest('hex')
-    );
+      expect(result.fileKey).toEqual(fileKey);
+      expect(result.hash).toEqual(
+        createHash('sha1').update(fileBuffer).digest('hex')
+      );
+    });
   });
 
-  it('copyFile should copy a file', async () => {
-    const fromKey = 'fromKey';
-    const toKey = 'toKey';
+  describe('copyFile', () => {
+    it('should copy a file', async () => {
+      const fromKey = 'fromKey';
+      const toKey = 'toKey';
 
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.resolve());
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.resolve());
 
-    const result = await service.copyFile(fromKey, toKey);
+      const result = await service.copyFile(fromKey, toKey);
 
-    expect(result).toEqual(true);
+      expect(result).toEqual(true);
+    });
+
+    it('should return false if the file is missing', async () => {
+      const fromKey = 'fromKey';
+      const toKey = 'toKey';
+
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
+
+      const result = await service.copyFile(fromKey, toKey);
+
+      expect(result).toEqual(false);
+    });
   });
 
-  it('copyFile should return false if the file is missing', async () => {
-    const fromKey = 'fromKey';
-    const toKey = 'toKey';
+  describe('deleteFile', () => {
+    it('deleteFile should delete a file', async () => {
+      const fileKey = 'fileKey';
 
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.resolve());
 
-    const result = await service.copyFile(fromKey, toKey);
+      const result = await service.deleteFile(fileKey);
 
-    expect(result).toEqual(false);
+      expect(result).toEqual(true);
+    });
+
+    it('deleteFile should return false if the file is missing', async () => {
+      const fileKey = 'fileKey';
+
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
+
+      const result = await service.deleteFile(fileKey);
+
+      expect(result).toEqual(false);
+    });
+
+    it('deleteFiles should delete multiple files', async () => {
+      const fileKeys = ['fileKey1', 'fileKey2'];
+
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.resolve());
+
+      const result = await service.deleteFiles(fileKeys);
+
+      expect(result).toEqual(true);
+    });
+
+    it('deleteFiles should handle deleting LOADS of files', async () => {
+      const fileKeys = arrayFrom(10000, (k) => `fileKey${k}`);
+
+      const spy = jest.spyOn(service['s3Client'], 'send');
+      spy.mockImplementation(() => Promise.resolve());
+
+      const result = await service.deleteFiles(fileKeys);
+
+      expect(spy).toHaveBeenCalledTimes(10);
+
+      expect(result).toEqual(true);
+    });
+
+    it('deleteFiles should return false if any file is missing', async () => {
+      const fileKeys = ['fileKey1', 'fileKey2'];
+
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
+
+      const result = await service.deleteFiles(fileKeys);
+
+      expect(result).toEqual(false);
+    });
   });
 
-  it('deleteFile should delete a file', async () => {
-    const fileKey = 'fileKey';
+  describe('getFile', () => {
+    it('getFile should get a file', async () => {
+      const fileKey = 'fileKey';
 
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.resolve());
+      jest.spyOn(service['s3Client'], 'send').mockImplementation(() =>
+        Promise.resolve({
+          Body: {
+            transformToByteArray: () => new Uint8Array()
+          }
+        })
+      );
 
-    const result = await service.deleteFile(fileKey);
+      const result = await service.getFile(fileKey);
 
-    expect(result).toEqual(true);
+      expect(result).toBeInstanceOf(Uint8Array);
+    });
+
+    it('getFile should return null if the file is missing', async () => {
+      const fileKey = 'fileKey';
+
+      jest
+        .spyOn(service['s3Client'], 'send')
+        .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
+
+      const result = await service.getFile(fileKey);
+
+      expect(result).toBeNull();
+    });
   });
 
-  it('deleteFile should return false if the file is missing', async () => {
-    const fileKey = 'fileKey';
 
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
 
-    const result = await service.deleteFile(fileKey);
 
-    expect(result).toEqual(false);
-  });
-
-  it('deleteFiles should delete multiple files', async () => {
-    const fileKeys = ['fileKey1', 'fileKey2'];
-
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.resolve());
-
-    const result = await service.deleteFiles(fileKeys);
-
-    expect(result).toEqual(true);
-  });
-
-  it('deleteFiles should handle deleting LOADS of files', async () => {
-    const fileKeys = arrayFrom(10000, (k) => `fileKey${k}`);
-
-    const spy = jest.spyOn(service['s3Client'], 'send');
-    spy.mockImplementation(() => Promise.resolve());
-
-    const result = await service.deleteFiles(fileKeys);
-
-    expect(spy).toHaveBeenCalledTimes(10);
-
-    expect(result).toEqual(true);
-  });
-
-  it('deleteFiles should return false if any file is missing', async () => {
-    const fileKeys = ['fileKey1', 'fileKey2'];
-
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
-
-    const result = await service.deleteFiles(fileKeys);
-
-    expect(result).toEqual(false);
-  });
-
-  it('getFile should get a file', async () => {
-    const fileKey = 'fileKey';
-
-    jest.spyOn(service['s3Client'], 'send').mockImplementation(() =>
-      Promise.resolve({
-        Body: {
-          transformToByteArray: () => new Uint8Array()
-        }
-      })
-    );
-
-    const result = await service.getFile(fileKey);
-
-    expect(result).toBeInstanceOf(Uint8Array);
-  });
-
-  it('getFile should return null if the file is missing', async () => {
-    const fileKey = 'fileKey';
-
-    jest
-      .spyOn(service['s3Client'], 'send')
-      .mockImplementation(() => Promise.reject({ Code: 'NoSuchKey' }));
-
-    const result = await service.getFile(fileKey);
-
-    expect(result).toBeNull();
   });
 });
