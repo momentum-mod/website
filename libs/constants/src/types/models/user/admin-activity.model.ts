0a668331a486ad37f9dd4537435f9aa2bc6e281e
GordiNoki <gordinoki8@gmail.com>
2024-01-27 23:43:31 +0300

    fix(back): Include user in admin activity

--- libs/constants/src/types/models/user/admin-activity.model.ts
+++ libs/constants/src/types/models/user/admin-activity.model.ts
@@ -1,7 +1,9 @@
 import { AdminActivity as PrismaAdminActivity } from '@prisma/client';
+import { User } from './user.model';
 import { Jsonify } from 'type-fest';
 
 export interface AdminActivity extends PrismaAdminActivity {
   oldData: Jsonify<object>;
   newData: Jsonify<object>;
+  user?: User;
 }
