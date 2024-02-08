737b34d476d337c45119a18be657bcdbd592c09e
tsa96 <tom@trumpetdust.org>
2024-02-08 09:16:35 +0000

    refactor(front): rework map-info and map-leaderboard pages

--- apps/frontend/src/app/pages/maps/map-info/map-info.component.ts
+++ apps/frontend/src/app/pages/maps/map-info/map-info.component.ts
@@ -1,18 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import {
+  CombinedMapStatuses,
+  CombinedRoles,
+  DateString,
+  MapCreditNames,
+  MapCreditType,
   MapImage,
   MapNotify,
+  MapStatusNameNew,
+  MapStatusNew,
+  MMap,
   ReportType
 } from '@momentum/constants';
-import { PartialDeep } from 'type-fest';
 import { MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { TabViewModule } from 'primeng/tabview';
 import {
   AvatarComponent,
+  CardBodyComponent,
+  CardComponent,
+  CardHeaderComponent,
+  ReportButtonComponent,
+  SpinnerComponent,
+  TabComponent,
+  TabDirective,
+  TabsComponent
 } from '../../../components';
+import { SpinnerDirective, TooltipDirective } from '../../../directives';
 import { SharedModule } from '../../../shared.module';
+import {
+  LayoutService,
+  LocalUserService,
+  MapsService
+} from '../../../services';
+import {
+  PluralPipe,
+  ThousandsSuffixPipe,
+  UnsortedKeyvaluePipe
+} from '../../../pipes';
 import { MapLeaderboardComponent } from './map-leaderboard/map-leaderboard.component';
 import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-edit.component';
+import { GalleriaModule } from 'primeng/galleria';
+import { GroupedMapCredits, GroupedMapLeaderboards } from '../../../util';
+import { Enum } from '@momentum/enum';
+import { MapSubmissionComponent } from './map-submission/map-submission.component';
+
+/**
+ * Using an m-tabs for this page doesn't work with the layout, we use this to
+ * explicitly enumerate possible RHS sections.
+ */
+enum MapInfoSection {
+  SUBMISSION = 1,
+  LEADERBOARDS = 2
+}
 
 @Component({
   selector: 'm-map-info',
@@ -21,6 +63,7 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
   imports: [
     SharedModule,
     MapLeaderboardComponent,
+    MapSubmissionComponent,
     ReportButtonComponent,
     CardHeaderComponent,
     CardComponent,
@@ -29,20 +72,42 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
     AvatarComponent,
     PluralPipe,
     ThousandsSuffixPipe,
+    TabsComponent,
+    GalleriaModule,
+    CardBodyComponent,
+    TabDirective,
+    TabComponent,
+    UnsortedKeyvaluePipe,
+    SpinnerDirective,
+    SpinnerComponent
   ]
 })
+export class MapInfoComponent implements OnInit {
   protected readonly ReportType = ReportType;
+  protected readonly MapStatusNew = MapStatusNew;
+  protected readonly MapCreditType = MapCreditType;
+  protected readonly MapCreditNames = MapCreditNames;
+  protected readonly MapInfoSection = MapInfoSection;
+  protected readonly MapStatusName = MapStatusNameNew;
 
+  protected loading = false;
+  protected displayFullscreenGallery = false;
 
-  @Input() previewMap: PartialDeep<
-    { map: MMap; images: MapImage[] },
-    { recurseIntoArrays: true }
-  >;
   map: MMap;
-  mapNotify: MapNotify;
-  mapNotifications = false;
+
+  leaderboards: GroupedMapLeaderboards;
+  credits: GroupedMapCredits;
+  images: Array<{ full: string; thumb: string }>;
+
+  notify: MapNotify;
+  notifications = false;
+  inFavorites = false;
   isSubmitter: boolean;
   isModerator: boolean;
+  inSubmission: boolean;
+
+  currentSection?: MapInfoSection = null;
+  sections = () => Enum.values(MapInfoSection);
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -50,45 +115,99 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
     private readonly localUserService: LocalUserService,
     private readonly messageService: MessageService,
     private readonly dialogService: DialogService,
+    private readonly layoutService: LayoutService
   ) {}
 
   ngOnInit() {
+    this.layoutService.reserveBackgroundUrl(/\/maps\/\d+\/?$/);
 
-    if (this.previewMap) {
-      this.map = this.previewMap.map as MMap;
-    } else {
-      this.route.paramMap
-        .pipe(
-          switchMap((params: ParamMap) =>
-            this.mapService.getMap(Number(params.get('id')), {
-              expand: [
-                'info',
-                'zones',
-                'leaderboards',
-                'credits',
-                'submitter',
-                'stats',
-                'images',
-                'inFavorites',
-              ]
-            })
-        )
-    }
+    this.route.paramMap
+      .pipe(
+        tap(() => (this.loading = true)),
+        // delay(1000),
+        switchMap((params: ParamMap) =>
+          this.mapService.getMap(Number(params.get('id')), {
+            expand: [
+              'info',
+              'zones',
+              'leaderboards',
+              'credits',
+              'submitter',
+              'stats',
+              'images',
+              'thumbnail',
+              'inFavorites',
+              'inLibrary',
+              'submission',
+              'versions',
+              'currentVersion'
+            ]
+          })
+        ),
+        tap(() => (this.loading = false))
+      )
+      .subscribe((map) => this.setMap(map));
   }
 
+  setMap(map: MMap) {
+    this.map = map;
+    this.credits = new GroupedMapCredits(this.map.credits);
+    this.inSubmission = CombinedMapStatuses.IN_SUBMISSION.includes(map.status);
+    // Show Review section first if in review, otherwise leaderboards (and the
+    // tab view won't be visible anyway).
+    this.currentSection = this.inSubmission
+      ? MapInfoSection.SUBMISSION
+      : MapInfoSection.LEADERBOARDS;
 
+    this.setImages(this.map.images);
+
+    this.layoutService.setBackgroundImage(this.map.thumbnail.large);
+
+    this.localUserService.checkMapNotify(this.map.id).subscribe({
+      next: (resp) => {
+        this.notify = resp;
+        if (resp) this.notifications = true;
+      },
+      error: (error) => {
+        if (error.status !== 404)
           this.messageService.add({
+            severity: 'error',
+            summary: 'Could not check if following',
             detail: error.message
+          });
+      }
+    });
+
+    // In favourites iff num mapfavorites entries for user > 0. sorry
+    this.inFavorites = this.map.favorites?.length > 0;
+
+    this.localUserService.localUserSubject.subscribe((locUser) => {
+      this.isModerator = this.localUserService.hasRole(
+        CombinedRoles.MOD_OR_ADMIN
+      );
+      this.isSubmitter = this.map.submitterID === locUser.id;
+    });
   }
 
+  setImages(images: MapImage[]) {
+    this.images = images.map(({ small, large }) => ({
+      thumb: small,
+      full: large
+    }));
+  }
+
+  toggleFavorite() {
+    if (this.inFavorites) {
       this.localUserService
         .removeMapFromFavorites(this.map.id)
         .subscribe(() => {
+          this.inFavorites = false;
           this.map.stats.favorites--;
         });
     } else {
       this.localUserService.addMapToFavorites(this.map.id).subscribe({
         next: () => {
+          this.inFavorites = true;
           this.map.stats.favorites++;
         },
         error: (error) =>
@@ -106,17 +225,17 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
       .open(MapNotifyEditComponent, {
         header: 'Edit map notification',
         data: {
-          flags: this.mapNotify ? this.mapNotify.notifyOn : 0
+          flags: this.notify ? this.notify.notifyOn : 0
         }
       })
       .onClose.subscribe((response) => {
         if (!response) return;
         if (response.newFlags === 0) {
-          if (!this.mapNotify) return;
+          if (!this.notify) return;
           this.localUserService.disableMapNotify(this.map.id).subscribe({
             next: () => {
-              this.mapNotify.notifyOn = 0;
-              this.mapNotifications = false;
+              this.notify.notifyOn = 0;
+              this.notifications = false;
             },
             error: (error) =>
               this.messageService.add({
@@ -130,9 +249,9 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
             .updateMapNotify(this.map.id, response.newFlags)
             .subscribe({
               next: (response) => {
-                this.mapNotifications = true;
-                if (!this.mapNotify) this.mapNotify = response;
-                else this.mapNotify.notifyOn = response.newFlags;
+                this.notifications = true;
+                if (!this.notify) this.notify = response;
+                else this.notify.notifyOn = response.newFlags;
               },
               error: (error) =>
                 this.messageService.add({
@@ -145,7 +264,14 @@ import { MapNotifyEditComponent } from './map-info-notify-edit/map-info-notify-e
       });
   }
 
+  toggleMapBackground(): void {
+    this.layoutService.toggleBackgroundEnable();
   }
 
+  // TODO: https://github.com/momentum-mod/website/issues/903
+  getLatestStatusChangeDate(status: MapStatusNew): DateString {
+    return this.map.submission.dates
+      .filter((date) => date.status === status)
+      .at(-1)?.date;
   }
 }
