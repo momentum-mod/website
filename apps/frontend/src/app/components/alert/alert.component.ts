7899b89c701627d715acd42d7ed6af5029139155
tsa96 <tom@trumpetdust.org>
2024-02-09 12:01:57 +0000

    feat(front): m-alert component

---
+++ apps/frontend/src/app/components/alert/alert.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconComponent } from '../../icons';
+
+@Component({
+  selector: 'm-alert',
+  standalone: true,
+  imports: [CommonModule, IconComponent],
+  template: ` <div class="prose max-w-none">
+      <ng-content />
+    </div>
+    <button
+      type="button"
+      class="absolute right-0 top-0 m-4 opacity-75 transition-opacity hover:opacity-100"
+      (click)="dismiss()"
+    >
+      <m-icon icon="close-thick" />
+    </button>`
+})
+export class AlertComponent {
+  @HostBinding('style.display')
+  protected display = 'block';
+
+  @HostBinding('class')
+  private readonly class = 'm-alert';
+
+  dismiss() {
+    this.display = 'none';
+  }
+}
