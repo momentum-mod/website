b4d36f5333d2b8a67856a0a0e4b45d8fa78cac0b
benjl <benluijkx@gmail.com>
2024-02-19 03:07:35 -0700

    feat(back): add sendNotifications method

--- apps/backend/src/app/modules/notifications/notifications.service.ts
+++ apps/backend/src/app/modules/notifications/notifications.service.ts
@@ -1,8 +1,42 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { EXTENDED_PRISMA_SERVICE } from '../database/db.constants';
-import { ExtendedPrismaService } from '../database/prisma.extension';
+import {
+  ExtendedPrismaService,
+  ExtendedPrismaServiceTransaction
+} from '../database/prisma.extension';
 import { NotificationDto, DtoFactory } from '../../dto';
 import { NotificationType } from '@momentum/constants';
+import { Prisma } from '@prisma/client';
+
+export type AnnouncementData = {
+  type: NotificationType.ANNOUNCEMENT;
+  message: string;
+};
+export type WRAchievedData = {
+  type: NotificationType.WR_ACHIEVED;
+  runID: bigint;
+};
+export type MapStatusChangeData = {
+  type: NotificationType.MAP_STATUS_CHANGE;
+  mapID: number;
+};
+export type MapTestReqData = {
+  type: NotificationType.MAP_TESTING_REQUEST;
+  requesterID: number;
+  mapID: number;
+};
+export type ReviewPostedData = {
+  type: NotificationType.REVIEW_POSTED;
+  reviewerID: number;
+  mapID: number;
+};
+export type NotificationData =
+  | AnnouncementData
+  | WRAchievedData
+  | MapStatusChangeData
+  | MapTestReqData
+  | ReviewPostedData;
+
 @Injectable()
 export class NotificationsService {
   constructor(
@@ -19,6 +53,7 @@ export class NotificationsService {
     });
     return dbResponse.map((x) => DtoFactory(NotificationDto, x));
   }
+
   async markAsRead(
     userID: number,
     notifIDs: number[],
@@ -43,5 +78,43 @@ export class NotificationsService {
           type: { not: NotificationType.MAP_TESTING_REQUEST }
         }
       });
+  }
+
+  async sendNotifications(
+    toUserIDs: number[],
+    data: NotificationData,
+    tx: ExtendedPrismaServiceTransaction = this.db
+  ): Promise<void> {
+    const newNotif: Prisma.NotificationCreateManyInput = {
+      targetUserID: -1,
+      type: data.type
+    };
+    switch (data.type) {
+      case NotificationType.ANNOUNCEMENT:
+        newNotif.message = data.message;
+        break;
+      case NotificationType.WR_ACHIEVED:
+        newNotif.runID = data.runID;
+        break;
+      case NotificationType.MAP_STATUS_CHANGE:
+        newNotif.mapID = data.mapID;
+        break;
+      case NotificationType.MAP_TESTING_REQUEST:
+        newNotif.userID = data.requesterID;
+        newNotif.mapID = data.mapID;
+        break;
+      case NotificationType.REVIEW_POSTED:
+        newNotif.userID = data.reviewerID;
+        newNotif.mapID = data.mapID;
+        break;
+      default:
+        throw new Error(
+          'Malformed data argument! Look at the types in notifications.service.ts'
+        );
+    }
+
+    await tx.notification.createMany({
+      data: toUserIDs.map((userID) => ({ ...newNotif, targetUserID: userID }))
+    });
   }
 }
