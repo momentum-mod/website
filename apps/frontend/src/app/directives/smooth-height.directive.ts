1feea92d4806951ae39f649acdaa876a8d23ae72
tsa96 <tom@trumpetdust.org>
2024-02-28 02:26:51 +0000

    feat(front): smoothHeight directive

---
+++ apps/frontend/src/app/directives/smooth-height.directive.ts
@@ -0,0 +1,44 @@
+import {
+  Directive,
+  OnChanges,
+  HostBinding,
+  ElementRef,
+  Input
+} from '@angular/core';
+
+/**
+ * Directive for smoothly animating heights of components.
+ * The `smoothHeight` animation must be provided to the *parent* component to
+ * work.
+ *
+ * This will forcible set the component to `display: block`!
+ *
+ * Based on https://stackoverflow.com/a/52277742
+ */
+@Directive({ selector: '[smoothHeight]', standalone: true })
+export class SmoothHeightAnimDirective implements OnChanges {
+  @Input() smoothHeight: boolean;
+  @Input() display = 'block';
+  private pulse: boolean;
+  private startHeight: number;
+
+  constructor(private readonly elRef: ElementRef) {}
+
+  @HostBinding('@grow')
+  get grow() {
+    return { value: this.pulse, params: { startHeight: this.startHeight } };
+  }
+
+  @HostBinding('style') get styles(): CSSStyleDeclaration {
+    return { display: this.display, overflow: 'hidden' } as CSSStyleDeclaration;
+  }
+
+  setStartHeight() {
+    this.startHeight = this.elRef.nativeElement.clientHeight;
+  }
+
+  ngOnChanges() {
+    this.setStartHeight();
+    this.pulse = !this.pulse;
+  }
+}
