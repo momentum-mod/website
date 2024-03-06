20886ffadde981d0269e602a186204fa397dc0d3
tsa96 <tom@trumpetdust.org>
2024-03-06 20:18:08 +0000

    refactor(front): restyle and refactor m-card component

--- apps/frontend/src/app/components/card/card-body.component.ts
+++ apps/frontend/src/app/components/card/card-body.component.ts
@@ -3,9 +3,8 @@ import { Component, HostBinding } from '@angular/core';
 @Component({
   selector: 'm-card-body',
   standalone: true,
-  styles: [':host {display: block;}'],
   template: '<ng-content></ng-content>'
 })
 export class CardBodyComponent {
-  @HostBinding('class') class = 'm-card';
+  @HostBinding('class') class = 'card-body';
 }
