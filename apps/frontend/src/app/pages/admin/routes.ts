a276a5a65606d467133023e7c914a68f2d3dc0b9
tsa96 <tom@trumpetdust.org>
2024-03-11 06:54:08 +0000

    chore(front): remove old map-queue component

--- apps/frontend/src/app/pages/admin/routes.ts
+++ apps/frontend/src/app/pages/admin/routes.ts
@@ -1,15 +1,9 @@
 import { Route } from '@angular/router';
-import { MapQueueComponent } from './map-queue/map-queue.component';
 import { ReportQueueComponent } from './report-queue/report-queue.component';
 import { AdminActivityComponent } from './admin-activity/admin-activity.component';
 import { UtilitiesComponent } from './utilities/utilities.component';
 
 export default [
-  {
-    path: 'map-queue',
-    pathMatch: 'full', // TODO: I don't know why this and below have pathMatch, maybe remove?
-    component: MapQueueComponent
-  },
   {
     path: 'report-queue',
     pathMatch: 'full',
