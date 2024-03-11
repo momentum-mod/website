be5fddf774353c1a17ef3b087933d142603748c3
tsa96 <tom@trumpetdust.org>
2024-03-11 07:37:29 +0000

    feat(front): enum validator

---
+++ apps/frontend/src/app/validators/extended-validators.validator.ts
@@ -0,0 +1,18 @@
+import { Enum } from '@momentum/enum';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class ExtendededValidators {
+  static enum<T extends string | number>(
+    _enum: Record<string, T>
+  ): ValidatorFn {
+    return (control: AbstractControl<T>): ValidationErrors | null => {
+      return Enum.values(_enum).includes(control.value)
+        ? null
+        : {
+            enumError: `${control.value} must be one of ${Enum.keys(_enum).join(
+              ', '
+            )}`
+          };
+    };
+  }
+}
