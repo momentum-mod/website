f234cd1a002ab25b9e50e03c3435644ca35a38d5
tsa96 <tom@trumpetdust.org>
2024-03-12 05:05:11 +0000

    feat(front): signify unranked and hidden leaderboards

---
+++ apps/frontend/src/app/components/tooltips/unranked-leaderboards-info.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'm-unranked-leaderboards-info',
+  template: `
+    <div class="prose p-3">
+      <p class="text-lg font-medium">Unranked Leaderboard</p>
+      <p>Unranked leaderboards don't grant ranked points.</p>
+      <p>
+        Players can compete for top times on this leaderboard specifically, but
+        runs don't contribute towards your overall gamemode ranking.
+      </p>
+    </div>
+  `,
+  standalone: true
+})
+export class UnrankedLeaderboardsInfoComponent {}
