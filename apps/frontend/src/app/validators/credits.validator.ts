c2fadc7f207f09de6702844d3a4e64f784088a1e
tsa96 <tom@trumpetdust.org>
2024-03-13 04:14:58 +0000

    refactor(front): stronger typing in creditsValidator

--- apps/frontend/src/app/validators/credits.validator.ts
+++ apps/frontend/src/app/validators/credits.validator.ts
@@ -1,9 +1,8 @@
 import { AbstractControl } from '@angular/forms';
 import { MapCreditType } from '@momentum/constants';
+import { GroupedMapCredits } from '../util';
 
-// Not giving a type for AbstractControl as GroupedMapCredits lives in frontend
-// app which would violate a Nx boundary
-export function creditsValidator(control: AbstractControl) {
+export function creditsValidator(control: AbstractControl<GroupedMapCredits>) {
   return control.value?.[MapCreditType.AUTHOR].length > 0
     ? null
     : { authorRequired: true };
