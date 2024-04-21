d7295dd5aff7b675d87c6719c7a4abe5c2990b8f
tsa96 <tom@trumpetdust.org>
2024-04-21 17:17:48 +0100

    chore(meta): disable eslint in commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   '{apps,libs,tools}/**/*.{js,ts,jsx,tsx,json}': [
-    (files) => `nx affected:lint --fix --files=${files.join(',')}`,
+    // Disabling ESLint commithook, too goddamn slow!
+    // (files) => `nx affected:lint --fix --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`
   ]
 };
