c79b95049c3a714f0d57cf9c4d160d5c8fa18ca6
benjl <benluijkx@gmail.com>
2024-05-18 18:00:44 -0600

    feat(back): notifications/markAsRead DEL

--- apps/backend/src/app/dto/queries/notifications-queries.dto.ts
+++ apps/backend/src/app/dto/queries/notifications-queries.dto.ts
@@ -1,6 +1,26 @@
-import { SkipQueryProperty, TakeQueryProperty } from '../decorators';
+import {
+  BooleanQueryProperty,
+  IntCsvQueryProperty,
+  SkipQueryProperty,
+  TakeQueryProperty
+} from '../decorators';
 import { QueryDto } from './query.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
+export class NotifsMarkAsReadQueryDto extends QueryDto {
+  @IntCsvQueryProperty({
+    description: 'List of notification IDs to mark as read',
+    required: false
+  })
+  notifIDs?: number[];
+
+  @ApiProperty({
+    description:
+      'If true, notifIDs is ignored and all notifications are marked as read instead'
+  })
+  @BooleanQueryProperty({ required: false })
+  all?: boolean;
+}
 export class NotifsGetQueryDto extends QueryDto {
   @SkipQueryProperty(0)
   skip = 0;
