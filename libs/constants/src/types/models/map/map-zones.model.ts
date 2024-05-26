2844e9bbeb7ce94c84891628f78ea955dff17ed2
Panzer <tylerbeff@gmail.com>
2024-05-25 19:05:26 -0600

    refactor(shared): WIP zone format update

--- libs/constants/src/types/models/map/map-zones.model.ts
+++ libs/constants/src/types/models/map/map-zones.model.ts
@@ -4,50 +4,45 @@ export interface MapZones {
   // extends JsonObject { // TODO: #855
   formatVersion: number;
   dataTimestamp: number;
-  tracks: Tracks;
-  volumes: Volume[];
-}
-
-export interface Tracks {
-  // extends JsonObject { // TODO: #855
-  main: TrackEx;
-  stages: Track[];
-  bonuses: TrackEx[];
-}
-
-export interface Track {
-  // extends JsonObject { // TODO: #855
-  name?: string;
-  majorOrdered?: boolean;
-  minorRequired?: boolean;
-  zones: {
-    segments: Segment[];
-    end: Zone;
-    cancel?: Zone[];
-  };
-}
-
-export interface TrackEx extends Track {
-  // extends JsonObject { // TODO: #855
   maxVelocity?: number;
+  tracks: MapTracks;
+}
+
+export interface MapTracks {
+  // extends JsonObject { // TODO: #855
+  main: MainTrack;
+  bonuses: BonusTrack[];
+}
+
+export interface MainTrack {
+  zones: TrackZones;
+  stagesEndAtStageStarts: boolean;
+}
+
+export interface BonusTrack {
+  zones?: TrackZones;
   defragFlags?: number;
+}
+
+export interface TrackZones {
+  segments: Segment[];
+  end: Zone;
 }
 
 export interface Segment {
   // extends JsonObject { // TODO: #855
   limitStartGroundSpeed: boolean;
+  checkpointsRequired: boolean;
+  checkpointsOrdered: boolean;
   checkpoints: Zone[];
+  cancel: Zone[];
+  name?: string;
 }
 
 export interface Zone {
   // extends JsonObject { // TODO: #855
-  volumeIndex: number;
-  filterName?: string;
-}
-
-export interface Volume {
-  // extends JsonObject { // TODO: #855
   regions: Region[];
+  filterName?: string;
 }
 
 export interface Region {
@@ -55,7 +50,8 @@ export interface Region {
   points: Vector2D[];
   bottom: number;
   height: number;
-  teleportPos?: Vector;
-  teleportYaw?: number;
+  teleDestTargetname?: string;
+  teleDestPos?: Vector;
+  teleDestYaw?: number;
   safeHeight?: number;
 }
