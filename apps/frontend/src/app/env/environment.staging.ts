65517336090ad3218c5ebf13b4f71e3d911fa6ee
tsa96 <tom@trumpetdust.org>
2024-06-27 08:19:51 +0100

    ci(front): add staging angular configuration

---
+++ apps/frontend/src/app/env/environment.staging.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.interface';
+
+export const env: Environment = {
+  production: true,
+  api: 'https://staging-api.momentum-mod.org',
+  auth: 'https://staging-api.momentum-mod.org/auth'
+};
