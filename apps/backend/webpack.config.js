79c7f160b9e1e63feb7e16fb153bbae57ba6b1df
tsa96 <tom@trumpetdust.org>
2024-07-06 04:39:45 +0100

    build(back): stop sentryWebpackPlugin trying to run outside of CI
    Devs should never have this var defined, simplest way to do this.

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -2,13 +2,15 @@ const { composePlugins, withNx } = require('@nx/webpack');
 const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = composePlugins(withNx(), (config) => {
-  config.plugins.push(
-    sentryWebpackPlugin({
-      org: process.env['SENTRY_ORG'],
-      project: process.env['SENTRY_PROJECT'],
-      authToken: process.env['SENTRY_AUTH_TOKEN']
-    })
-  );
+  if (process.env['SENTRY_AUTH_TOKEN']) {
+    config.plugins.push(
+      sentryWebpackPlugin({
+        org: process.env['SENTRY_ORG'],
+        project: process.env['SENTRY_PROJECT'],
+        authToken: process.env['SENTRY_AUTH_TOKEN']
+      })
+    );
+  }
 
   return config;
 });
