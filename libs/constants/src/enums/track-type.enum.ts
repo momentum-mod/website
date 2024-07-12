cf9e704f0b7a33c27200a3f6f4bfba0467ecfda1
tsa96 <tom@trumpetdust.org>
2024-07-12 12:41:35 +0100

    chore(shared): remove old TrackTypeKey map
    don't even remember what I was using this for

--- libs/constants/src/enums/track-type.enum.ts
+++ libs/constants/src/enums/track-type.enum.ts
@@ -1,5 +1,3 @@
-import { MapZones } from '../types/models';
-
 export enum TrackType {
   MAIN = 0,
   STAGE = 1,
@@ -8,13 +6,6 @@ export enum TrackType {
 
 export const TrackTypeName: ReadonlyMap<TrackType, string> = new Map([
   [TrackType.MAIN, 'Main'],
-  //[TrackType.STAGE, 'Stage'],
+  [TrackType.STAGE, 'Stage'],
   [TrackType.BONUS, 'Bonus']
 ]);
-
-export const TrackTypeKey: ReadonlyMap<TrackType, keyof MapZones['tracks']> =
-  new Map([
-    [TrackType.MAIN, 'main'],
-    //[TrackType.STAGE, 'stages'],
-    [TrackType.BONUS, 'bonuses']
-  ]);
