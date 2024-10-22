760f5e7dc7403822ea7c36f80d6cc920e3697cc3
tsa96 <tom@trumpetdust.org>
2024-10-22 18:46:28 +0100

    fix(xpsys): bad index/bounds checks in getCosmeticXpForLevel
    I've tested these values against the C++ implementations, they're identical.

--- libs/xp-systems/src/xp-system.class.spec.ts
+++ libs/xp-systems/src/xp-system.class.spec.ts
@@ -8,13 +8,13 @@ describe('XpSystemsService', () => {
   });
 
   it('should calculate correct level assignments for original constants', () => {
-    // These constants may get changed in the future, we also care about the maths,
-    // so mock here and check values correspond to below sheet
+    // These constants may get changed in the future, we only care about the
+    // maths, so mock here and check values correspond to below sheet
     // https://docs.google.com/spreadsheets/d/1JiHJYsxlGPXAZqCPh7-paJI6U_TH-Ro0H0OWDFCiA74
 
     // Note, this is NOT quite what the linked spreadsheet produces.
     // At level 101, the sheet expects xpInLevel from 101 to be
-    // 152000 - it must factor in `startingValue` for some reason.  In code,
+    // 152000 - it must factor in `startingValue` for some reason. In code,
     // `startingValue` isn't used at all for levels greater than
     // `staticScaleStart`. Not really an issue, but worth being aware of.
     jest.replaceProperty(service, 'cosXpParams', {
@@ -38,29 +38,31 @@ describe('XpSystemsService', () => {
     });
 
     // prettier-ignore
-    for (const { level, xpInLevel, total } of [
-      { level: 1, xpInLevel: 21000, total: 21000 },
-      { level: 2, xpInLevel: 22000, total: 43000 },
-      { level: 3, xpInLevel: 23000, total: 66000 },
-      { level: 4, xpInLevel: 24000, total: 90000 },
-      { level: 5, xpInLevel: 25000, total: 115000 },
-      { level: 6, xpInLevel: 26000, total: 141000 },
-      { level: 7, xpInLevel: 27000, total: 168000 },
-      { level: 8, xpInLevel: 28000, total: 196000 },
-      { level: 9, xpInLevel: 29000, total: 225000 },
-      { level: 10, xpInLevel: 30000, total: 255000 },
-      { level: 11, xpInLevel: 42000, total: 297000 },
-      { level: 97, xpInLevel: 990000, total: 34995000 },
-      { level: 98, xpInLevel: 1000000, total: 35995000 },
-      { level: 99, xpInLevel: 1010000, total: 37005000 },
-      { level: 100, xpInLevel: 1020000, total: 38025000 },
-      { level: 101, xpInLevel: 1500000, total: 39525000 },
-      { level: 102, xpInLevel: 1500000, total: 41025000 },
-      { level: 103, xpInLevel: 1500000, total: 42525000 },
-      { level: 104, xpInLevel: 1500000, total: 44025000 }
+    for (const [ level, xpInLevel, xpForLevel ] of [
+      // Level | XP in level | XP for level
+      [  0,      0,            0         ],
+      [  1,      21000,        0         ],
+      [  2,      22000,        21000     ],
+      [  3,      23000,        43000     ],
+      [  4,      24000,        66000     ],
+      [  5,      25000,        90000     ],
+      [  6,      26000,        115000    ],
+      [  7,      27000,        141000    ],
+      [  8,      28000,        168000    ],
+      [  9,      29000,        196000    ],
+      [  10,     30000,        225000    ],
+      [  11,     42000,        255000    ],
+      [  97,     990000,       34005000  ],
+      [  98,     1000000,      34995000  ],
+      [  99,     1010000,      35995000  ],
+      [  100,    1020000,      37005000  ],
+      [  101,    1500000,      38025000  ],
+      [  102,    1500000,      39525000  ],
+      [  103,    1500000,      41025000  ],
+      [  104,    1500000,      42525000  ],
     ]) {
       expect(service.getCosmeticXpInLevel(level)).toBe(xpInLevel);
-      expect(service.getCosmeticXpForLevel(level)).toBe(total);
+      expect(service.getCosmeticXpForLevel(level)).toBe(xpForLevel);
     }
   });
 });
