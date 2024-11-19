c4a47fcaa9fb08ab0de465422a8215ca1ac6c2b0
tsa96 <tom@trumpetdust.org>
2024-11-19 11:13:02 +0000

    chore(meta): run nx generate convert-to-flat-config

---
+++ libs/xp-systems/eslint.config.js
@@ -0,0 +1,26 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended
+});
+
+module.exports = [
+  ...compat.extends('../../.eslintrc.js'),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {}
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    // Override or add rules here
+    rules: {}
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {}
+  }
+];
