ddeaca109c03a3760d49388bbdd894d5b309fb54
tsa96 <tom@trumpetdust.org>
2024-11-22 14:32:33 +0000

    feat(formats): new replay format
    Writer is just for E2E tests

--- libs/formats/replay/src/index.ts
+++ libs/formats/replay/src/index.ts
@@ -1,62 +1,38 @@
-export * from './replay-reader';
-export * from './replay-writer';
+import {
+  double,
+  float,
+  int32,
+  TrackType,
+  uint64,
+  uint8
+} from '@momentum/constants';
+import { magic } from '@momentum/util-fn';
 
-export interface Replay {
-  magic: number;
-  version: number;
-  header: {
-    mapName: string;
-    mapHash: string;
-    playerName: string;
-    steamID: bigint;
-    tickRate: number;
-    runFlags: number;
-    runDate: string;
-    startTick: number;
-    stopTick: number;
-    trackNum: number;
-    zoneNum: number;
-  };
-  overallStats: BaseStats;
-  zoneStats: ZoneStats[];
-  frames: RunFrame[];
+export const REPLAY_MAGIC: int32 = magic('MMTV');
+
+export * as Reader from './replay-reader';
+export * as Writer from './replay-writer';
+export * as Stubs from './replay.stub';
+
+/**
+ * ReplayHeader struct in C++
+ * 193 bytes total, packed on byte boundaries with #pragma pack(1)
+ * @see momtv.h, mom_timer_defs.h (licensee-only)
+ */
+// prettier-ignore
+export interface ReplayHeader {
+  //                         Size  Offset  Description
+  magic: int32;           // 4     0
+  formatVersion: int32;   // 4     4
+  timestamp: number;      // 8     8       Unix time (ms) written when the replay file is created
+  mapName: string;        // 64    16
+  mapHash: string;        // 41    80
+  gamemode: uint8;        // 1     121
+  tickInterval: float;    // 4     122
+  playerSteamID: uint64;  // 8     126
+  playerName: string;     // 32    134
+  trackType: TrackType;   // 1     166
+  trackNum: uint8;        // 1     167
+  runTime: double;        // 8     168
+                          // 17    176     Other replay data, unused by us
 }
-
-export type ReplayHeader = Omit<
-  Replay,
-  'overallStats' | 'zoneStats' | 'frames'
->;
-
-export interface RunFrame {
-  eyeAngleX: number;
-  eyeAngleY: number;
-  eyeAngleZ: number;
-  posX: number;
-  posY: number;
-  posZ: number;
-  viewOffset: number;
-  buttons: number;
-}
-
-// This should correspond to both Prisma and game's BaseStats model
-export type BaseStats = {
-  jumps: number;
-  strafes: number;
-  avgStrafeSync: number;
-  avgStrafeSync2: number;
-  enterTime: number;
-  totalTime: number;
-  velAvg3D: number;
-  velAvg2D: number;
-  velMax3D: number;
-  velMax2D: number;
-  velEnter3D: number;
-  velEnter2D: number;
-  velExit3D: number;
-  velExit2D: number;
-};
-
-export type ZoneStats = {
-  zoneNum: number;
-  baseStats: BaseStats;
-};
