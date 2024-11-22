88ee3f3a69ae1a385e632209b8be2119db7c547d
tsa96 <tom@trumpetdust.org>
2024-11-22 14:31:03 +0000

    feat(shared): numeric type aliases
    Not branding these, too annoying, just doing this for clarity when
    parsing replay headers

---
+++ libs/constants/src/types/utils/numeric.type.ts
@@ -0,0 +1,14 @@
+export type uint8 = number;
+export type uint16 = number;
+export type uint32 = number;
+export type uint64 = bigint;
+export type int8 = number;
+export type int16 = number;
+export type int32 = number;
+export type int64 = bigint;
+
+export type float = number;
+export type double = number;
+
+export type vec2 = { x: number; y: number };
+export type vec3 = { x: number; y: number; z: number };
