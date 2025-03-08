47f1ccb8623762c3dfbf9a8eee4e87cfee4f1bd3
tsa96 <tom@trumpetdust.org>
2025-03-08 17:55:44 +0000

    feat(back): explicitly disconnect Prisma client on DbService shutdown

--- apps/backend/src/app/modules/database/db.service.ts
+++ apps/backend/src/app/modules/database/db.service.ts
@@ -1,9 +1,19 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DbService extends PrismaClient implements OnModuleInit {
+export class DbService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+  }
+
+  // Prisma supposedly has some some magic built-in support for this, don't
+  // understand exactly how it works, and doesn't seem to work with our client
+  // extension setup in E2E tests. Easier to just do ourselves like this.
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
