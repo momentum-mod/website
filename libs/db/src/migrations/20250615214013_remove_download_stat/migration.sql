504f4881a21b478091d09ed64520b3b104fccb72
Ash_and_Cinders <171430588+AshenCinders@users.noreply.github.com>
2025-06-15 22:12:22 +0200

    chore(shared): remove map download stat

---
+++ libs/db/src/migrations/20250615214013_remove_download_stat/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `downloads` on the `MapStats` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "MapStats" DROP COLUMN "downloads";
