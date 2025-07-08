e5645fd2e8cec0b7f43499eb94d40724bf0c494d
GordiNoki <gordinoki8@gmail.com>
2025-07-08 12:19:32 +0300

    feat(back): use discord bot instead of webhooks for map status notifications

---
+++ apps/backend/src/app/modules/discord/discord.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client } from 'discord.js';
+import { Agent } from 'undici';
+
+@Injectable()
+export class DiscordService {
+  private enabled = false;
+
+  isEnabled(): this is Client {
+    return this.enabled;
+  }
+
+  private constructor() {
+    const client = new Client({ intents: [] });
+
+    // https://github.com/nodejs/undici/issues/1531#issuecomment-1178869993
+    client.rest.setAgent(new Agent({ connect: { timeout: 60000 } }));
+
+    Object.assign(this, client);
+  }
+
+  static async factory(config: ConfigService) {
+    const client = new DiscordService();
+    client.enabled = true;
+
+    if (!client.isEnabled()) return client;
+
+    const token = config.getOrThrow('discord.token');
+    if (!token) return client;
+
+    client.token = token;
+    client.rest.setToken(token);
+
+    try {
+      await client.guilds.fetch(config.getOrThrow('discord.guild'));
+    } catch (e) {
+      Logger.error(
+        'Failed to fetch Discord guild, Discord client will be disabled.'
+      );
+      Logger.error(e);
+      client.enabled = false;
+    }
+
+    return client;
+  }
+}
