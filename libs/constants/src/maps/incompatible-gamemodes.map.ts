6f01291703d889f1c635efa54cc70cdcb275638a
tsa96 <tom@trumpetdust.org>
2025-08-01 04:00:45 +0100

    refactor(shared): update IncompatibleGamemodes map

--- libs/constants/src/maps/incompatible-gamemodes.map.ts
+++ libs/constants/src/maps/incompatible-gamemodes.map.ts
@@ -6,7 +6,7 @@ import { Gamemode, GamemodeCategories, GamemodeCategory } from '../';
 // for future-proofing, for example 100t surf would have the exact same rules
 // as regular surf.
 // JAVASCRIPT PLEASE ADD CONSTEXPR!!!
-const G = Gamemode; //
+const G = Gamemode;
 const GC = GamemodeCategory;
 const Cats = (...gcs: GamemodeCategory[]) =>
   gcs.flatMap((gc) => GamemodeCategories.get(gc)!);
@@ -15,30 +15,29 @@ const Cats = (...gcs: GamemodeCategory[]) =>
  * A non-reflexive map of gamemodes and collections of gamemodes they can
  * *never* have compatibile leaderboards for.
  *
- * For example, there's *never* a possible case
- * where a surf leaderboard should also have a bhop leaderboard, that'd just be
- * jank 100-tick surf.
+ * For example, there's *never* a possible case where a surf leaderboard should
+ * also have a bhop leaderboard, that'd just be jank 100-tick surf.
  *
- * Note the non-reflexivity, you might have a surf map that's worth a
- * leaderboard in ahop (e.g. utopia), but a ahop map would never have a surf
- * leaderboard, since that'd just be bhop with worse settings.
+ * Note the non-reflexivity, some bhop maps are playable in climb, but no climb
+ * map has a bhop leaderboard, as we don't want to have an easy mode of climb
+ * with autohop.
  */
 // prettier-ignore
 export const IncompatibleGamemodes: ReadonlyMap<Gamemode, ReadonlySet<Gamemode>> = new Map(
   [
-    [G.SURF,        [Cats(GC.BHOP, GC.CLIMB)]],
-    [G.BHOP,        [Cats(GC.SURF, GC.CLIMB)]],
-    [G.BHOP_HL1,    [Cats(GC.SURF, GC.CLIMB)]],
+    [G.SURF,        [Cats(GC.BHOP), G.CLIMB_MOM, G.CLIMB_KZT]],
+    [G.BHOP,        [Cats(GC.SURF), G.CLIMB_MOM, G.CLIMB_KZT]],
+    [G.BHOP_HL1,    [Cats(GC.SURF), G.CLIMB_MOM, G.CLIMB_KZT]],
     [G.CLIMB_MOM,   [Cats(GC.SURF, GC.BHOP)]],
     [G.CLIMB_KZT,   [Cats(GC.SURF, GC.BHOP)]],
     [G.CLIMB_16,    [Cats(GC.SURF, GC.BHOP)]],
-    [G.RJ,          [Cats(GC.SURF, GC.CLIMB, GC.BHOP, GC.AHOP)]],
-    [G.SJ,          [Cats(GC.SURF, GC.CLIMB, GC.BHOP, GC.AHOP)]],
-    [G.AHOP,        [Cats(GC.SURF, GC.CLIMB)]],
-    [G.CONC,        [Cats(GC.SURF, GC.CLIMB, GC.BHOP, GC.AHOP)]],
-    [G.DEFRAG_CPM,  [Cats(GC.SURF, GC.CLIMB)]],
-    [G.DEFRAG_VQ3,  [Cats(GC.SURF, GC.CLIMB)]],
-    [G.DEFRAG_VTG,  [Cats(GC.SURF, GC.CLIMB)]]
+    [G.RJ,          []],
+    [G.SJ,          []],
+    [G.AHOP,        []],
+    [G.CONC,        []],
+    [G.DEFRAG_CPM,  []],
+    [G.DEFRAG_VQ3,  []],
+    [G.DEFRAG_VTG,  []]
   ].map(([k, v]) => [
     k as Gamemode,
     new Set((v as [Gamemode | Gamemode[]]).flat())
