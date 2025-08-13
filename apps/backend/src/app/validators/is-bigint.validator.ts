94b100d90c5829cd1ef3bc61fdf72428f1146dcf
GordiNoki <gordinoki8@gmail.com>
2025-08-13 19:52:31 +0300

    fix(back): limit bigint numbers to signed 8 byte number

--- apps/backend/src/app/validators/is-bigint.validator.ts
+++ apps/backend/src/app/validators/is-bigint.validator.ts
@@ -1,5 +1,8 @@
 ﻿import { registerDecorator, ValidationOptions } from 'class-validator';
 
+const MAX_INT64 = BigInt(2 ** 63);
+const MIN_INT64 = -BigInt(2 ** 63);
+
 export function IsBigInt(validationOptions?: ValidationOptions) {
   return function (object: object, propertyName: string) {
     registerDecorator({
@@ -8,7 +11,8 @@ export function IsBigInt(validationOptions?: ValidationOptions) {
       propertyName: propertyName,
       options: validationOptions,
       validator: {
-        validate: (value: unknown) => typeof value === 'bigint',
+        validate: (value: unknown) =>
+          typeof value === 'bigint' && value < MAX_INT64 && value >= MIN_INT64,
         defaultMessage() {
           return `${propertyName} must be a BigInt.`;
         }
