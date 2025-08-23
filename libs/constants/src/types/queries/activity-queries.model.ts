d8bff4df423225eae48f947d46451d9600513ce2
GordiNoki <gordinoki8@gmail.com>
2025-08-23 23:23:49 +0300

    chore(shared): remove unused type

--- libs/constants/src/types/queries/activity-queries.model.ts
+++ libs/constants/src/types/queries/activity-queries.model.ts
@@ -1,11 +1,8 @@
 ﻿import { ActivityType } from '../../enums/activity-type.enum';
 import { PagedQuery } from './pagination.model';
-import { Notification } from '../../';
 
 export type ActivitiesGetQuery = PagedQuery & {
   userID?: number;
   type?: ActivityType;
   data?: number;
 };
-
-export type UpdateNotification = Pick<Notification, 'read'>;
