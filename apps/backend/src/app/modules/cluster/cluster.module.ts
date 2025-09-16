1512ea41d6a0a42564179747e57815a507ea2e70
tsa96 <tom@trumpetdust.org>
2025-09-16 06:39:25 +0100

    feat(back): add clustering

---
+++ apps/backend/src/app/modules/cluster/cluster.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ClusterService } from './cluster.service';
+
+@Module({
+  providers: [ClusterService],
+  exports: [ClusterService]
+})
+export class ClusterModule {}
