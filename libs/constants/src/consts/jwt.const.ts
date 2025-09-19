0d700057a8bbbdfadf85835a841dc4330e0c81dd
tsa96 <tom@trumpetdust.org>
2025-09-19 06:55:38 +0100

    refactor(back): increase JWT expiration times

--- libs/constants/src/consts/jwt.const.ts
+++ libs/constants/src/consts/jwt.const.ts
@@ -1,3 +1,3 @@
-export const JWT_WEB_EXPIRY_TIME = '15m';
+export const JWT_WEB_EXPIRY_TIME = '24h';
 export const JWT_GAME_EXPIRY_TIME = '24h';
-export const JWT_REFRESH_EXPIRY_TIME = '5d';
+export const JWT_REFRESH_EXPIRY_TIME = '30d';
