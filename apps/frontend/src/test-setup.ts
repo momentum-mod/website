6548f4bb145b4dfd925a5eb7870d9b233d7b6663
tsa96 <tom@trumpetdust.org>
2025-09-29 23:27:59 +0100

    fix(front): remove broken polyfill from unit tests
    I don't know or care

--- apps/frontend/src/test-setup.ts
+++ apps/frontend/src/test-setup.ts
@@ -1,8 +1,3 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv();
-
-// Dumb polyfill to fix dumb Jest errors
-import { TextEncoder, TextDecoder } from 'util';
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
