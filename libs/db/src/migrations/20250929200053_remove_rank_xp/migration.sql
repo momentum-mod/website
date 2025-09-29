93622547efece7957469e3ff27adc5b4669fb75f
tsa96 <tom@trumpetdust.org>
2025-09-29 21:01:33 +0100

    feat(db): add migration to remove rankXP column

---
+++ libs/db/src/migrations/20250929200053_remove_rank_xp/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rankXP` on the `LeaderboardRun` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LeaderboardRun" DROP COLUMN "rankXP";
