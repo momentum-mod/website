d07cfd3ca7994e2e371db8db52ec8e704e4d1115
Vitor Caleffi <vitor.formigari@gmail.com>
2025-11-20 13:45:35 +0100

    bug(dev): Set host to 0.0.0.0 on start.sh

--- dev/start.sh
+++ dev/start.sh
@@ -10,4 +10,4 @@ npx nx run db:generate --args="--sql --no-hints"
 echo "Starting services..."
 # We use npx concurrently to run both services. 
 # Adjust the commands if your nx targets are different (e.g., serve:backend vs serve backend)
-npx nx run-many -t serve -p backend frontend
+npx nx run-many -t serve -p backend frontend -- --host=0.0.0.0
