08c1738efdb6714e2f32bed5f7ce054a919d9a59
Glyph <171430588+AshenCinders@users.noreply.github.com>
2026-01-18 21:27:29 +0100

    refactor(front): remove anchor positioning fix
    now redundant

--- apps/frontend/src/app/components/dropdown/dropdown.component.ts
+++ apps/frontend/src/app/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { IconComponent } from '../../icons';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
@@ -19,7 +19,7 @@ export class DropdownComponent implements ControlValueAccessor {
   // First element is used as default for dropdown button in template.
   @Input({ required: true }) entries: number[] = [];
 
-  // TODO when you add support for string entries, use same strategy as chips component
+  // If you're adding support for string entries, use the same strategy as chips component does
   @Input({ required: true }) nameFn: (entry: number) => string;
 
   protected selectedEntry: number;
@@ -52,27 +52,5 @@ export class DropdownComponent implements ControlValueAccessor {
   onTouched = () => void 0;
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
-  }
-
-  // Remove everything below when Firefox implements anchor positioning.
-  // Also remove call in template, and labels for ViewChild-s.
-
-  @ViewChild('toggle')
-  toggleElem: ElementRef<HTMLButtonElement>;
-
-  @ViewChild('dropdown', { static: false })
-  dropdownElem: ElementRef<HTMLDivElement>;
-
-  // NOTE: left-aligned; standard is right-aligned to toggle element.
-  fixUnsupportedAnchorPosition() {
-    if (!('anchorName' in document.documentElement.style)) {
-      const toggleElemCoords =
-        this.toggleElem.nativeElement.getBoundingClientRect();
-      // Gets position fixed implicitly by popover when shown.
-      this.dropdownElem.nativeElement.style.top =
-        toggleElemCoords.bottom.toString() + 'px';
-      this.dropdownElem.nativeElement.style.left =
-        toggleElemCoords.left.toString() + 'px';
-    }
   }
 }
