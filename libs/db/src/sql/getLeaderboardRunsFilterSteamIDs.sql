d1d9cb7d8ede3c7c08fbf9cebd565b74822da0d3
tsa96 <tom@trumpetdust.org>
2026-02-25 23:03:24 +0000

    feat(back): leaderboard queries in SQL
    The core queries using for LeaderboardRuns, plus the wrapper service
    that handles unflattening the queries

---
+++ libs/db/src/sql/getLeaderboardRunsFilterSteamIDs.sql
@@ -0,0 +1,46 @@
+-- @param {Int} $1:mapID Map ID
+-- @param {Int} $2:gamemode Gamemode
+-- @param {Int} $3:trackType Track Type
+-- @param {Int} $4:trackNum Track Number
+-- @param {Int} $5:style Style number
+-- @param       $6:userIDs Array of Steam IDs to filter by
+-- @param {Int} $7:offset Pagination offset
+-- @param {Int} $8:limit Pagination limit
+
+WITH ranked AS (SELECT "userID",
+                       "mapID",
+                       "gamemode",
+                       "trackType",
+                       "trackNum",
+                       "style",
+                       "time",
+                       "replayHash",
+                       "flags",
+                       "pastRunID",
+                       "createdAt",
+                       CAST(ROW_NUMBER()
+                            OVER (ORDER BY "time", "createdAt") AS INT) "rank"
+                FROM "LeaderboardRun"
+                WHERE ("mapID",
+                       "gamemode",
+                       "trackType",
+                       "trackNum",
+                       "style") = ($1, $2, $3, $4, $5))
+SELECT "ranked".*,
+       u."id"        AS "user_id",
+       u."steamID"   AS "user_steamID",
+       u."alias"     AS "user_alias",
+       u."avatar"    AS "user_avatar",
+       u."country"   AS "user_country",
+       u."roles"     AS "user_roles",
+       u."bans"      AS "user_bans",
+       u."createdAt" AS "user_createdAt"
+FROM ranked
+       INNER JOIN "User" AS "u" ON "ranked"."userID" = "u"."id"
+WHERE "u"."steamID" = ANY ($6)
+ORDER BY "ranked"."time",
+         "ranked"."createdAt"
+OFFSET $7 LIMIT $8;
+
+
+
