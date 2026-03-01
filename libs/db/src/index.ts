f4c52a2af1c3c92e8997f7192c6a3ffe4aa22676
tsa96 <tom@trumpetdust.org>
2026-03-01 17:07:40 +0000

    refactor(back): export TypedSql via modules, not Client Extensions
    We want to be able to use some of these queries in tests, which don't
    currently used the extended client. They're just JS modules anyway,
    simplest to just export them

--- libs/db/src/index.ts
+++ libs/db/src/index.ts
@@ -2,3 +2,4 @@ export * from './extended-client';
 export * from './generated/client';
 export * from './generated/models';
 export * from './generated/enums';
+export * as TypedSql from './generated/sql';
