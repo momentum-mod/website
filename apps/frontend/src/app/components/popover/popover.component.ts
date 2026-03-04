ce184c6f7ad2fea3bd1acf1aa9d8e0cd344f7c44
taoaustin <taoaustin@yahoo.ca>
2026-03-04 00:46:34 -0500

    refactor(front): generic popover component

---
+++ apps/frontend/src/app/components/popover/popover.component.ts
@@ -0,0 +1,69 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild
+} from '@angular/core';
+
+@Component({
+  selector: 'm-popover',
+  template: `
+    <div
+      #popover
+      popover
+      [style]="{
+        'position-anchor': this.anchorName
+      }"
+      (toggle)="onToggle($event)"
+    >
+      <ng-content />
+    </div>
+  `,
+  styleUrl: './popover.component.css'
+})
+export class PopoverComponent {
+  /**
+   * https://www.oddbird.net/2025/01/29/anchor-position-validity/
+   * an anchor should typically be a sibling element, or sibling to an ancestor, but not a direct ancestor
+   */
+  @Input() anchorName: string;
+
+  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
+  @Output() onShow = new EventEmitter();
+  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
+  @Output() onHide = new EventEmitter();
+
+  @ViewChild('popover') popover: ElementRef<HTMLElement>;
+
+  active = false;
+
+  onToggle(event: ToggleEvent) {
+    if (event.newState === 'open') {
+      this.active = true;
+      this.onShow.emit();
+    } else {
+      this.active = false;
+      this.onHide.emit();
+    }
+  }
+
+  show() {
+    this.popover.nativeElement.showPopover();
+  }
+
+  hide() {
+    this.popover.nativeElement.hidePopover();
+  }
+
+  toggle() {
+    if (this.active) {
+      this.hide();
+    } else {
+      this.show();
+    }
+    // only works when popover="manual"
+    // this.popover.nativeElement.togglePopover();
+  }
+}
