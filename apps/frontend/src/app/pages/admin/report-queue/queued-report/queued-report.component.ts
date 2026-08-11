6b675c315dc45683481b83600117e0ee12562467
Panzer <tylerbeff@gmail.com>
2026-08-10 21:22:54 -0600

    feat(front): Report queue improvements

--- apps/frontend/src/app/pages/admin/report-queue/queued-report/queued-report.component.ts
+++ apps/frontend/src/app/pages/admin/report-queue/queued-report/queued-report.component.ts
@@ -1,33 +1,52 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
   OnInit,
   Output,
+  ViewChild,
   inject
 } from '@angular/core';
 import { ReportCategory, ReportType, Report } from '@momentum/constants';
 import { DialogService } from 'primeng/dynamicdialog';
 import { UpdateReportDialogComponent } from '../update-report-dialog/update-report-dialog.component';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 import { AvatarComponent } from '../../../../components/avatar/avatar.component';
 import { IconComponent } from '../../../../icons';
 import { TooltipDirective } from '../../../../directives/tooltip.directive';
 
 @Component({
   selector: 'm-queued-report',
-  imports: [RouterLink, AvatarComponent, IconComponent, TooltipDirective],
+  imports: [
+    RouterLink,
+    AvatarComponent,
+    IconComponent,
+    TooltipDirective,
+    DatePipe
+  ],
   templateUrl: './queued-report.component.html'
 })
-export class QueuedReportComponent implements OnInit {
+export class QueuedReportComponent implements OnInit, AfterViewInit {
   private readonly dialogService = inject(DialogService);
 
   @Input() report: Report;
   @Output() reportUpdate = new EventEmitter<Report>();
 
+  @ViewChild('messageText') messageText: ElementRef<HTMLElement>;
+  @ViewChild('resolutionMessageText')
+  resolutionMessageText: ElementRef<HTMLElement>;
+
   typeText: string;
   categoryText: string;
   reportedResourceURL = '';
+
+  messageExpanded = false;
+  resolutionMessageExpanded = false;
+  messageOverflows = false;
+  resolutionMessageOverflows = false;
 
   ngOnInit() {
     switch (this.report.type) {
@@ -61,6 +80,34 @@ export class QueuedReportComponent implements OnInit {
       case ReportCategory.OTHER:
         this.categoryText = 'Other';
         break;
+    }
+  }
+
+  ngAfterViewInit() {
+    // Deferred a tick to avoid writing to these bindings during the same
+    // change detection pass that rendered them (ExpressionChangedAfterChecked).
+    // Measured while collapsed (line-clamp-2 applied), so scrollHeight
+    // exceeding clientHeight means the text is actually being truncated.
+    setTimeout(() => {
+      this.messageOverflows = this.isOverflowing(this.messageText);
+      this.resolutionMessageOverflows = this.isOverflowing(
+        this.resolutionMessageText
+      );
+    });
+  }
+
+  private isOverflowing(ref: ElementRef<HTMLElement>): boolean {
+    const el = ref?.nativeElement;
+    return !!el && el.scrollHeight > el.clientHeight;
+  }
+
+  toggleMessage() {
+    if (this.messageOverflows) this.messageExpanded = !this.messageExpanded;
+  }
+
+  toggleResolutionMessage() {
+    if (this.resolutionMessageOverflows) {
+      this.resolutionMessageExpanded = !this.resolutionMessageExpanded;
     }
   }
 
